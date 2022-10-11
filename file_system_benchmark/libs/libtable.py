d763d497651c5b62831ded6dd133f59eb1aa01c0
Роман Кузнецов <rkuznetsov@astralinux.ru>
2022-10-11 11:29:41 +0300

    * new total rating

--- file_system_benchmark/libs/libtable.py
+++ file_system_benchmark/libs/libtable.py
@@ -320,10 +320,12 @@ class Report:
     def get_speed_rating(self,
                          x_lst,
                          lower_limit=START_BORDER_FOR_DATA,
-                         upper_limit=END_BORDER_FOR_DATA):
+                         upper_limit=END_BORDER_FOR_DATA,
+                         corr_coeff1=10 ** 3,
+                         corr_coeff2=10 ** 20):
         func_speed = self.data_aproximation(x_lst, self.speed_lst)
         i_spd, err = integrate.quad(func_speed, lower_limit, upper_limit)
-        return 1 / i_spd
+        return i_spd * (corr_coeff1) / (corr_coeff2)
 
     def get_app_overhead_rating(self,
                                 x_lst,
@@ -388,16 +390,17 @@ class Report:
                          x_lst,
                          lower_limit=START_BORDER_FOR_DATA,
                          upper_limit=END_BORDER_FOR_DATA,
-                         accuracy=13):
-        return round(self.get_speed_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_app_overhead_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_create_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_write_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_fsync_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_sync_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_close_rating(x_lst, lower_limit, upper_limit) + \
-                     self.get_unlink_rating(x_lst, lower_limit, upper_limit),
-                     accuracy) * 10000000000
+                         accuracy=10,
+                         multiplier=10**10):
+        return round(round(self.get_speed_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_app_overhead_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_create_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_write_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_fsync_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_sync_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_close_rating(x_lst, lower_limit, upper_limit) + \
+                           self.get_unlink_rating(x_lst, lower_limit, upper_limit),
+                           accuracy) * multiplier, 3)
 
     ####################################################################################################################
     def merge(self, ox_lst, table_lst, graph_lst, path=REPORT_PATH):
@@ -493,3 +496,16 @@ class Report:
                                                              t=time()), 'w') as tar:
             for file in listdir(path_to_files):
                 tar.add('{}/{}'.format(path_to_files, file))
+
+r = Report()
+print(r.get_speed_rating(r.file_count_lst))
+print(r.get_app_overhead_rating(r.file_count_lst))
+print(r.get_create_rating(r.file_count_lst))
+print(r.get_write_rating(r.file_count_lst))
+print(r.get_fsync_rating(r.file_count_lst))
+print(r.get_sync_rating(r.file_count_lst))
+print(r.get_close_rating(r.file_count_lst))
+print(r.get_unlink_rating(r.file_count_lst))
+
+print('total')
+print(r.get_total_rating(r.file_count_lst))

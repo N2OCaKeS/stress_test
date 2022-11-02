2f6285bce5d9eb1ceff609a3469520edcce288ad
Роман Кузнецов <rkuznetsov@astralinux.ru>
2022-11-02 18:31:43 +0300

    * DEVQA-1688: fileaud in work

--- auditd_benchmark/aub_conf.py
+++ auditd_benchmark/aub_conf.py
@@ -12,8 +12,10 @@ REPORT = '{}/{}'.format(REPORT_DIR, REPORT_FILENAME)
 LATENCY_REPORT = '{}/aub_report_latency.txt'.format(REPORT_DIR)
 LOSSES_REPORT = '{}/aub_report_losses.txt'.format(REPORT_DIR)
 
-TEST_USER='u'
-PROC_BODYS = {'open': ('cat /etc/passwd', ''),
+MAIN_USER='u'
+TEST_USER='tester'
+
+PSAUD_PROC_BODYS = {'open': ('cat /etc/passwd', ''),
               'create': ('touch /tmp/file1', ''),
               'exec': ('/bin/true', ''),
               'remove': ('rm /tmp/file1', ''),
@@ -21,21 +23,49 @@ PROC_BODYS = {'open': ('cat /etc/passwd', ''),
               'chown': ('chown :users /tmp/file1', ''),
               'mount': ('mount --bind /tmp/dir1 /mnt/', ''),
               'module': ('modprobe 8021q', ''),
-              'uid': ('sudo -u {} /bin/true'.format(TEST_USER), ''),
-              'gid': ('sudo -u {} /bin/true'.format(TEST_USER), ''),
-              'acl': ('setfacl -m u:{}:rx /tmp/dir1'.format(TEST_USER), ''),
+              'uid': ('sudo -u {} /bin/true'.format(MAIN_USER), ''),
+              'gid': ('sudo -u {} /bin/true'.format(MAIN_USER), ''),
+              'acl': ('setfacl -m u:{}:rx /tmp/dir1'.format(MAIN_USER), ''),
               'mac': ('pdpl-file 0:63:0:ccnri /dir1', ''),
               'cap': ('usercaps -l 0x1 u', ''),
-              #'chroot': ('chroot /', ''),
+              # 'chroot': ('chroot /', ''),
               'rename': ('mv /tmp/file1 /tmp/file2', ''),
               'net': ('ping -c 1 localhost', '')}
 
 
+USERAUD_PROC_BODYS = { 'open': ('cat /etc/passwd',''),
+                       'create': ('touch /tmp/file1',''),
+                       'exec': ('/bin/true',''),
+                       'delete': ('rm /tmp/file1',''),
+                       'chmod': ('chmod 700 /tmp/file1',''),
+                       'chown': ('chown :users /tmp/file1',''),
+                       'mount': ('mount --bind /tmp/dir1 /mnt/',''),
+                       'module': ('/sbin/modprobe evbug',''),
+                       'uid': ('',''),
+                       'gid': ('',''),
+                       'audit': ('/usr/sbin/setfaud -m o:o:o /root',''),
+                       'acl': ('setfacl -m u:{}:rx /tmp/dir1'.format(TEST_USER), ''),
+                       'mac': ('/usr/sbin/pdpl-file 2:0:0 /home/{}/test_mac'.format(TEST_USER),''),
+                       'cap': ('pscaps 0 0x1',''),
+                       # 'chroot': ('usercaps -l 0x1 u',''),
+                       'rename': ('mv /tmp/file1 /tmp/file2',''),
+                       'net': ('ping -c 1 localhost','')}
+
+FILEAUD_PROC_BODYS = { 'open': ('cat /tmp/file_open', ''),
+                       'create': ('touch /tmp/file_create', ''),
+                       'exec': ('/tmp/file_exec', ''),
+                       'delete': ('rm -f /tmp/file_delete', ''),
+                       'chmod': ('chmod 777 /tmp/file_chmod', ''),
+                       'chown': ('chown {u}:{u} /tmp/file_chown'.format(u=MAIN_USER), ''),
+                       'audit': ('getfaud /tmp/file_audit', ''),
+                       'acl': ('setfacl -m u:{}:rw /tmp/file_acl'.format(MAIN_USER), ''),
+                       'mac': ('pdpl-file 2:0:0 /tmp/file_mac'.format(MAIN_USER), ''),
+                       'modify': ("echo '1' >> /tmp/file_modify", '')}
 
 
 PS_LOWER_LIMIT = 1
 PS_UPPER_LIMIT = 5
 PS_STEP = 1
 
-DEFAULT_PS_LIFETIME = 10
+DEFAULT_PS_LIFETIME = 1
 DEFAULT_PS_EVENT_RE_INITIALIZATION_DELAY = 0.01

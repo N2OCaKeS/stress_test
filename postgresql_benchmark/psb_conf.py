d3ecff165838c2b1b42cf5a6d926e34c1baf9b47
Роман Кузнецов <rkuznetsov@astralinux.ru>
2023-01-10 15:27:13 +0300

    * added storage for database

--- postgresql_benchmark/psb_conf.py
+++ postgresql_benchmark/psb_conf.py
@@ -1,4 +1,4 @@
-SCRIPT_DIR = '/media/sf_git/stress_test/postgresql_benchmark'
+SCRIPT_DIR = '/home/u/git/stress_test/postgresql_benchmark'
 LOG_FILENAME = '{}/psb_log'.format(SCRIPT_DIR)
 REPORT_PATH = '{}/report'.format(SCRIPT_DIR)
 REPORT_FILENAME = '{}/psb_report.txt'.format(REPORT_PATH)

25b492b24c489e4d9c42d922fcb0cc184a7af610
Роман Кузнецов <rkuznetsov@astralinux.ru>
2023-01-24 10:35:54 +0300

    * new params

--- cluster_file_system_benchmark/cfs_publish.py
+++ cluster_file_system_benchmark/cfs_publish.py
@@ -76,7 +76,21 @@ parser.add_argument('-rp', '--report-path',
                     help='path to report files',
                     dest='R_PATH')
 
-parser.add_argument('-tp', '--tarfile-path',
+parser.add_argument('-tp', '--template-path',
+                    action='store',
+                    required=False,
+                    default=TEMPLATE_PATH,
+                    help='path to dir with template files',
+                    dest='T_PATH')
+
+parser.add_argument('-ip', '--info-path',
+                    action='store',
+                    required=False,
+                    default=INFO_FILENAME,
+                    help='path to info file',
+                    dest='I_PATH')
+
+parser.add_argument('-tarp', '--tarfile-path',
                     action='store',
                     required=False,
                     default=None,
@@ -138,9 +152,9 @@ for file in os.listdir(args.R_PATH):
                                     args.NPAGE)
 
 # генерация вступительной таблицы
-with open(INFO_FILENAME) as info:
+with open(args.I_PATH) as info:
     info_lst = info.read().split('\n')
-with open('{}/header_table_template.html'.format(TEMPLATE_PATH), 'r') as file:
+with open('{}/header_table_template.html'.format(args.T_PATH), 'r') as file:
     header_table_temp = file.read()
     header_table = header_table_temp.format(av=info_lst[0],
                                             kernel=info_lst[1],
@@ -160,19 +174,19 @@ with open('{}/header_table_template.html'.format(TEMPLATE_PATH), 'r') as file:
 
 
 # создание страницы отчета
-with open('{}/rating_template.html'.format(TEMPLATE_PATH), 'r') as template:
+with open('{}/rating_template.html'.format(args.T_PATH), 'r') as template:
     rating_temp = template.read()
     if args.TS == 'fs_mark_count':
-        rep = Report(ox_lo_lim=FILES, ox_step=FILES_STEP, ox_up_lim=FILES_LIMIT)
+        rep = Report(ox_lo_lim=FILES, ox_step=FILES_STEP, ox_up_lim=FILES_LIMIT, report=args.R_PATH)
         rating = rating_temp.format(r=str(rep.get_total_rating(rep.file_count_lst)))
     if args.TS == 'fs_mark_size':
-        rep = Report(ox_lo_lim=SIZE, ox_step=SIZE_STEP, ox_up_lim=SIZE_LIMIT)
+        rep = Report(ox_lo_lim=SIZE, ox_step=SIZE_STEP, ox_up_lim=SIZE_LIMIT, report=args.R_PATH)
         rating = rating_temp.format(r=str(rep.get_total_rating(rep.file_size_lst)))
 
-with open('{}/cfs_report_table.html'.format(REPORT_DIR), 'r') as file:
+with open('{}/cfs_report_table.html'.format(args.R_PATH), 'r') as file:
     main_table = file.read()
 
-with open('{}/img_template.html'.format(TEMPLATE_PATH), 'r') as template:
+with open('{}/img_template.html'.format(args.T_PATH), 'r') as template:
     images_lst = []
     img_temp = template.read()
     for file in os.listdir(args.R_PATH):
@@ -188,7 +202,7 @@ html_page = '\n'.join([header_table, rating, main_table, images])
 confluence_report.update_confluence_page(args.SPACE, args.NPAGE, html_page)
 
 if args.JIRA_ISSUE:
-    with open('{}/issue_comment_template.txt'.format(TEMPLATE_PATH), 'r') as template:
+    with open('{}/issue_comment_template.txt'.format(args.T_PATH), 'r') as template:
         comment = template.read()
         jira_report.add_comment_to_issue(args.JIRA_ISSUE,
                                          comment.format(url=confluence_report.get_confluence_public_url(args.SPACE,

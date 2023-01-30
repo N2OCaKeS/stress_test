44c65113da8777181624811d8a9fd9b470915a50
Роман Кузнецов <rkuznetsov@astralinux.ru>
2023-01-30 13:30:52 +0300

    * added get_stat script

---
+++ libs/get_stat.py
@@ -0,0 +1,246 @@
+import numpy as np
+import argparse
+import pandas
+
+from libreport import ConfluencePage
+from pretty_html_table import build_table
+from bs4 import BeautifulSoup
+
+DESCRIPTION = ""
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument('-u', '--username',
+                    action='store',
+                    required=True,
+                    help='confluence user',
+                    dest='USER')
+
+parser.add_argument('-t', '--token',
+                    action='store',
+                    required=False,
+                    default=None,
+                    help='confluence access token',
+                    dest='TOKEN')
+
+parser.add_argument('-c', '--component',
+                    action='store',
+                    choices=['fs', 'ps', 'sc'],
+                    required=True,
+                    help='system component',
+                    dest='COMPONENT')
+
+parser.add_argument('-sth', '--save-to-html',
+                    action='store_true',
+                    required=False,
+                    help='save table in beauty html',
+                    dest='SAVE_TO_HTML')
+
+parser.add_argument('-co', '--console-output',
+                    action='store_true',
+                    required=False,
+                    help='output stat info in console',
+                    dest='OUTPUT_TO_CONSOLE')
+
+args = parser.parse_args()
+
+
+def get_statistics(statistical_sampling_lst: list,
+                   sampling_name='nameless',
+                   save_to_html=True,
+                   output_to_console=True,
+                   accuracy=2) -> dict:
+
+    print('\033[92m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\033[0m')
+    print(statistical_sampling_lst)
+
+    statistics = {
+        'min': min(statistical_sampling_lst),
+        'max': max(statistical_sampling_lst),
+        'mean': round(np.mean(statistical_sampling_lst), accuracy),
+        'median': round(np.median(statistical_sampling_lst), accuracy),
+        'std': round(np.std(statistical_sampling_lst), accuracy),
+        'var': round(np.var(statistical_sampling_lst), accuracy),
+        'smin': round(1 - min(statistical_sampling_lst) / np.mean(statistical_sampling_lst), accuracy),
+        'smax': round(max(statistical_sampling_lst) / np.mean(statistical_sampling_lst) - 1, accuracy),
+    }
+
+    # raw_stat_table = pandas.DataFrame(statistics, index=[0])
+    # print(raw_stat_table)
+    raw_stat_table = pandas.DataFrame({'Оценка': ('MIN',
+                                                  'MAX',
+                                                  'Мат. ожидание',
+                                                  'Медиана',
+                                                  'Стандартное отклонение',
+                                                  'Дисперсия',
+                                                  'Отклонение MIN от среднего',
+                                                  'Отклонение MAX от среднего'),
+                                       sampling_name: (statistics['min'],
+                                                       statistics['max'],
+                                                       statistics['mean'],
+                                                       statistics['median'],
+                                                       statistics['std'],
+                                                       statistics['var'],
+                                                       statistics['smin'],
+                                                       statistics['smax'])
+                                       })
+    if save_to_html:
+        beauty_table = build_table(raw_stat_table, 'blue_light')
+        with open('table.html', 'w') as beauty_html_table:
+            beauty_html_table.write(beauty_table).decode
+
+    if output_to_console:
+        print('{} MIN: {}'.format(sampling_name, str(statistics['min'])))
+        print('{} MAX: {}'.format(sampling_name, str(statistics['max'])))
+        print('{} Мат. ожидание: {}'.format(sampling_name, str(statistics['mean'])))
+        print('{} Медиана: {}'.format(sampling_name, str(statistics['median'])))
+        print('{} Стандартное отклонение: {}'.format(sampling_name, str(statistics['std'])))
+        print('{} Дисперсия: {}'.format(sampling_name, str(statistics['var'])))
+        print('{} Отклонение MIN от среднего: {}'.format(sampling_name, str(statistics['smin'])))
+        print('{} Отклонение MAX от среднего: {}'.format(sampling_name, str(statistics['smax'])))
+
+    return statistics, raw_stat_table
+
+
+if args.COMPONENT == 'fs':
+    cp = ConfluencePage(username=args.USER, token=args.TOKEN)
+    src_html = cp.get_page_as_html('DD', 'Общая статистика. Файловые системы.')
+    # print(src_html)
+
+    # ищем все таблицы
+    soup = BeautifulSoup(src_html, 'lxml')
+    tables = soup.find_all(name='table')
+
+    # записываем в переменные с человеческими названиями
+    non_clustered_file_systems_table = tables[0]
+    non_clustered_file_systems_stat_table = tables[1]
+    clustered_file_systems_table = tables[2]
+    clustered_file_systems_stat_table = tables[3]
+
+    ####################################################################################################################
+    # ищем в первой таблице все значения rating
+    ratings = []
+    for link in non_clustered_file_systems_table.find_all(name='a'):
+        ratings.append(float(link.get_text()))
+
+    # сортируем относительно ФС
+    non_clustered_file_systems_ratings = {
+        'ratings_ext4': [rating for rating in ratings[0::4]],
+        'ratings_xfs': [rating for rating in ratings[1::4]],
+        'ratings_ntfs': [rating for rating in ratings[2::4]],
+        'ratings_ext4_parsec': [rating for rating in ratings[3::4]],
+    }
+
+    # генерим статистику
+    statistic_ext4 = get_statistics(non_clustered_file_systems_ratings['ratings_ext4'],
+                                    'ext4',
+                                    args.SAVE_TO_HTML,
+                                    args.OUTPUT_TO_CONSOLE)
+    statistic_xfs = get_statistics(non_clustered_file_systems_ratings['ratings_xfs'],
+                                   'xfs',
+                                   args.SAVE_TO_HTML,
+                                   args.OUTPUT_TO_CONSOLE)
+    statistic_ntfs = get_statistics(non_clustered_file_systems_ratings['ratings_ntfs'],
+                                    'ntfs',
+                                    args.SAVE_TO_HTML,
+                                    args.OUTPUT_TO_CONSOLE)
+    statistic_ext4_parsec = get_statistics(non_clustered_file_systems_ratings['ratings_ext4_parsec'],
+                                           'ext4_parsec',
+                                           args.SAVE_TO_HTML,
+                                           args.OUTPUT_TO_CONSOLE)
+    ####################################################################################################################
+    ratings = []
+    for link in clustered_file_systems_table.find_all(name='a'):
+        ratings.append(float(link.get_text()))
+
+    # сортируем относительно версии
+    non_clustered_file_systems_ratings = {
+        'ratings_ocfs2': [rating for rating in ratings[0::2]],
+        'ratings_ocfs2_parsec': [rating for rating in ratings[1::2]],
+    }
+
+    statistic_ext4 = get_statistics(non_clustered_file_systems_ratings['ratings_ocfs2'],
+                                    'ocfs2',
+                                    args.SAVE_TO_HTML,
+                                    args.OUTPUT_TO_CONSOLE)
+    statistic_ext4_parsec = get_statistics(non_clustered_file_systems_ratings['ratings_ocfs2_parsec'],
+                                           'ocfs2_parsec',
+                                           args.SAVE_TO_HTML,
+                                           args.OUTPUT_TO_CONSOLE)
+    ####################################################################################################################
+elif args.COMPONENT == 'ps':
+    cp = ConfluencePage(username=args.USER, token=args.TOKEN)
+    src_html = cp.get_page_as_html('DD', 'Общая статистика. PostgreSQL.')
+
+    # ищем все таблицы
+    soup = BeautifulSoup(src_html, 'lxml')
+    tables = soup.find_all(name='table')
+
+    # записываем в переменные с человеческими названиями
+    postgresql_table = tables[0]
+    postgresql_stat_table = tables[1]
+
+    # ищем в таблице все значения rating
+    ratings = []
+    for link in postgresql_table(name='a'):
+        ratings.append(float(link.get_text()))
+
+    statistic_postgresql_11 = get_statistics(ratings,
+                                             'PSQL11',
+                                             args.SAVE_TO_HTML,
+                                             args.OUTPUT_TO_CONSOLE)
+
+elif args.COMPONENT == 'sc':
+    cp = ConfluencePage(username=args.USER, token=args.TOKEN)
+    src_html = cp.get_page_as_html('DD', 'Общая статистика. Системные службы.')
+
+    # ищем все таблицы
+    soup = BeautifulSoup(src_html, 'lxml')
+    tables = soup.find_all(name='table')
+
+    # записываем в переменные с человеческими названиями
+    syslog_table = tables[0]
+    syslog_stat_table = tables[1]
+    auditd_table = tables[2]
+    auditd_stat_table = tables[3]
+
+    ####################################################################################################################
+    # ищем в первой таблице Syslog-NG все значения rating
+    ratings = []
+    for link in syslog_table.find_all(name='a'):
+        ratings.append(float(link.get_text()))
+
+    statistic_syslog_ng = get_statistics(ratings,
+                                         'Syslog-NG',
+                                         args.SAVE_TO_HTML,
+                                         args.OUTPUT_TO_CONSOLE)
+    ####################################################################################################################
+    # ищем в первой таблице Auditd все значения rating
+    ratings = []
+    for link in auditd_table.find_all(name='a'):
+        ratings.append(float(link.get_text()))
+
+    # сортируем относительно режима
+    auditd_ratings = {
+        'ratings_fileaud': [rating for rating in ratings[0::3]],
+        'ratings_psaud': [rating for rating in ratings[1::3]],
+        'ratings_useraud': [rating for rating in ratings[2::3]],
+        'ratings_total': [rating for rating in ratings],
+    }
+
+    statistic_auditd_fileaud = get_statistics(auditd_ratings['ratings_fileaud'],
+                                              'fileaud',
+                                              args.SAVE_TO_HTML,
+                                              args.OUTPUT_TO_CONSOLE)
+    statistic_auditd_psaud = get_statistics(auditd_ratings['ratings_psaud'],
+                                            'psaud',
+                                            args.SAVE_TO_HTML,
+                                            args.OUTPUT_TO_CONSOLE)
+    statistic_auditd_useraud = get_statistics(auditd_ratings['ratings_useraud'],
+                                              'useraud',
+                                              args.SAVE_TO_HTML,
+                                              args.OUTPUT_TO_CONSOLE)
+    statistic_auditd_total = get_statistics(auditd_ratings['ratings_total'],
+                                            'auditd total',
+                                            args.SAVE_TO_HTML,
+                                            args.OUTPUT_TO_CONSOLE)
+else:
+    exit(1)

e044354822000b4212bdc1fef96ab984c9203ec8
dtimonin <dtimonin@astralinux.ru>
2023-05-11 16:10:53 +0300

    fix install rows

--- postgresql/prepare.sh
+++ postgresql/prepare.sh
@@ -2,7 +2,8 @@
 
 # create venv in script_dir
 sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev \
-linux-tools-5.10*-generic linux-tools-5.15*-generic linux-tools-5.15*-lowlatency linux-tools-common-5.15*
+linux-tools-5.10*-generic linux-tools-5.15*-generic linux-tools-common-5.15*
+sudo apt-get install -y linux-tools-5.15*-lowlatency
 python3 -m venv venv
 
 # install python dependencies in venv

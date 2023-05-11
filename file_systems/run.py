ee438aee03f152508a543ce04d5e0b7dc8af5f90
dtimonin <dtimonin@astralinux.ru>
2023-05-11 16:37:02 +0300

    Change file name

--- file_systems/run.py
+++ file_systems/run.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-with open('/home/u/dates.txt', 'r') as r:
+with open('/home/u/dates.conf', 'r') as r:
     dates = r.read()
 
 subprocess.run(f'sudo venv/bin/python3 fsb_run.py {dates}', shell=True)

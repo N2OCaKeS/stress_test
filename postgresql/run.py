04bd5ce6c1d270cc4bd1f6d0f4496de8b64c600b
dtimonin <dtimonin@astralinux.ru>
2023-05-17 11:19:09 +0300

    add db del before runnig test

--- postgresql/run.py
+++ postgresql/run.py
@@ -5,4 +5,5 @@ import subprocess
 with open('/home/u/dates.conf', 'r') as r:
     dates = r.read()
 
+subprocess.run('sudo bash psb_db_del.sh', shell=True)
 subprocess.run(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}', shell=True)

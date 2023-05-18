68d2c82055d7cddd39bfde3aa82c2c114702743a
dtimonin <dtimonin@astralinux.ru>
2023-05-18 15:25:46 +0300

    fix

--- bendiks.py
+++ bendiks.py
@@ -5,5 +5,5 @@ import subprocess
 
 
 
-subprocess.run('./backup_image.py -sn 1 -rs 1.7.4 -test XFS -mode orel -kn 5.15.0-70-generic -stand stand1 -tcyc 1.7.4_orel_5.15.0-70-generic_stand1 -tcas file "system benchmark. XFS" -branch file_systems -cti 2773', shell=True)
+subprocess.run('./backup_image.py -sn 1 -rs 1.7.4 -test XFS -mode orel -kn 5.15.0-70-generic -stand stand1 -tcyc 1.7.4_orel_5.15.0-70-generic_stand1 -tcas "file system benchmark. XFS" -branch file_systems -cti 2773', shell=True)
 

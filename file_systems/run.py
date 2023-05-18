32dd5dcf7a3edbcdf2505f7157c4dd72c07f7de3
dtimonin <dtimonin@astralinux.ru>
2023-05-18 14:23:11 +0300

    fix path

--- file_systems/run.py
+++ file_systems/run.py
@@ -11,7 +11,7 @@ parser.add_argument('-n',
                     dest='NAME')
 args = parser.parse_args()
 
-with open(f'/home/u/{args.NAME}', 'r') as r:
+with open(f'/home/u/{args.NAME}.conf', 'r') as r:
     dates = r.read()
 
 subprocess.run(f'sudo venv/bin/python3 run.py {dates}', shell=True)

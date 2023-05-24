64898c4d38a798e748d7dba31538a6f71051c351
dtimonin <dtimonin@astralinux.ru>
2023-05-24 13:31:17 +0300

    fix commands

--- postgresql/libs/libpsb.py
+++ postgresql/libs/libpsb.py
@@ -169,13 +169,13 @@ def get_memory_load_by_psql():
 
 
 def perf():
-    logging.info(check_output_command('perf script > out.perf1', shell=True, check=True))
-    logging.info(check_output_command('perl libs/libstackcollapse-perf.pl out.perf1 > out.folded1', shell=True, check=True))
-    logging.info(check_output_command(f'perl libs/libflamegraph.pl out.folded1 > {REPORT_PATH}/result_flamegraph.svg', shell=True, check=True))
+    logging.info(check_output_command('perf script > out.perf1'))
+    logging.info(check_output_command('perl libs/libstackcollapse-perf.pl out.perf1 > out.folded1'))
+    logging.info(check_output_command(f'perl libs/libflamegraph.pl out.folded1 > {REPORT_PATH}/result_flamegraph.svg'))
 
 
 def dump():
-    logging.info(check_output_command(f'pg_dump -U postgres -d postgres -F tar -f {REPORT_PATH}/dump_db.tar', shell=True, check=True))
+    logging.info(check_output_command(f'pg_dump -U postgres -d postgres -F tar -f {REPORT_PATH}/dump_db.tar'))
 
 
 def log_in(name, message):
--- postgresql/run.py
+++ postgresql/run.py
@@ -29,6 +29,6 @@ logging.basicConfig(
 with open(f'/home/u/{args.NAME}', 'r') as r:
     dates = r.read()
 
-logging.info(check_output_command('sudo bash psb_db_del.sh', shell=True))
-logging.info(check_output_command(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}', shell=True))
-logging.info(check_output_command(f'sudo venv/bin/python3 psb_public.py', shell=True))
+logging.info(check_output_command('sudo bash psb_db_del.sh'))
+logging.info(check_output_command(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}'))
+logging.info(check_output_command(f'sudo venv/bin/python3 psb_public.py'))

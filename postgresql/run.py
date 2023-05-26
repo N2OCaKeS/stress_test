68ca55325a167179502f7304eb8eebab8de67c79
dtimonin <dtimonin@astralinux.ru>
2023-05-26 13:21:54 +0300

    fix func

--- postgresql/run.py
+++ postgresql/run.py
@@ -5,7 +5,7 @@ import argparse
 import logging
 from psb_conf import LOG_FILENAME, REPORT_PATH, RUN_LOG
 from os import path, mkdir
-from libs.libpsb import check_output_command
+from libs.libpsb import check_output_command, logging
 from time import sleep
 from sys import exit
 
@@ -20,17 +20,9 @@ args = parser.parse_args()
 if not path.isdir(REPORT_PATH):
     mkdir(REPORT_PATH)
 
-logging.basicConfig(
-        filename=RUN_LOG, 
-        level=logging.INFO,
-        filemode='a',
-        format='%(asctime)s - %(levelname)s - %(name)s - %(funcName)s: %(lineno)d - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-)
-
 def holder_transaction(command):
     holder = 0
-    while holder < 2:
+    while holder < 1:
         try:
             subprocess.run(command, shell=True, check=True)
             break
@@ -41,16 +33,16 @@ def holder_transaction(command):
             logging.error(e)
             logging.error('-----' * 30)
             sleep(60)
-            if holder == 2:
+            if holder == 1:
                 logging.error('Скрипт остановлен так как истек период ожидания:', holder, 'попытки')
                 exit(2)
 
 with open(f'/home/u/{args.NAME}', 'r') as r:
     dates = r.read()
 
-logging.info(check_output_command('sudo bash psb_db_del.sh', out=True))
+logging.info(subprocess.run('sudo bash psb_db_del.sh', shell=True))
 
-logging.info(check_output_command(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}', out=True))
+logging.info(subprocess.run(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}', shell=True))
 sleep(3)
 holder_transaction('sudo venv/bin/python3 psb_public.py')
 

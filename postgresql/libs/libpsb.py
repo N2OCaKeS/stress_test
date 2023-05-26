5c92ad4eef403e7efda0bf51e526ff362e5dd4f4
dtimonin <dtimonin@astralinux.ru>
2023-05-26 16:45:57 +0300

    fix comm

--- postgresql/libs/libpsb.py
+++ postgresql/libs/libpsb.py
@@ -170,13 +170,13 @@ def get_memory_load_by_psql():
 
 def perf():
     logging.info(check_output_command('ls -lh'))
-    logging.info(check_output_command('sudo perf script -i perf.data > out.perf1', out=True))
-    logging.info(check_output_command('sudo perl libs/libstackcollapse-perf.pl out.perf1 > out.folded1', out=True))
-    logging.info(check_output_command(f'sudo perl libs/libflamegraph.pl out.folded1 > {REPORT_PATH}/result_flamegraph.svg', out=True))
+    logging.info(check_output_command('sudo perf script -i perf.data > out.perf1'))
+    logging.info(check_output_command('sudo perl libs/libstackcollapse-perf.pl out.perf1 > out.folded1'))
+    logging.info(check_output_command(f'sudo perl libs/libflamegraph.pl out.folded1 > {REPORT_PATH}/result_flamegraph.svg'))
 
 
 def dump():
-    logging.info(check_output_command(f'pg_dump -U postgres -d postgres -F tar -f {REPORT_PATH}/dump_db.tar', out=True))
+    logging.info(check_output_command(f'pg_dump -U postgres -d postgres -F tar -f {REPORT_PATH}/dump_db.tar'))
 
 
 def log_in(name, message):

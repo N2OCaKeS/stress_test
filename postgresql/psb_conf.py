b7139645976914fdf02ae8465c455e757b64b474
dtimonin <dtimonin@astralinux.ru>
2023-05-26 14:16:32 +0300

    backup conf

--- postgresql/psb_conf.py
+++ postgresql/psb_conf.py
@@ -65,10 +65,10 @@ LIMITE_THREADS = 100  # 10000
 '''
     Число клиентов
 '''
-CLIENTS = 1 #100
-CLIENTS_STEP = 1 #100
+CLIENTS = 100
+CLIENTS_STEP = 100
 STEP_RATIO_BY_CLIENTS = 1
-LIMITE_CLIENTS = 2 #500
+LIMITE_CLIENTS = 500
 '''
     Расширенный репозиторий
 '''

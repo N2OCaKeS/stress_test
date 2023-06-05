566c1543b4ba2757474c4ab56cb9edec6c88fea3
dtimonin <dtimonin@astralinux.ru>
2023-06-05 19:45:52 +0300

    add img

--- backup_image_command.py
+++ backup_image_command.py
@@ -18,6 +18,10 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-173UU2 sdb',
         '1.7.4':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.203" \
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-174-TEST sda sdb'
+        },
+    'stand2':{
+        '1.7.3.UU.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-173UU2 sdb'
         }
         
 }

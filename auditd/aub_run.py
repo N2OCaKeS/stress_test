0e5b0f8526eb572266a4db288f0bca2ae868b9ba
dtimonin <dtimonin@astralinux.ru>
2023-06-15 12:23:47 +0300

    mod func

--- auditd/aub_run.py
+++ auditd/aub_run.py
@@ -7,7 +7,7 @@
 
 import argparse
 import subprocess
-from libs.libtest import AuditdTestSet
+
 import os
 from time import time, strftime, gmtime, sleep, ctime
 from os import path, mkdir, listdir, remove
@@ -147,7 +147,7 @@ args = parser.parse_args()
 
 with open('libs/stand_number.txt', 'w') as wr:
     wr.write(args.STAND)
-
+from libs.libtest import AuditdTestSet
 
 def test_cycle_status_start():
     zefir = ZefirStatusAPI(folder_tree_id=args.FTI,

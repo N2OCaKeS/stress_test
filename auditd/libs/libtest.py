67371ed2016e6c6330b447105821a8aadd6d34fa
dtimonin <dtimonin@astralinux.ru>
2023-06-15 12:11:37 +0300

    mod func

--- auditd/libs/libtest.py
+++ auditd/libs/libtest.py
@@ -14,11 +14,9 @@ from aub_conf import PSAUD_PROC_BODYS, USERAUD_PROC_BODYS, FILEAUD_PROC_BODYS, T
 from libs.libaub import Auditd, CheckAusearch, Prepare, User, UnixUser, cmd
 from libs.libaub import killer_ps
 
-try:
-    with open('libs/stand_number.txt', 'r') as r:
-        stand_number = r.read()
-except Exception as e:
-    print('stand_number_error: ', e)
+with open('git/stress_test/auditd/libs/stand_number.txt', 'r') as r:
+    stand_number = r.read()
+
 
 class AuditdTest(Auditd, CheckAusearch):
 

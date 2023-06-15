2056dd6c0628f0aac8e12e61396066db81b07cab
dtimonin <dtimonin@astralinux.ru>
2023-06-15 16:30:22 +0300

    debug

--- auditd/libs/libtest.py
+++ auditd/libs/libtest.py
@@ -593,7 +593,7 @@ class AuditdTest(Auditd, CheckAusearch):
         manager = Manager()
         timers = manager.list([None]*count)
 
-        killer_ps(stand_number)
+        #killer_ps(stand_number)
 
         test_ps_lst = self._create_ps(syscall=audit_flag,
                                       func=self._template_ps_psaud_timer,

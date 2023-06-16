093ca310eeb32198dfb310a7a02253b51dd9eea7
dtimonin <dtimonin@astralinux.ru>
2023-06-16 15:45:38 +0300

    add tests

--- bendiks.py
+++ bendiks.py
@@ -37,7 +37,7 @@ __jira_token = tokens['jira_token']
 __pt_version = args.RELEASE
 #__stand = 'stand1'
 __stand = args.STAND
-__test_list = ['XFS', 'EXT4', 'NTFS', 'EXT4 parsec', 'postgresql', 'auditd_p']
+__test_list = ['XFS', 'EXT4', 'NTFS', 'EXT4 parsec', 'postgresql', 'auditd_p', 'auditd_u', 'auditd_f']
 
 #Делаем get запрос в jira
 matrix_url = f'''https://jira.astralinux.ru/rest/tests/1.0/reports/testresults/matrix/testrun?displayUnit=COUNT&epicJQL=&jql=&

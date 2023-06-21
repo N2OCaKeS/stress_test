911c1acccdb1e74d435f8d9a2cce21e4c241e838
dtimonin <dtimonin@astralinux.ru>
2023-06-21 15:55:25 +0300

    fix hook

--- auditd/aub_run.py
+++ auditd/aub_run.py
@@ -157,8 +157,8 @@ def test_cycle_status_start():
     zefir_table
 
 start_status = 0
-jira_start, life_start = response()
 while start_status == 0:
+    jira_start, life_start = response()
     try:
         if jira_start == 200 and life_start == 200:
             test_cycle_status_start()
@@ -476,8 +476,8 @@ def upload_result_status():
     #statisctics.update_statistics()
 
 end_status = 0
-jira_end, life_end = response()
 while end_status == 0:
+    jira_end, life_end = response()
     try:
         if jira_end == 200 and life_end == 200:
             upload_result_status()

dc310eb7d990ad2ce89d814b6f9d4aa0ee988eab
ivelikanov <ivelikanov@astralinux.ru>
2023-06-26 15:49:51 +0300

    DEVQA-2228: Добавлены диаграммы сравнения по ядрам

--- postgresql/libs/libstatistics.py
+++ postgresql/libs/libstatistics.py
@@ -229,6 +229,11 @@ class PSQLStatistics:
             Строим таблицу №1
         """
         temp_data_for_graph = {}
+        temp_data_kernel = {
+            '5.10': [],
+            '5.15-gen': [],
+            '5.15-ll': []
+        }
         for key, data in data_for_df.items():
             if len(data.get("data")) == 0:
                 continue
@@ -238,6 +243,15 @@ class PSQLStatistics:
             panda_series = df['rating_2']
             data_for_df[key]['rating'] = panda_series.tolist()
             df.insert(0, "№", [x for x in range(1, len(panda_series.tolist()) + 1, 1)])
+            
+            df_5_10 = df[df["Ядро"].str.contains('5.10', case=False)]
+            temp_data_kernel['5.10'].append(df_5_10)
+            df_5_15_gen = df[df["Ядро"].str.contains('5.15\S*generic', case=False, regex=True)]
+            temp_data_kernel["5.15-gen"].append(df_5_15_gen)
+            df_5_15_ll = df[df["Ядро"].str.contains('5.15\S*low', case=False, regex=True)]
+            temp_data_kernel["5.15-ll"].append(df_5_15_ll)
+
+            # print(df_5_15_gen)
 
             df = df.drop('rating_2', axis=1)
             # print(type(df['Релиз'] + "_" + df['Ядро']))
@@ -249,6 +263,7 @@ class PSQLStatistics:
             """
             statistics_table_html = df.to_html(escape=False, index=False)
             file_html = open(f"statistics/{name_html}_{key}_1.html", "w")
+            file_html.writelines('<h1><a href="https://life.astralinux.ru/pages/viewpage.action?pageId=192234259">Описание стендов нагрузочного тестирования</a></h1>')
             file_html.writelines(f"<h1>Сводная таблица результатов тестирования {key}</h1> {statistics_table_html}")
             # file_html.write(statistics_table_html)
             file_html.close()
@@ -325,6 +340,21 @@ class PSQLStatistics:
             fig.savefig(f"statistics/postresql_statistics_{key}.png")
             # plt.show()
             
+        for kernel, temp_data_frame in temp_data_kernel.items():
+            for data_kernel in temp_data_frame:
+                fig, ax = plt.subplots(figsize=(12.8, 7.2))
+                ax.bar(data_kernel['Релиз'], data_kernel['rating_2'], color="#a3d1cd")
+                # print()
+                ax.set_title(f"PostgreSQL. Сводная диаграмма сравнения по ядрам.\n{list(data_kernel['Стенд'])[0]} - {kernel}")
+                for i, val in enumerate(data_kernel['rating_2']):
+                    try:
+                        val = int(val)
+                    except ValueError:
+                        pass
+                    plt.text(i, val * 0.5, val, horizontalalignment='center', verticalalignment='bottom', fontdict={'fontweight':500})
+                fig.savefig(f"statistics/postresql_statistics_{list(data_kernel['Стенд'])[0]}_{kernel}.jpg")
+        
+        # print(sorted(os.listdir("statistics")))
 
     """
         Создаем итоговую html страницу для life
@@ -344,12 +374,13 @@ class PSQLStatistics:
             <span class="confluence-embedded-file-wrapper confluence-embedded-manual-size">
                 <img class="confluence-embedded-image" draggable="false" src="/download/attachments/{page_id}/{img_png}" data-image-src="/download/attachments/{page_id}/{img_png}" data-unresolved-comment-count="0" data-linked-resource-id="{page_id}" data-linked-resource-version="1" data-linked-resource-type="attachment" data-linked-resource-default-alias="{img_png}" data-base-url="https://life.astralinux.ru" data-linked-resource-content-type="image/png" data-linked-resource-container-id="{page_id}" data-linked-resource-container-version="6"></img>
             </span>
-            <br/>
-            <h1><a href="https://life.astralinux.ru/pages/viewpage.action?pageId=192234259">Описание стендов нагрузочного тестирования</a></h1>
         """
         image_list = []
         table_with_data_list = []
         table_with_mat_stat_list = []
+
+        kernel_image_list = [[], [], []]
+
 
         for file in sorted(os.listdir("statistics")):
             
@@ -367,14 +398,36 @@ class PSQLStatistics:
                 mat_stat_table = new_file_table.read()
                 new_file_table.close()
                 table_with_mat_stat_list.append(mat_stat_table)
+            if file.endswith("5.10.jpg"):
+                # print(file)
+                confluence_stat.attache_files(file=f'statistics/{file}', page_space="DD", page_title=f"Статистика. {type_stat}")
+                kernel_image_list[0].append(template_img.format(page_id=confluence_stat.get_confluence_page_id("DD", f"Статистика. {type_stat}"),
+                                                    img_png=file))
+            if file.endswith("5.15-gen.jpg"):
+                # print(file)
+                confluence_stat.attache_files(file=f'statistics/{file}', page_space="DD", page_title=f"Статистика. {type_stat}")
+                kernel_image_list[1].append(template_img.format(page_id=confluence_stat.get_confluence_page_id("DD", f"Статистика. {type_stat}"),
+                                                    img_png=file))
+            if file.endswith("5.15-ll.jpg"):
+                # print(file)
+                confluence_stat.attache_files(file=f'statistics/{file}', page_space="DD", page_title=f"Статистика. {type_stat}")
+                kernel_image_list[2].append(template_img.format(page_id=confluence_stat.get_confluence_page_id("DD", f"Статистика. {type_stat}"),
+                                                    img_png=file))
 
         html_list = []
 
+        # print(kernel_image_list)
+
         for ind, item in enumerate(table_with_data_list):
+            # print(ind)
             html_list.append(image_list[ind])
             # html_list.append("<hr>")
             html_list.append(item)
             html_list.append("<h1>Таблица основных статистических параметров.</h1>" + "<br/>" + table_with_mat_stat_list[ind])
+            
+            html_list.append(kernel_image_list[0][ind])
+            html_list.append(kernel_image_list[1][ind])
+            html_list.append(kernel_image_list[2][ind])
         
         html_page = "".join(html_list)
 

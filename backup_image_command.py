aea5cac5dca9c9117bc08e366adeed733f077473
dtimonin <dtimonin@astralinux.ru>
2023-06-26 13:29:51 +0300

    add img

--- backup_image_command.py
+++ backup_image_command.py
@@ -46,8 +46,18 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-171-new nvme0n1'
         },
     'stand2':{
+        '1.7.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-171-new nvme0n1',
+        '1.7.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-172-new nvme0n1',
+        '1.7.3':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-173-new nvme0n1',
+        '1.7.3.UU.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-173UU1-new nvme0n1',
         '1.7.3.UU.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
-                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-173UU2 sdb'
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-173UU2-new nvme0n1',
+        '1.7.4':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-174-new nvme0n1',
         }
         
 }

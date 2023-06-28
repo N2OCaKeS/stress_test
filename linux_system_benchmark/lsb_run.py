ccd48b2880f04a9fa1a44bcfc70513e38b10c3be
dtimonin <dtimonin@astralinux.ru>
2023-06-28 14:28:37 +0300

    add snooper

--- linux_system_benchmark/lsb_run.py
+++ linux_system_benchmark/lsb_run.py
@@ -7,7 +7,7 @@
 
 import argparse
 import subprocess
-
+import pysnooper
 from time import time
 from shutil import copy
 from os import path, chdir, listdir, remove, mkdir
@@ -43,122 +43,126 @@ parser.add_argument('-sn', '--stand-name',
 
 args = parser.parse_args()
 
-if args.MODE == 'default':
-    # определить текущую директрию
-    current_dir = path.dirname(path.realpath(__file__))
+@pysnooper.snoop()
+def main():
+    if args.MODE == 'default':
+        # определить текущую директрию
+        current_dir = path.dirname(path.realpath(__file__))
 
-    # Создать /report
-    if not path.exists(REPORT_DIR):
-        mkdir(REPORT_DIR, mode=0o755)
+        # Создать /report
+        if not path.exists(REPORT_DIR):
+            mkdir(REPORT_DIR, mode=0o755)
 
-    # Создать /log
-    if not path.exists(LOG_DIR):
-        mkdir(LOG_DIR, mode=0o755)
+        # Создать /log
+        if not path.exists(LOG_DIR):
+            mkdir(LOG_DIR, mode=0o755)
 
-    # собираем проект
-    chdir(current_dir + '/byte-unixbench-master/UnixBench/')
-    cmd('make')
-
-    # очистить файлы /results
-    try:
-        result_dir = current_dir + '/byte-unixbench-master/UnixBench/results/'
-        for file in listdir(result_dir):
-            remove(result_dir + file)
-    except FileNotFoundError:
-        pass
-
-    # Засечь время выполнения скрипта
-    start_time = time()
-
-    '''
-        stand1
-    '''
-    if args.STAND == 'stand1':  # итеративный проход stand1
-        parallel_processes = ['-c ' + str(proc) for proc in range(STAND1_LOWER_LIMIT, STAND1_UPPER_LIMIT, STAND1_STEP)]
-        cmd_parallel_processes = ' '.join(parallel_processes)
-
-        # запустить тест
+        # собираем проект
         chdir(current_dir + '/byte-unixbench-master/UnixBench/')
-        cmd('./Run ' + cmd_parallel_processes)
+        cmd('make')
 
-        # выгрузить результаты
-        upload_result(current_dir)
+        # очистить файлы /results
+        try:
+            result_dir = current_dir + '/byte-unixbench-master/UnixBench/results/'
+            for file in listdir(result_dir):
+                remove(result_dir + file)
+        except FileNotFoundError:
+            pass
 
-        # Собираем результаты
-        r = Report(STAND1_LOWER_LIMIT,
-                   STAND1_UPPER_LIMIT,
-                   STAND1_STEP)
-        r.create_all_graphs()
-        r.get_all_ratings()
-        r.create_tar()
+        # Засечь время выполнения скрипта
+        start_time = time()
 
-    '''
-        stand2
-    '''
-    if args.STAND == 'stand2':  # итеративный проход stand2
-        parallel_processes = ['-c ' + str(proc) for proc in range(STAND2_LOWER_LIMIT, STAND2_UPPER_LIMIT, STAND2_STEP)]
-        cmd_parallel_processes = ' '.join(parallel_processes)
+        '''
+            stand1
+        '''
+        if args.STAND == 'stand1':  # итеративный проход stand1
+            parallel_processes = ['-c ' + str(proc) for proc in range(STAND1_LOWER_LIMIT, STAND1_UPPER_LIMIT, STAND1_STEP)]
+            cmd_parallel_processes = ' '.join(parallel_processes)
 
-        # запустить тест
-        chdir(current_dir + '/byte-unixbench-master/UnixBench/')
-        cmd('./Run ' + cmd_parallel_processes)
+            # запустить тест
+            chdir(current_dir + '/byte-unixbench-master/UnixBench/')
+            cmd('./Run ' + cmd_parallel_processes)
 
-        # выгрузить результаты
-        upload_result(current_dir)
+            # выгрузить результаты
+            upload_result(current_dir)
 
-        # Собираем результаты
-        r = Report(STAND2_LOWER_LIMIT,
-                   STAND2_UPPER_LIMIT,
-                   STAND2_STEP)
-        r.create_all_graphs()
-        r.get_all_ratings()
-        r.create_tar()
+            # Собираем результаты
+            r = Report(STAND1_LOWER_LIMIT,
+                    STAND1_UPPER_LIMIT,
+                    STAND1_STEP)
+            r.create_all_graphs()
+            r.get_all_ratings()
+            r.create_tar()
 
-    '''
-        stand3
-    '''
-    if args.STAND == 'stand3':  # итеративный проход stand3
-        parallel_processes = ['-c ' + str(proc) for proc in range(STAND3_LOWER_LIMIT, STAND3_UPPER_LIMIT, STAND3_STEP)]
-        cmd_parallel_processes = ' '.join(parallel_processes)
+        '''
+            stand2
+        '''
+        if args.STAND == 'stand2':  # итеративный проход stand2
+            parallel_processes = ['-c ' + str(proc) for proc in range(STAND2_LOWER_LIMIT, STAND2_UPPER_LIMIT, STAND2_STEP)]
+            cmd_parallel_processes = ' '.join(parallel_processes)
 
-        # запустить тест
-        chdir(current_dir + '/byte-unixbench-master/UnixBench/')
-        cmd('./Run ' + cmd_parallel_processes)
+            # запустить тест
+            chdir(current_dir + '/byte-unixbench-master/UnixBench/')
+            cmd('./Run ' + cmd_parallel_processes)
 
-        # выгрузить результаты
-        upload_result(current_dir)
+            # выгрузить результаты
+            upload_result(current_dir)
 
-        # Собираем результаты
-        r = Report(STAND3_LOWER_LIMIT,
-                   STAND3_UPPER_LIMIT,
-                   STAND3_STEP)
-        r.create_all_graphs()
-        r.get_all_ratings()
-        r.create_tar()
+            # Собираем результаты
+            r = Report(STAND2_LOWER_LIMIT,
+                    STAND2_UPPER_LIMIT,
+                    STAND2_STEP)
+            r.create_all_graphs()
+            r.get_all_ratings()
+            r.create_tar()
 
-    '''
-        stand4
-    '''
-    if args.STAND == 'stand4':  # итеративный проход stand4
-        parallel_processes = ['-c ' + str(proc) for proc in range(STAND4_LOWER_LIMIT, STAND4_UPPER_LIMIT, STAND4_STEP)]
-        cmd_parallel_processes = ' '.join(parallel_processes)
+        '''
+            stand3
+        '''
+        if args.STAND == 'stand3':  # итеративный проход stand3
+            parallel_processes = ['-c ' + str(proc) for proc in range(STAND3_LOWER_LIMIT, STAND3_UPPER_LIMIT, STAND3_STEP)]
+            cmd_parallel_processes = ' '.join(parallel_processes)
 
-        # запустить тест
-        chdir(current_dir + '/byte-unixbench-master/UnixBench/')
-        cmd('./Run ' + cmd_parallel_processes)
+            # запустить тест
+            chdir(current_dir + '/byte-unixbench-master/UnixBench/')
+            cmd('./Run ' + cmd_parallel_processes)
 
-        # выгрузить результаты
-        upload_result(current_dir)
+            # выгрузить результаты
+            upload_result(current_dir)
 
-        # Собираем результаты
-        r = Report(STAND4_LOWER_LIMIT,
-                   STAND4_UPPER_LIMIT,
-                   STAND4_STEP)
-        r.create_all_graphs()
-        r.get_all_ratings()
-        r.create_tar()
+            # Собираем результаты
+            r = Report(STAND3_LOWER_LIMIT,
+                    STAND3_UPPER_LIMIT,
+                    STAND3_STEP)
+            r.create_all_graphs()
+            r.get_all_ratings()
+            r.create_tar()
 
-    put_system_info_in_file(start_time, current_dir+'/report/'+INFO_FILENAME)
+        '''
+            stand4
+        '''
+        if args.STAND == 'stand4':  # итеративный проход stand4
+            parallel_processes = ['-c ' + str(proc) for proc in range(STAND4_LOWER_LIMIT, STAND4_UPPER_LIMIT, STAND4_STEP)]
+            cmd_parallel_processes = ' '.join(parallel_processes)
 
-elif args.MODE == 'extended':
-    print("In developing")
+            # запустить тест
+            chdir(current_dir + '/byte-unixbench-master/UnixBench/')
+            cmd('./Run ' + cmd_parallel_processes)
+
+            # выгрузить результаты
+            upload_result(current_dir)
+
+            # Собираем результаты
+            r = Report(STAND4_LOWER_LIMIT,
+                    STAND4_UPPER_LIMIT,
+                    STAND4_STEP)
+            r.create_all_graphs()
+            r.get_all_ratings()
+            r.create_tar()
+
+        put_system_info_in_file(start_time, current_dir+'/report/'+INFO_FILENAME)
+
+    elif args.MODE == 'extended':
+        print("In developing")
+
+main()

DEVQA-2251: small fixes normalization

syslog_ng/libs/libtable.py
@@ -78,9 +78,10 @@ class Report:
         '''
             Получить рейтинг
         '''
-        y = y + [100]
+        added_value = 100
+        y_new = y.append(pd.Series(added_value))
         scaler = preprocessing.MinMaxScaler()
-        normalized_data_2d_array = scaler.fit_transform(np.array(y)[:, np.newaxis])
+        normalized_data_2d_array = scaler.fit_transform(np.array(y_new)[:, np.newaxis])
         normalized_data_list = [float(list(item)[0]) for item in list(normalized_data_2d_array[0:-1])]
         func = self.data_aproximation(x, normalized_data_list)
         I, err = integrate.quad(func, x[0], x[-1])
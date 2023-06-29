804e42d3b60f691ae370922aa74d4b4e7d913cb9
dtimonin <dtimonin@astralinux.ru>
2023-06-29 16:31:20 +0300

    debug brooken module

--- linux_system/lsb_prep.sh
+++ linux_system/lsb_prep.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # create venv in script_dir
-sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev python3-sklearn
-python3 -m venv venv
+sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev 
+sudo apt-get install -y python3-numpy python3-scipy python3-pandas python3-matplotlib
+#python3-sklearn
+#python3 -m venv venv
+sudo mkdir /home/u/modules
+sudo wget -P /home/u/modules ftp://10.177.103.10/modules/*
+sudo dpkg -i /home/u/modules/*.deb
 
 #sudo apt-get install -y python3-dev python3-requests libffi-dev syslog-ng python3-numpy python3-scipy python3-pandas
 #sudo apt-get install -y python3-matplotlib
 # install python dependencies in venv
-source venv/bin/activate
-pip install --upgrade pip
+#source venv/bin/activate
+python3 -m pip install --upgrade pip
 pip3 install -r req.txt

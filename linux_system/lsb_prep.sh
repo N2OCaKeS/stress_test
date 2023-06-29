dd7de0897becc3e1043096c8f730ea44c1a3d7b3
dtimonin <dtimonin@astralinux.ru>
2023-06-29 13:48:49 +0300

    add lib sklearn

--- linux_system/lsb_prep.sh
+++ linux_system/lsb_prep.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # create venv in script_dir
-sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev
+sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev python3-sklearn
 python3 -m venv venv
 
+#sudo apt-get install -y python3-dev python3-requests libffi-dev syslog-ng python3-numpy python3-scipy python3-pandas
+#sudo apt-get install -y python3-matplotlib
 # install python dependencies in venv
 source venv/bin/activate
 pip install --upgrade pip

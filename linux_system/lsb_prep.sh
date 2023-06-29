40a0947456932d9997e602c1c57120383ca185ec
dtimonin <dtimonin@astralinux.ru>
2023-06-29 17:38:02 +0300

    add space

--- linux_system/lsb_prep.sh
+++ linux_system/lsb_prep.sh
@@ -3,6 +3,7 @@
 # create venv in script_dir
 sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev 
 sudo apt-get install -y python3-numpy python3-scipy python3-pandas python3-matplotlib
+
 #python3-sklearn
 #python3 -m venv venv
 sudo mkdir /home/u/modules
@@ -10,8 +11,6 @@ sudo wget -P /home/u/modules ftp://10.177.103.10/modules/*
 sudo dpkg -i /home/u/modules/*.deb
 sudo apt install -fy
 
-#sudo apt-get install -y python3-dev python3-requests libffi-dev syslog-ng python3-numpy python3-scipy python3-pandas
-#sudo apt-get install -y python3-matplotlib
 # install python dependencies in venv
 #source venv/bin/activate
 python3 -m pip install --upgrade pip

1fe82966892d30ab2b19f3c34c62130b4fb03d78
dtimonin <dtimonin@astralinux.ru>
2023-06-30 11:33:42 +0300

    fix sklearn module

--- auditd/run.py
+++ auditd/run.py
@@ -14,4 +14,4 @@ args = parser.parse_args()
 with open(f'/home/u/{args.NAME}', 'r') as r:
     dates = r.read()
 
-subprocess.run(f'sudo venv/bin/python3 aub_run.py {dates}', shell=True)
+subprocess.run(f'sudo python3 aub_run.py {dates}', shell=True)

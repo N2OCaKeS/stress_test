4f0e30025e0001eab34cf4e1a26dd0bed3eb1864
dtimonin <dtimonin@astralinux.ru>
2023-06-30 15:27:11 +0300

    add run

---
+++ linux_system/run.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n',
+                    action='store',
+                    required=True,
+                    help='dates name',
+                    dest='NAME')
+args = parser.parse_args()
+
+with open(f'/home/u/{args.NAME}', 'r') as r:
+    dates = r.read()
+
+subprocess.run(f'sudo python3 lsb_run.py {dates}', shell=True)

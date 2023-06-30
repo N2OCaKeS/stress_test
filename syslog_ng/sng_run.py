55c62422bff108a2d09cf225a7d38f7dbe5c7c89
dtimonin <dtimonin@astralinux.ru>
2023-06-30 11:27:26 +0300

    del venv

--- syslog_ng/sng_run.py
+++ syslog_ng/sng_run.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
                 'Restart=always\n',
                 'WorkingDirectory={}/\n'.format(dir),
                 'OOMScoreAdjust = -100\n', # Prohibition on the use of the out-of-memory service and the OOM trigger mechanism
-                'ExecStart={}/venv/bin/python3 /tmp/dirtylogger{}.py\n'.format(dir, service_num),
+                'ExecStart={}/python3 /tmp/dirtylogger{}.py\n'.format(dir, service_num),
                 'TimeoutSec=1\n',
                 '[Install]\n',
                 'WantedBy = multi - user.target\n']

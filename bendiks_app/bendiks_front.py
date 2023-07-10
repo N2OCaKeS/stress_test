22a150fd2a459d4ee6c1735a881e5a07da07839c
dtimonin <dtimonin@astralinux.ru>
2023-07-10 16:13:47 +0300

    fix wsgi

--- bendiks_app/bendiks_front.py
+++ bendiks_app/bendiks_front.py
@@ -49,7 +49,7 @@ def login():
         # Обработка логики входа пользователя
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == 'u' and password == up:
+        if username == 'u' and password == '1':
             return redirect(f'/{main_url}')  # Перенаправление на главную страницу после успешного входа
         else:
             return render_template('login.html', error="Неверный логин или пароль")
--- bendiks_app/wsgi.py
+++ bendiks_app/wsgi.py
@@ -3,6 +3,9 @@ import sys
 import logging
 
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/home/u/bendiks_app")
-from bendiks_front import app as application
-application.secret_key = 'srv_2113'
+sys.path.insert(0,"/home/u/git/stress_test/bendiks_app")
+from bendiks_front import app
+app.secret_key = 'srv_2113'
+
+if __name__ == '__main__':
+    app.run()

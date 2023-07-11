b96ec26a8138d91bb644fb6a6b5c787dc414ec7a
dtimonin <dtimonin@astralinux.ru>
2023-07-11 19:08:39 +0300

    backup pip pandas

--- postgresql/prepare.sh
+++ postgresql/prepare.sh
@@ -2,9 +2,10 @@
 
 # create venv in script_dir
 sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev
-sudo apt-get install -y python3-numpy python3-scipy python3-pandas python3-matplotlib python3-lxml python3-bs4 python3-prettytable
+sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib python3-lxml python3-bs4 python3-prettytable
 sudo apt-get install -y linux-tools-5.10*-generic linux-tools-5.15*-generic linux-tools-common-5.15*
 sudo apt-get install -y linux-tools-5.15*-lowlatency
+#python3-pandas
 #python3 -m venv venv
 sudo mkdir /home/u/modules
 sudo wget -P /home/u/modules ftp://10.177.103.10/modules/*

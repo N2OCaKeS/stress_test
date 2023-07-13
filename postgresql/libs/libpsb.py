0e1f9ec63198085a83bac54d916e1ffbe6157959
ivelikanov <ivelikanov@astralinux.ru>
2023-07-13 17:35:04 +0300

    Добавлена возможность проверки версии для debian

--- postgresql/libs/libpsb.py
+++ postgresql/libs/libpsb.py
@@ -10,6 +10,7 @@ import subprocess
 import logging
 from sys import exit
 from os import chmod, remove, chdir, path, mkdir, linesep
+from os.path import exists
 from shutil import copy2
 import ftplib
 import requests
@@ -44,6 +45,10 @@ def cmd(command, err=subprocess.DEVNULL, out=subprocess.DEVNULL):
     subprocess.run(command, shell=True, stderr=err, stdout=out)
 
 
+def debian_version():
+
+
+
 def astra_version():
     version = []
     # astra_digit_version = subprocess.run("cat /etc/os-release | grep '^VERSION_ID'",
@@ -64,36 +69,41 @@ def astra_version():
     #     print("Version of distribution not found")
     #     exit(2)
 
-    with open("/etc/astra_version", "r") as file:
-        astra_update_version = file.read()
-    version.append(astra_update_version.strip('\n'))
-
-    try:
-        with open("/etc/astra_license", "r") as file:
-            astra_license = file.read()
-            if "orel" in astra_license:
-                version.append("orel")
-            elif "smolensk" in astra_license:
-                version.append("smolensk")
-            elif "voronezh" in astra_license:
-                version.append("voronezh")
-            else:
-                print("Version of distribution not found")
-                exit(2)
-    except IOError:
+    if exists("/etc/debian_version"):
+        with open("/etc/debian_version", "r") as file:
+            debian_version = file.read()
+        version.append(debian_version.strip('\n'))
+    else:
         with open("/etc/astra_version", "r") as file:
-            astra_version = file.read()
-            if "1.6" in astra_version:
-                version.append("smolensk")
-            elif "1.5" in astra_version:
-                version.append("smolensk")
-            elif "8.1" in astra_version:
-                version.append("smolensk")
-            elif "2.12" in astra_version:
-                version.append("orel")
-            else:
-                print("Version of distribution not found")
-                exit(2)
+            astra_update_version = file.read()
+        version.append(astra_update_version.strip('\n'))
+
+        try:
+            with open("/etc/astra_license", "r") as file:
+                astra_license = file.read()
+                if "orel" in astra_license:
+                    version.append("orel")
+                elif "smolensk" in astra_license:
+                    version.append("smolensk")
+                elif "voronezh" in astra_license:
+                    version.append("voronezh")
+                else:
+                    print("Version of distribution not found")
+                    exit(2)
+        except IOError:
+            with open("/etc/astra_version", "r") as file:
+                astra_version = file.read()
+                if "1.6" in astra_version:
+                    version.append("smolensk")
+                elif "1.5" in astra_version:
+                    version.append("smolensk")
+                elif "8.1" in astra_version:
+                    version.append("smolensk")
+                elif "2.12" in astra_version:
+                    version.append("orel")
+                else:
+                    print("Version of distribution not found")
+                    exit(2)
 
     return version
 

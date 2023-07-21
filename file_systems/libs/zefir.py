24b4e45d8d8e75e9df95b98d3559c68da04c3009
dtimonin <dtimonin@astralinux.ru>
2023-07-21 19:19:54 +0300

    fix

--- file_systems/libs/zefir.py
+++ file_systems/libs/zefir.py
@@ -367,7 +367,7 @@ class Zefir_result_table:
                             'file system benchmark. EXT4 parsec':'FS_EXT4_parsec', 'auditd benchmark. fileaud':'Auditd_fileaud',
                             'auditd benchmark. useraud':'Auditd_useraud', 'file system benchmark. OCFS2 parsec':'FS_OCFS2_parsec',
                             'postgresql benchmark smol':'PostgreSQL_smol', 'postgresql benchmark audit-off':'PSQL_audit-off',
-                            'storage drive overflow':'SD_overflow', 'storage drive overflow':'RAM_overflow'}
+                            'storage drive overflow':'SD_overflow', 'ram overflow':'RAM_overflow'}
         for k, v in testname_columns.items():
             self.new_tab.rename(columns={k:v}, inplace=True)
         for name in self.new_tab.columns:

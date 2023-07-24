415d2e6ccd580ba158e3efe71c949596a67d894a
dtimonin <dtimonin@astralinux.ru>
2023-07-24 15:02:24 +0300

    add pip prep

--- overflow/prepare.sh
+++ overflow/prepare.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev
+sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib
 
+python3 -m pip install --upgrade pip
+python3 -m pip install -r req.txt

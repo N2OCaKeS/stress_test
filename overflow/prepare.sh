b93fca86e28f9ea3bf08834795fda1f643f523d0
dtimonin <dtimonin@astralinux.ru>
2023-07-24 18:28:55 +0300

    add time

--- overflow/prepare.sh
+++ overflow/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev
-sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib
+sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib htop
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r req.txt

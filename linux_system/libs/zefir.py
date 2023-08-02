e6416bca60c67a4b3a684750f191582f8be8b21b
dtimonin <dtimonin@astralinux.ru>
2023-08-02 20:10:56 +0300

    fix log exc

--- linux_system/libs/zefir.py
+++ linux_system/libs/zefir.py
@@ -108,7 +108,7 @@ class UploaderZC(Public, UnixBenchStatistics):
                 with open('JIRA_ERROR.log', 'a') as err:
                     err.write('start:\n')
                     err.write(ctime())
-                    err.write(str(e))
+                    err.write(f'Type: {type(e).__name__}, Message: {str(e)}')
                     err.write('---------' * 25)
                     err.write('\n\n')
                     except_counter += 1

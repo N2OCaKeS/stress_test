be6d233eb9e62a494e11f3c9bf2248350093f9da
dtimonin <dtimonin@astralinux.ru>
2023-08-03 11:16:38 +0300

    upd func

--- syslog_ng/libs/zefir.py
+++ syslog_ng/libs/zefir.py
@@ -67,6 +67,12 @@ class UploaderZC(Public, FileSystemStatistics):
                                test_cycle_name=self.TCYC,
                                test_case_name=self.TCAS,
                                basic_auth=self.BA)
+
+        if self.statistics == True:
+            statistics = FileSystemStatistics(username=self.UN, 
+                                              token=self.CT)
+            statistics.update_statistics()
+
         if status == 'pass':
             status_code = 91
         elif status == 'fail':
@@ -79,12 +85,7 @@ class UploaderZC(Public, FileSystemStatistics):
                                        basic_auth=self.BA,
                                        username=self.UN)
         zefir_table
-
-        if self.statistics == True:
-            statistics = FileSystemStatistics(username=self.UN, 
-                                              token=self.CT)
-            statistics.update_statistics()
-
+        return 0
 
     def upload_test_cycle_status(self, zefir_status):
         wait_time = 30 #Минут ожидания
@@ -95,12 +96,12 @@ class UploaderZC(Public, FileSystemStatistics):
             jira, life = response()
             try:
                 if jira == 200 and life == 200:
-                    self.test_cycle_status_changer(zefir_status)
-                    status += 1
+                    if self.test_cycle_status_changer(zefir_status) == 0:
+                        status += 1
                 else: 
                     with open('JIRA_ERROR.log', 'a') as err:
                         err.write('start:\n')
-                        err.write(ctime())
+                        err.write(str(ctime()) + '\n')
                         err.write(f'jira_status = {jira}\nlife_status = {life}')
                         err.write('---------' * 25)
                         err.write('\n\n')
@@ -108,7 +109,7 @@ class UploaderZC(Public, FileSystemStatistics):
             except Exception as e:
                 with open('JIRA_ERROR.log', 'a') as err:
                     err.write('start:\n')
-                    err.write(ctime())
+                    err.write(str(ctime()) + '\n')
                     err.write(f'Type: {type(e).__name__}, Message: {str(e)}')
                     err.write('---------' * 25)
                     err.write('\n\n')

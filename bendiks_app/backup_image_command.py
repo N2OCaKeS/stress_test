816730b41cebcbc1ba44716d920ead9116e907d1
dtimonin <dtimonin@astralinux.ru>
2023-08-23 20:22:29 +0300

    add clonz 1.7.5

--- bendiks_app/backup_image_command.py
+++ bendiks_app/backup_image_command.py
@@ -10,6 +10,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-debian10 nvme0n1',
         'debian10-5.15':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.201" \
                 -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-debian10-5.15.10 nvme0n1',
+        '1.7.5':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.201" \
+                -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-1.7.5.rc2 nvme0n1',
         '1.7.4.UU.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.201" \
                 -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-174UU1 nvme0n1',
         '1.7.4':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.201" \

b7d774c5fa4cab86e916196d18be61da24425071
dtimonin <dtimonin@astralinux.ru>
2023-09-14 18:46:20 +0300

    replace sett and descript conf

--- bendiks_app/libs/libbend.py
+++ bendiks_app/libs/libbend.py
@@ -16,24 +16,20 @@ from paramiko import ssh_exception
 import socket
 import psycopg2
 from backup_image_conf import (psyc,
-                               stands_ip)
+                               stands_ip,
+                               main_tests,
+                               brest_tests,
+                               releases,
+                               kernels,
+                               main_stands,
+                               mobile_stands,
+                               brest_stands)
 from time import sleep
 
 
 
-
-main_options = sorted(['XFS', 'EXT4', 'NTFS', 'EXT4 parsec', 'postgresql', 'postgresql-sm', 
-                  'auditd-p', 'auditd-u', 'auditd-f', 'syslog-ng', 'unix', 'postgresql-aud-off', 'SD-overflow', 'RAM-overflow'])
-brest_options = ['apache-graph']
-
-releases = ['1.7.1', '1.7.2', '1.7.3', '1.7.3.UU.1', '1.7.3.UU.2', '1.7.4', '1.7.4.UU.1', '1.7.5']
-kernels = ['5.10.0-1057-generic', '5.10.142-1-generic', '5.15.0-33-generic', '5.15.0-33-lowlatency', 
-           '5.10.176-1-generic', '5.15.0-70-generic', '5.15.0-70-lowlatency', '5.10.190-1-generic',
-           '6.1.29-1-generic']
-
-main_stands = ['stand1', 'stand2', 'stand3', 'stand4']
-mobile_stands = ['stand1', 'stand2', 'stand3', 'stand4']
-brest_stands = ['stand10', 'stand11', 'stand12']
+main_options = sorted(main_tests)
+brest_options = sorted(brest_tests)
 
 #main_url = generate_random_string(60)
 red_gif = 'http://10.177.103.10:8000/static/red_ring_64.gif'

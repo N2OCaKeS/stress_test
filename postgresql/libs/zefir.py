6cd57d311ccc68126efbef7871f3a75778f0c945
dtimonin <dtimonin@astralinux.ru>
2023-09-19 12:58:02 +0300

    add func to build with RC

--- postgresql/libs/zefir.py
+++ postgresql/libs/zefir.py
@@ -393,10 +393,19 @@ class ZefirResultTable:
         self.__basic = basic_auth
         self.__pt_version = test_cycle_version
 
+        check_len_version = self.__pt_version.split('.')
+        if len(check_len_version) == 4:
+            release_version = '.'.join(check_len_version[:3])
+            rc_version = self.__pt_version
+            filter_url = f"'%2Fstress_test%2F{release_version}%2F{self.__pt_version}%2F**'"
+        elif len(check_len_version) == 3:
+            filter_url = f'%27%2Fstress_test%27,%27%2Fstress_test%2F{self.__pt_version}%27'
+
+
         #Делаем get запрос в jira
         matrix_url = f'''https://jira.astralinux.ru/rest/tests/1.0/reports/testresults/matrix/testrun?displayUnit=COUNT&epicJQL=&jql=&
                         period=MONTH&projectId=11200&scorecardOption=EXECUTION_RESULTS&tql=testResult.projectId+IN+(11200)+AND+testRun.
-                        folderName+IN+(%27%2Fstress_test%27,%27%2Fstress_test%2F{self.__pt_version}%27)&traceabilityCustomTreeDisplayOption=
+                        folderName+IN+({filter_url})&traceabilityCustomTreeDisplayOption=
                         CONDENSED&traceabilityMatrixOption=COVERAGE_TEST_CASES&traceabilityReportOption=COVERAGE_TEST_CASES&traceability
                         TreeOption=COVERAGE_TEST_CASES
                         '''
@@ -526,18 +535,48 @@ class ZefirResultTable:
         #print(table)
 
 
-        def upload_page(space, title, name_page, body):
-            if not confluence.page_exists(space=space,
-                                        title=name_page):
-                confluence.create_page(space=space,
-                                    parent_id=confluence.get_page_id(space=space,
-                                                                        title=title),
-                                    title=name_page,
-                                    body=body)
-            else: confluence.update_page(page_id=confluence.get_page_id(space=space,
-                                                                        title=name_page),
-                                        title=name_page,
-                                        body=body)
+        def upload_page(space, 
+                        title, 
+                        name_page:str, 
+                        body):
+            check_len_version = name_page.split('.')
+            if len(check_len_version) == 4:
+                release_version = '.'.join(check_len_version[:3])
+                rc_version = self.__pt_version
+                if not confluence.page_exists(space=space,
+                                             title=release_version):
+                    confluence.create_page(space=space,
+                                           parent_id=confluence.get_page_id(space=space,
+                                                                            title=title),
+                                           title=release_version,
+                                           body=body)
+                else: confluence.update_page(page_id=confluence.get_page_id(space=space,
+                                                                            title=release_version),
+                                            title=release_version,
+                                            body=body)
+                if not confluence.page_exists(space=space,
+                                             title=rc_version):
+                    confluence.create_page(space=space,
+                                           parent_id=confluence.get_page_id(space=space,
+                                                                            title=release_version),
+                                           title=rc_version,
+                                           body=body)
+                else: confluence.update_page(page_id=confluence.get_page_id(space=space,
+                                                                            title=rc_version),
+                                            title=rc_version,
+                                            body=body)
+            elif len(check_len_version) == 3:
+                if not confluence.page_exists(space=space,
+                                             title=name_page):
+                    confluence.create_page(space=space,
+                                           parent_id=confluence.get_page_id(space=space,
+                                                                            title=title),
+                                           title=name_page,
+                                           body=body)
+                else: confluence.update_page(page_id=confluence.get_page_id(space=space,
+                                                                            title=name_page),
+                                            title=name_page,
+                                            body=body)
 
         upload_page('DD', 'Состав тестового прогона', self.__pt_version, table)
 

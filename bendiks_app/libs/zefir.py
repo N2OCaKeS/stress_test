8464d4e83d76dd57b1bd8f91bf450ee05ed6e7f7
dtimonin <dtimonin@astralinux.ru>
2023-09-19 13:50:37 +0300

    fix body err

--- bendiks_app/libs/zefir.py
+++ bendiks_app/libs/zefir.py
@@ -385,11 +385,7 @@ class ZefirResultTable:
                                            parent_id=confluence.get_page_id(space=space,
                                                                             title=title),
                                            title=release_version,
-                                           body=body)
-                else: confluence.update_page(page_id=confluence.get_page_id(space=space,
-                                                                            title=release_version),
-                                            title=release_version,
-                                            body=body)
+                                           body='')
                 if not confluence.page_exists(space=space,
                                              title=rc_version):
                     confluence.create_page(space=space,

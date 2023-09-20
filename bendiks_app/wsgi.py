5870311cf96692f48a86b49ebfb66aad56795754
dtimonin <dtimonin@astralinux.ru>
2023-09-20 14:02:26 +0300

    rebuild logg

--- bendiks_app/wsgi.py
+++ bendiks_app/wsgi.py
@@ -2,30 +2,14 @@
 import sys
 import logging
 
-
-log_file = '/home/u/git/stress_test/bendiks_app/error.log'
-sys.path.insert(0,'/home/u/git/stress_test/bendiks_app')
-
-
-class MyHandler(logging.StreamHandler):
-
-    def emit(self, record):
-        self.stream.write('\n\n\n\n' + '='*50 + '\n')
-        super().emit(record)
-
-
-handler = MyHandler(open(log_file, 'a'))
-handler.setFormatter(logging.Formatter('%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S'))
-
-
-logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
-logger.addHandler(handler)
+logging.basicConfig(filename='/home/u/git/stress_test/bendiks_app/error.log',
+                    format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S',
+                    filemode='a')
+sys.path.insert(0,"/home/u/git/stress_test/bendiks_app")
 
 from bendiks_front import app
 app.secret_key = 'srv_2113'
 
 if __name__ == '__main__':
     app.run()
-
-

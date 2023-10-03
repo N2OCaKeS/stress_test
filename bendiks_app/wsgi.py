885ee782fb44948a5be4a908e7efcc22978c9472
dtimonin <dtimonin@astralinux.ru>
2023-10-03 18:44:22 +0300

    rebuild logging format

--- bendiks_app/wsgi.py
+++ bendiks_app/wsgi.py
@@ -3,8 +3,8 @@ import sys
 import logging
 
 logging.basicConfig(filename='/home/u/git/stress_test/bendiks_app/error.log',
-                    format='%(asctime)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S',
+                    format='%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S',
                     filemode='a')
 sys.path.insert(0,"/home/u/git/stress_test/bendiks_app")
 

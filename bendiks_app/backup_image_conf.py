c8ba0a74c7f1714b01870c0abcf5611b8346eb7d
dtimonin <dtimonin@astralinux.ru>
2023-10-04 11:53:47 +0300

    add uu & dict with links

--- bendiks_app/backup_image_conf.py
+++ bendiks_app/backup_image_conf.py
@@ -326,17 +326,28 @@ brest_stands = ['stand10', 'stand11', 'stand12']
 #################################################################################################################################################
 #Перечень настроек, используемых для создания тестовых прогонов
 #################################################################################################################################################
-pkg_path_testing = 'http://qa111.devos.astralinux.ru/astra/testing/1.7-testing/base-repository/dists/1.7_x86-64/main/binary-amd64/Packages'
-vers_path_testing = 'http://qa111.devos.astralinux.ru/astra/testing/1.7-testing/base-repository/dists/1.7_x86-64/Release'
-vers_path_174 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4/dists/1.7_x86-64/Release'
-pkg_path_174 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4/dists/1.7_x86-64/main/binary-amd64/Packages'
-vers_path_173 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3/dists/1.7_x86-64/Release'
-pkg_path_173 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3/dists/1.7_x86-64/main/binary-amd64/Packages'
-vers_path_172 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2/dists/1.7_x86-64/Release'
-pkg_path_172 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2/dists/1.7_x86-64/main/binary-amd64/Packages'
-vers_path_171 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-1/dists/1.7_x86-64/Release'
-pkg_path_171 = 'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-1/dists/1.7_x86-64/main/binary-amd64/Packages'
-rc_list = ['1.7.5.4', '1.7.5.5']
+repo_path = {
+    'pkg_path_testing':'http://qa111.devos.astralinux.ru/astra/testing/1.7-testing/base-repository/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_testing':'http://qa111.devos.astralinux.ru/astra/testing/1.7-testing/base-repository/dists/1.7_x86-64/Release',
+    'vers_path_174UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4.1/dists/1.7_x86-64/Release',
+    'pkg_path_174UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4.1/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_174':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4/dists/1.7_x86-64/Release',
+    'pkg_path_174':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-4/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_173UU2':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3.2/dists/1.7_x86-64/Release',
+    'pkg_path_173UU2':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3.2/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_173UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3.1/dists/1.7_x86-64/Release',
+    'pkg_path_173UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3.1/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_173':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3/dists/1.7_x86-64/Release',
+    'pkg_path_173':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-3/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_172UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2.1/dists/1.7_x86-64/Release',
+    'pkg_path_172UU1':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2.1/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_172':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2/dists/1.7_x86-64/Release',
+    'pkg_path_172':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-2/dists/1.7_x86-64/main/binary-amd64/Packages',
+    'vers_path_171':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-1/dists/1.7_x86-64/Release',
+    'pkg_path_171':'http://qa111.devos.astralinux.ru/astra/stable/1.7/base-repository-1/dists/1.7_x86-64/main/binary-amd64/Packages'
+}
+
+rc_list = ['1.7.5.4', '1.7.5.5', '1.7.5.6']
 releases_list = ['1.7.1', '1.7.2', '1.7.3', '1.7.3.UU.1', '1.7.3.UU.2', '1.7.4', '1.7.4.UU.1', '1.7.5']
 testcase_orel = ['EXT4', 'NTFS', 'XFS', 'postgresql-aud-off', 'postgresql', 'syslog-ng', 'unix']
 testcase_orel_stand2 = ['EXT4', 'NTFS', 'XFS', 'syslog-ng', 'unix', 'RAM-overflow', 'SD-overflow']
--- bendiks_app/libs/libbend.py
+++ bendiks_app/libs/libbend.py
@@ -28,16 +28,7 @@ from backup_image_conf import (psyc,
                                test_run_stands,
                                rc_list,
                                releases_list,
-                               pkg_path_testing,
-                               vers_path_testing,
-                               pkg_path_174,
-                               vers_path_174,
-                               pkg_path_173,
-                               vers_path_173,
-                               pkg_path_172,
-                               vers_path_172,
-                               pkg_path_171,
-                               vers_path_171)
+                               repo_path)
 from time import sleep
 from libs.zefir import ZefirTestRun
 import ctypes
@@ -500,10 +491,8 @@ def get_kernels_from_rc(version_rc: str):
         c_name = ctypes.c_char_p(bytes(name, encoding='utf8'))
         clib.download_file(c_path, c_name)
 
-    pkg_path_chank = 'pkg_path_' + version_rc.replace('.', '')
-    vers_path_chank = 'vers_path_' + version_rc.replace('.', '')
-    pkg_path = globals()[pkg_path_chank]
-    vers_path = globals()[vers_path_chank]
+    pkg_path = repo_path[f'pkg_path_{version_rc.replace(".", "")}']
+    vers_path = repo_path[f'vers_path_{version_rc.replace(".", "")}']
 
     get_kernels = ZefirTestRun()
     get_file(pkg_path, 'available_packages')

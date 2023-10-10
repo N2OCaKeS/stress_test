1ec844c768fa8b8fcbb9928959b82e8adaa48b50
dtimonin <dtimonin@astralinux.ru>
2023-10-10 12:18:38 +0300

    add pgbench & some fixes

--- postgresql/sql/psb_parsec.sql
+++ postgresql/sql/psb_parsec.sql
@@ -1,13 +1,5 @@
--- Создаем пользователей
--- CREATE USER u_1_01; -- метка {0,0}  00
-
--- Даем привилегии PARSEC_CAP_CHMAC и PARSEC_CAP_SETMAC
--- ALTER USER u_0_00 WITH PARSEC_CAP_CHMAC, PARSEC_CAP_SETMAC;
-
-
-
 -- Создаем тестовую базу данных
--- CREATE DATABASE test_parsec;
+CREATE DATABASE test_parsec;
 
 -- Устанавливаем мандатную метку кластера
 MAC LABEL ON CLUSTER IS '{255,0xFFFFFFFFFFFFFFFF}';

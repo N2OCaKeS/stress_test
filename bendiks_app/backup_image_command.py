2d4801746546ab9bdf547d37354ae93d123a6064
dtimonin <dtimonin@astralinux.ru>
2023-10-11 17:51:54 +0300

    add 1.7.5.7

--- bendiks_app/backup_image_command.py
+++ bendiks_app/backup_image_command.py
@@ -2,6 +2,8 @@
 
 cz_comm = {
     'stand1':{
+        '1.7.5.7':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.201" \
+                -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-175rc7 nvme0n1',
         'altlinux-5.10':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.201" \
                 -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-alt nvme0n1',
         'debian11-6.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.201" \
@@ -34,6 +36,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-1-qa-team-13-171 nvme0n1'
         },
     'stand4':{
+        '1.7.5.7':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
+                -l ru_RU.UTF-8 startdisk restore stand-4-qa-team-13-175rc7 nvme0n1',
         '1.7.3.UU.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore stand-4-qa-team-13-173UU2 nvme0n1',
         '1.7.3.UU.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.204" \
@@ -52,6 +56,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-4-qa-team-13-175rc4 nvme0n1'
         },
     'stand3':{
+        '1.7.5.7':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
+                -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-175rc7 nvme0n1',
         '1.7.3.UU.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.203" \
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-173UU2 nvme0n1',
         '1.7.3.UU.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.203" \
@@ -72,6 +78,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-175rc5 nvme0n1'
         },
     'stand2':{
+        '1.7.5.7':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
+                -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-175rc7 nvme0n1',
         '1.7.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \
                 -l ru_RU.UTF-8 startdisk restore stand-2-qa-team-13-171 nvme0n1',
         '1.7.2':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \

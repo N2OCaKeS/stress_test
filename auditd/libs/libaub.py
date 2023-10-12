a417681048e31e3641db73a2d1532e2fb9d910ab
dtimonin <dtimonin@astralinux.ru>
2023-10-12 19:03:34 +0300

    fix response err

--- auditd/libs/libaub.py
+++ auditd/libs/libaub.py
@@ -109,9 +109,13 @@ def put_system_info_in_file(start, file):
 
 
 def response():
-    jira = requests.get('https://jira.astralinux.ru').status_code
-    life = requests.get('https://life.astralinux.ru').status_code
-    return jira, life
+    try:
+        jira = requests.get('https://jira.astralinux.ru').status_code
+        life = requests.get('https://life.astralinux.ru').status_code
+        return jira, life
+    except Exception as e:
+        jira, life = str(type(e).__name__, e)
+        return jira, life
 
 
 

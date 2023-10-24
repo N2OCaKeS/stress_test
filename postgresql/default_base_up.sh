21936c89c2b26771519b7aff351e88e42294c6f2
dtimonin <dtimonin@astralinux.ru>
2023-10-24 17:00:13 +0300

    fix path

--- postgresql/default_base_up.sh
+++ postgresql/default_base_up.sh
@@ -160,22 +160,22 @@ if [ "$2" == "tantor" ]; then
 
   if [ "$4" == "4" ]; then
     #sed -i 's/ac_enable_maclabels_on_files.*/ac_enable_maclabels_on_files = true/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_connections.*/max_connections = 2000/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*shared_buffers.*/shared_buffers = 32256MB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*effective_cache_size.*/effective_cache_size = 96768MB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*maintenance_work_mem.*/maintenance_work_mem = 2GB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*checkpoint_completion_target.*/checkpoint_completion_target = 0.9/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*wal_buffers.*/wal_buffers = 16MB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*default_statistics_target.*/default_statistics_target = 100/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*random_page_cost.*/random_page_cost = 1.1/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*effective_io_concurrency.*/effective_io_concurrency = 200/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*min_wal_size.*/min_wal_size = 1GB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_wal_size.*/max_wal_size = 4GB/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_worker_processes.*/max_worker_processes = 32/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_parallel_workers_per_gather.*/max_parallel_workers_per_gather = 4/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_parallel_workers.*/max_parallel_workers = 32/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/.*max_parallel_maintenance_workers.*/max_parallel_maintenance_workers = 4/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/postgresql.conf
-    sed -i 's/md5/trust/g' /etc/postgresql/$PG_VERSION/$PG_SETEST_CLUSTER/pg_hba.conf
+    sed -i 's/.*max_connections.*/max_connections = 2000/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*shared_buffers.*/shared_buffers = 32256MB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*effective_cache_size.*/effective_cache_size = 96768MB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*maintenance_work_mem.*/maintenance_work_mem = 2GB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*checkpoint_completion_target.*/checkpoint_completion_target = 0.9/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*wal_buffers.*/wal_buffers = 16MB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*default_statistics_target.*/default_statistics_target = 100/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*random_page_cost.*/random_page_cost = 1.1/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*effective_io_concurrency.*/effective_io_concurrency = 200/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*min_wal_size.*/min_wal_size = 1GB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*max_wal_size.*/max_wal_size = 4GB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*max_worker_processes.*/max_worker_processes = 32/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*max_parallel_workers_per_gather.*/max_parallel_workers_per_gather = 4/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*max_parallel_workers.*/max_parallel_workers = 32/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/.*max_parallel_maintenance_workers.*/max_parallel_maintenance_workers = 4/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
+    sed -i 's/md5/trust/g' /var/lib/postgresql/tantor-se-15/data/pg_hba.conf
   else
     sed -i 's/.*max_connections.*/max_connections = 2000/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf
     sed -i 's/.*shared_buffers.*/shared_buffers = 8GB/g' /var/lib/postgresql/tantor-se-15/data/postgresql.conf

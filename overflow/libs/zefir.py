a532ec5cd1809781ba8723a0a7af226e2acd611a
dtimonin <dtimonin@astralinux.ru>
2023-11-01 19:36:47 +0300

    add tantor

--- overflow/libs/zefir.py
+++ overflow/libs/zefir.py
@@ -167,7 +167,7 @@ class ZefirStatusAPI:
                                 estimatedTime,testResultStatuses,testCaseCount,issueCount,status(id,name,i18nKey,color),
                                 customFieldValues,createdOn,createdBy,updatedOn,updatedBy,
                                 owner&query=testRun.projectId+IN+(11200)+AND+testRun.folderTreeId+IN+({self.__folder_tree_id})+ORDER+
-                                BY+testRun.name+ASC&maxResults=40&startAt=0&archived=false
+                                BY+testRun.name+ASC&maxResults=400&startAt=0&archived=false
                             '''
         response_list_test_cycles = requests.get(url_list_test_cycles, headers=self.headers)
         data = response_list_test_cycles.json()
@@ -381,7 +381,7 @@ class ZefirResultTable:
                             'auditd benchmark. useraud':'Auditd_useraud', 'file system benchmark. OCFS2 parsec':'FS_OCFS2_parsec',
                             'postgresql benchmark smol':'PostgreSQL_smol', 'postgresql benchmark audit-off':'PSQL_audit-off',
                             'storage drive overflow':'SD_overflow', 'ram overflow':'RAM_overflow', 'file system benchmark. XFS parsec':'FS_XFS_parsec', 
-                            'postgresql benchmark vanilla':'PSQL_vanilla'}
+                            'postgresql benchmark vanilla':'PSQL_vanilla', 'tantor benchmark vanilla':'Tantor_vanilla'}
         for k, v in testname_columns.items():
             self.new_tab.rename(columns={k:v}, inplace=True)
         for name in self.new_tab.columns:

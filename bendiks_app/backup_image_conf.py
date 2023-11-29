9922b63adcc19ddcb16b0692b25f28acaf09f519
dtimonin <dtimonin@astralinux.ru>
2023-11-29 18:34:39 +0300

    del ntfs from stp

--- bendiks_app/backup_image_conf.py
+++ bendiks_app/backup_image_conf.py
@@ -460,7 +460,7 @@ repo_path = {
 rc_list = ['1.7.5.4', '1.7.5.5', '1.7.5.6', '1.7.5.7', '1.7.5.9', '1.8.0.1']
 releases_list = ['1.7.1', '1.7.2', '1.7.3', '1.7.3.UU.1', '1.7.3.UU.2', '1.7.4', '1.7.4.UU.1', '1.7.5', '1.8.0']
 
-testcase_orel_low_stand3 = ['EXT4', 'NTFS', 'XFS', 'syslog-ng', 'unix']
+testcase_orel_low_stand3 = ['EXT4', 'XFS', 'syslog-ng', 'unix']
 testcase_smolensk_low_stand3 = ['EXT4 parsec', 'XFS parsec', 'auditd-f', 'auditd-p', 'auditd-u']
 testcase_orel_middle_stand4 = ['postgresql-aud-off', 'postgresql', 'psql vanilla', 'tantor vanilla']
 testcase_smolensk_middle_stand4 = ['postgresql-sm'] #, 'psql parsec']

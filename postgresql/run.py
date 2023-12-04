fc80357ae35abb0742b4127e5f50968e5e8446b4
dtimonin <dtimonin@astralinux.ru>
2023-12-04 14:52:21 +0300

    fix start logic

--- postgresql/run.py
+++ postgresql/run.py
@@ -2,12 +2,9 @@
 
 import subprocess
 import argparse
-import logging
 from psb_conf import LOG_FILENAME, REPORT_PATH, RUN_LOG
 from os import path, mkdir
-from libs.libpsb import check_output_command, logging
-from time import sleep
-from sys import exit
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n',
@@ -16,25 +13,18 @@ parser.add_argument('-n',
                     help='dates name',
                     dest='NAME')
 
-parser.add_argument('-kn',
-                    action='store',
-                    required=False,
-                    help='kernel mode',
-                    dest='KERNEL')
-
 args = parser.parse_args()
 
 if not path.isdir(REPORT_PATH):
     mkdir(REPORT_PATH)
 
-if args.KERNEL:
-    subprocess.run(f'sudo python3 diff_kernel_quantity.py -q {args.KERNEL} -st 3', shell=True)
-else:
-    with open(f'/home/u/{args.NAME}', 'r') as r:
+with open(f'/home/u/{args.NAME}', 'r') as r:
         dates = r.read()
 
+if args.KERNEL:
+    subprocess.run(f'sudo python3 diff_kernel_quantity.py {dates}', shell=True)
+else:
     #subprocess.run(f'sudo perf record -a -g -F 99 venv/bin/python3 psb_run.py {dates}', shell=True)
     subprocess.run(f'sudo python3 psb_run.py {dates}', shell=True)
-    #holder_transaction('sudo python3 psb_public.py')
     subprocess.run('sudo python3 psb_public.py', shell=True)
 

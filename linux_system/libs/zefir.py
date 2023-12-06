32e5bb7cbe33ee842308e63f14c609270375ef14
dtimonin <dtimonin@astralinux.ru>
2023-12-06 19:00:38 +0300

    upd zefir

--- linux_system/libs/zefir.py
+++ linux_system/libs/zefir.py
@@ -492,7 +492,8 @@ class ZefirResultTable:
                             'postgresql benchmark smol':'PostgreSQL_smol', 'postgresql benchmark audit-off':'PSQL_audit-off',
                             'storage drive overflow':'SD_overflow', 'ram overflow':'RAM_overflow', 'file system benchmark. XFS parsec':'FS_XFS_parsec',
                             'postgresql benchmark parsec':'PSQL_parsec', 'postgresql benchmark vanilla':'PSQL_vanilla',
-                            'tantor benchmark vanilla':'Tantor_vanilla'}
+                            'tantor benchmark vanilla':'Tantor_vanilla', 'postgresql benchmark kernels':'PSQL_kernels',
+                            'tantor benchmark kernels':'Tantor_kernels'}
         for k, v in testname_columns.items():
             self.new_tab.rename(columns={k:v}, inplace=True)
         for name in self.new_tab.columns:

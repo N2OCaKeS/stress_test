19473ac5ac179d31aab25ad3374c94e69f1166a6
dtimonin <dtimonin@astralinux.ru>
2023-12-11 21:41:58 +0300

    upd comm

--- postgresql/balance/bl_prepare.sh
+++ postgresql/balance/bl_prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo echo deb ftp://10.177.5.111/astra/testing/1.8.0.2/devel 1.8_x86-64 main contrib non-free >> /etc/apt/sources.list
+sudo apt update -y
+
 #ansible
 sudo apt-get install ansible -y
 
@@ -14,6 +17,8 @@ sudo apt install libsdl1.2debian -y
 sudo dpkg -i libssl1.1_1.1.1n-0+deb10u6_amd64.deb 
 sudo dpkg -i libvpx5_1.7.0-3+deb10u1_amd64.deb
 sudo dpkg -i virtualbox-6.1_6.1.36-152435~Debian~buster_amd64.deb
+sudo apt install policykit-1
+sudo yes | VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-6.1.36a-152435.vbox-extpack
 
 #vagrant
 wget -r -nH --cut-dirs=2 --no-parent ftp://qa111.devos.astralinux.ru/packages/vagrant

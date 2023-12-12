364f3d9f0ed500e7f9ec62995e27085f4ccd28b3
dtimonin <dtimonin@astralinux.ru>
2023-12-12 13:04:32 +0300

    upd box

--- postgresql/balance/bl_run.py
+++ postgresql/balance/bl_run.py
@@ -4,11 +4,14 @@ import subprocess
 def cmd(command):
     return subprocess.run(command, shell=True)
 
-box_url = 'http://qa111.devos.astralinux.ru/vault/vagrant/smol-1.8.0.json'
-box_name = 'smolensk-vanilla-gui/1.8.0.2'
+box_url_18 = 'http://qa111.devos.astralinux.ru/vault/vagrant/smol-1.8.0.json'
+box_name_18 = 'smolensk-vanilla-gui/1.8.0.2'
+box_url_174 = 'http://qa111.devos.astralinux.ru/vault/vagrant/smolensk-vanilla-gui-1.7.4.json'
+box_name_174 = 'smolensk-vanilla-gui/1.7.4'
+
 
 cmd('sudo bash bl_prepare.sh')
-cmd(f'vagrant box add {box_url} --force')
-cmd(f'UPDATE={box_name} vagrant up --provider=virtualbox')
+cmd(f'vagrant box add {box_url_174} --force')
+cmd(f'UPDATE={box_name_174} vagrant up --provider=virtualbox')
 
 

71efdbd6d844171a45d741ac185498382d9e0f0e
dtimonin <dtimonin@astralinux.ru>
2023-12-19 11:53:20 +0300

    rebuild network to bridge

--- postgresql/balance/bl_run.py
+++ postgresql/balance/bl_run.py
@@ -18,28 +18,36 @@ vbox_subnet_mask = '19'
 vm_dates = {
         'database1':{'host-port':'2021',
                      'ip':'10.0.0.11',
-                     'sshnum':''},
+                     'sshnum':'',
+                     'ip_bridge':'10.177.103.111'},
         'database2':{'host-port':'2022',
                      'ip':'10.0.0.12',
-                     'sshnum':'1'},
+                     'sshnum':'1',
+                     'ip_bridge':'10.177.103.112'},
         'database3':{'host-port':'2025',
                      'ip':'10.0.0.13',
-                     'sshnum':'2'},
+                     'sshnum':'2',
+                     'ip_bridge':'10.177.103.113'},
         'lbdb1':{'host-port':'2024',
                  'ip':'10.0.0.41',
-                 'sshnum':'3'},
+                 'sshnum':'3',
+                 'ip_bridge':'10.177.103.141'},
         'lbdb2':{'host-port':'2023',
                  'ip':'10.0.0.42',
-                 'sshnum':'4'},
+                 'sshnum':'4',
+                 'ip_bridge':'10.177.103.142'},
         'lbdb3':{'host-port':'2026',
                  'ip':'10.0.0.43',
-                 'sshnum':'5'},
+                 'sshnum':'5',
+                 'ip_bridge':'10.177.103.143'},
         'pgpool':{'host-port':'2027',
                   'ip':'10.0.0.31',
-                  'sshnum':'6'},
+                  'sshnum':'6',
+                  'ip_bridge':'10.177.103.131'},
         'dcfreeipa':{'host-port':'2029',
                      'ip':'10.0.0.10',
-                     'sshnum':'7'}
+                     'sshnum':'7',
+                     'ip_bridge':'10.177.103.110'}
 }
 
 
@@ -61,6 +69,19 @@ def vm_port(vm_name):
                     | awk -F',' '{{for(i=1;i<=NF;i++) if ($i ~ /host port/) print $i}}' \
                     | awk '{{print $NF}}'"""
     return check_output_command(bash_command)
+
+def colors(text, color:str) -> str:
+    reset = '\033[0m'
+    yellow = '\033[93m'
+    green = '\033[92m'
+    red = '\033[91m'
+
+    if color == 'yellow':
+        return f'{yellow}{text}{reset}'
+    elif color == 'green':
+        return f'{green}{text}{reset}'
+    elif color == 'red':
+        return f'{red}{text}{reset}'
 
 def set_network(vm, nat_name):
     try:
@@ -89,21 +110,22 @@ cmd(f'UPDATE={box_name_174} BOX_URL={box_url_174} vagrant up --provider=virtualb
 
 # # # Network set
 #cmd(f'vboxmanage natnetwork add --netname {vbox_nat} --network "10.0.0.0/19" --enable --dhcp on')
-[set_network(vm, vbox_nat) for vm in VMs if vm in check_vm_list()]
+#[set_network(vm, vbox_nat) for vm in VMs if vm in check_vm_list()]
 cmd('vboxmanage natnetwork list')
 cmd('vboxmanage list vms')
 
 
 # # # SSH connect info
 #vm_ports = {name:f'ssh u@localhost -p {vm_port(name)}' for name in VMs}
-vm_ports = {name:f'sshpass -v -p 1 ssh {no_fprint} u@localhost -p {vm_dates[name]["host-port"]}' for name in VMs}
+#vm_ports = {name:f'sshpass -v -p 1 ssh {no_fprint} u@localhost -p {vm_dates[name]["host-port"]}' for name in VMs}
+vm_creds = {colors(name, 'yellow'):f'sshpass -v -p 1 ssh {no_fprint} u@{vm_dates[name]["ip_bridge"]}' for name in VMs}
 #[cmd(f'ssh-keygen -R [127.0.0.1]:{port}') for port in vm_ports.keys()]
-for item in vm_ports.items():
+for item in vm_creds.items():
     print(item)
 
 
-cmd('ansible-playbook bl_contrprimer.yml')
-cmd('ansible-playbook tasks/checks/db/replication.yml')
+#cmd('ansible-playbook bl_contrprimer.yml')
+#cmd('ansible-playbook tasks/checks/db/replication.yml')
 
 
 #sudo vboxmanage showvminfo database3
--- postgresql/balance/provision/env_provision.sh
+++ postgresql/balance/provision/env_provision.sh
@@ -22,28 +22,47 @@ vbox_machines=(\
 )
 
 net_name="Проводное соединение 1"
-vbox_subnet_mask=19
+#vbox_subnet_mask=19
+vbox_subnet_mask=24
 vbox_gateway=10.0.0.1
 vbox_nat=QANetwork
 vbox_nat_ip=10.0.0.0
 
-declare -A database1_br=( [ip]=10.0.0.11 [domain]=database1.balance.rbt [host]=database1 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A database2_br=( [ip]=10.0.0.12 [domain]=database2.balance.rbt [host]=database2 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A database3_br=( [ip]=10.0.0.13 [domain]=database3.balance.rbt [host]=database3 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A lbdb1_br=( [ip]=10.0.0.41 [domain]=lbdb1.balance.rbt [host]=lbdb1 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A lbdb2_br=( [ip]=10.0.0.42 [domain]=lbdb2.balance.rbt [host]=lbdb2 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A lbdb3_br=( [ip]=10.0.0.43 [domain]=lbdb3.balance.rbt [host]=lbdb3 [dns]="10.0.0.10, 8.8.8.8" )
-declare -A pgpool_br=( [ip]=10.0.0.31 [domain]=pgpool.balance.rbt [host]=pgpool [dns]="10.0.0.10, 8.8.8.8" )
-declare -A dcfreeipa_br=( [ip]=10.0.0.10 [domain]=dcfreeipa.balance.rbt [host]=dcfreeipa [dns]="10.0.0.10, 8.8.8.8" )
+declare -A database1_br=( [ip]=10.177.103.111 [domain]=database1.balance.rbt [host]=database1 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A database2_br=( [ip]=10.177.103.112 [domain]=database2.balance.rbt [host]=database2 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A database3_br=( [ip]=10.177.103.113 [domain]=database3.balance.rbt [host]=database3 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A lbdb1_br=( [ip]=10.177.103.141 [domain]=lbdb1.balance.rbt [host]=lbdb1 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A lbdb2_br=( [ip]=10.177.103.142 [domain]=lbdb2.balance.rbt [host]=lbdb2 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A lbdb3_br=( [ip]=10.177.103.143 [domain]=lbdb3.balance.rbt [host]=lbdb3 [dns]="10.177.103.110, 10.177.128.198" )
+declare -A pgpool_br=( [ip]=10.177.103.131 [domain]=pgpool.balance.rbt [host]=pgpool [dns]="10.177.103.110, 10.177.128.198" )
+declare -A dcfreeipa_br=( [ip]=10.177.103.110 [domain]=dcfreeipa.balance.rbt [host]=dcfreeipa [dns]="10.177.103.110, 10.177.128.198" )
 
-declare -A database1=( [ip]=10.0.0.11 [server-port]=3421 [forward-port]=2021 [mac]=08:00:27:E1:87:C4 [net]=int0) 
-declare -A database2=( [ip]=10.0.0.12 [server-port]=3422 [forward-port]=2022 [mac]=08:00:27:64:AF:57 [net]=int0)
-declare -A database3=( [ip]=10.0.0.13 [server-port]=3423 [forward-port]=2025 [mac]=08:00:27:35:FB:4D [net]=int0)
-declare -A lbdb1=(     [ip]=10.0.0.41 [server-port]=3424 [forward-port]=2024 [mac]=08:00:27:93:D3:2B [net]=int0)
-declare -A lbdb2=(     [ip]=10.0.0.42 [server-port]=3425 [forward-port]=2023 [mac]=08:00:27:73:E5:1C [net]=int0)
-declare -A lbdb3=(     [ip]=10.0.0.43 [server-port]=3431 [forward-port]=2026 [mac]=08:00:27:15:29:EA [net]=int0)
-declare -A pgpool=(    [ip]=10.0.0.31 [server-port]=3432 [forward-port]=2027 [mac]=08:00:27:BF:3D:49 [net]=int0) 
-declare -A dcfreeipa=( [ip]=10.0.0.10 [server-port]=3434 [forward-port]=2029 [mac]=08:00:27:D3:CB:DD [net]=int0)
+# declare -A database1_br=( [ip]=10.0.0.11 [domain]=database1.balance.rbt [host]=database1 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A database2_br=( [ip]=10.0.0.12 [domain]=database2.balance.rbt [host]=database2 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A database3_br=( [ip]=10.0.0.13 [domain]=database3.balance.rbt [host]=database3 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A lbdb1_br=( [ip]=10.0.0.41 [domain]=lbdb1.balance.rbt [host]=lbdb1 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A lbdb2_br=( [ip]=10.0.0.42 [domain]=lbdb2.balance.rbt [host]=lbdb2 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A lbdb3_br=( [ip]=10.0.0.43 [domain]=lbdb3.balance.rbt [host]=lbdb3 [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A pgpool_br=( [ip]=10.0.0.31 [domain]=pgpool.balance.rbt [host]=pgpool [dns]="10.0.0.10, 8.8.8.8" )
+# declare -A dcfreeipa_br=( [ip]=10.0.0.10 [domain]=dcfreeipa.balance.rbt [host]=dcfreeipa [dns]="10.0.0.10, 8.8.8.8" )
+
+declare -A database1=( [ip]=10.177.103.111 [server-port]=3421 [forward-port]=2021 [mac]=08:00:27:E1:87:C4 [net]=int0) 
+declare -A database2=( [ip]=10.177.103.112 [server-port]=3422 [forward-port]=2022 [mac]=08:00:27:64:AF:57 [net]=int0)
+declare -A database3=( [ip]=10.177.103.113 [server-port]=3423 [forward-port]=2025 [mac]=08:00:27:35:FB:4D [net]=int0)
+declare -A lbdb1=(     [ip]=10.177.103.141 [server-port]=3424 [forward-port]=2024 [mac]=08:00:27:93:D3:2B [net]=int0)
+declare -A lbdb2=(     [ip]=10.177.103.142 [server-port]=3425 [forward-port]=2023 [mac]=08:00:27:73:E5:1C [net]=int0)
+declare -A lbdb3=(     [ip]=10.177.103.143 [server-port]=3431 [forward-port]=2026 [mac]=08:00:27:15:29:EA [net]=int0)
+declare -A pgpool=(    [ip]=10.177.103.131 [server-port]=3432 [forward-port]=2027 [mac]=08:00:27:BF:3D:49 [net]=int0) 
+declare -A dcfreeipa=( [ip]=10.177.103.110 [server-port]=3434 [forward-port]=2029 [mac]=08:00:27:D3:CB:DD [net]=int0)
+
+# declare -A database1=( [ip]=10.0.0.11 [server-port]=3421 [forward-port]=2021 [mac]=08:00:27:E1:87:C4 [net]=int0) 
+# declare -A database2=( [ip]=10.0.0.12 [server-port]=3422 [forward-port]=2022 [mac]=08:00:27:64:AF:57 [net]=int0)
+# declare -A database3=( [ip]=10.0.0.13 [server-port]=3423 [forward-port]=2025 [mac]=08:00:27:35:FB:4D [net]=int0)
+# declare -A lbdb1=(     [ip]=10.0.0.41 [server-port]=3424 [forward-port]=2024 [mac]=08:00:27:93:D3:2B [net]=int0)
+# declare -A lbdb2=(     [ip]=10.0.0.42 [server-port]=3425 [forward-port]=2023 [mac]=08:00:27:73:E5:1C [net]=int0)
+# declare -A lbdb3=(     [ip]=10.0.0.43 [server-port]=3431 [forward-port]=2026 [mac]=08:00:27:15:29:EA [net]=int0)
+# declare -A pgpool=(    [ip]=10.0.0.31 [server-port]=3432 [forward-port]=2027 [mac]=08:00:27:BF:3D:49 [net]=int0) 
+# declare -A dcfreeipa=( [ip]=10.0.0.10 [server-port]=3434 [forward-port]=2029 [mac]=08:00:27:D3:CB:DD [net]=int0)
 
 # sudo systemctl restart networking
 
@@ -91,7 +110,7 @@ elif [ "$1" = "dcfreeipa" ]; then
 fi
 
 sudo nmcli connection modify "${net_name}" ipv4.method manual ip4 $ip/$vbox_subnet_mask
-sudo nmcli connection modify "${net_name}" gw4 $vbox_gateway
+#sudo nmcli connection modify "${net_name}" gw4 $vbox_gateway
 sudo nmcli connection modify "${net_name}" ipv4.dns "$dns"
 
 if id "$main_user" >/dev/null 2>&1; then

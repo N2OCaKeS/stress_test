0b265db816a2ec4f7cb487c2b45dffcea5e016cf
dtimonin <dtimonin@astralinux.ru>
2024-01-10 17:37:48 +0300

    add balance in zefir

--- bendiks_app/libs/zefir.py
+++ bendiks_app/libs/zefir.py
@@ -338,7 +338,8 @@ class ZefirResultTable:
                             'storage drive overflow':'SD_overflow', 'ram overflow':'RAM_overflow', 'file system benchmark. XFS parsec':'FS_XFS_parsec',
                             'postgresql benchmark parsec':'PSQL_parsec', 'postgresql benchmark vanilla':'PSQL_vanilla',
                             'tantor benchmark vanilla':'Tantor_vanilla', 'postgresql benchmark kernels':'PSQL_kernels',
-                            'tantor benchmark kernels':'Tantor_kernels', 'linux_system_benchmark. UnixBench parsec':'UnixBench_parsec'}
+                            'tantor benchmark kernels':'Tantor_kernels', 'linux_system_benchmark. UnixBench parsec':'UnixBench_parsec',
+                            'postgresql benchmark balance':'PSQL_balance'}
         for k, v in testname_columns.items():
             self.new_tab.rename(columns={k:v}, inplace=True)
         for name in self.new_tab.columns:

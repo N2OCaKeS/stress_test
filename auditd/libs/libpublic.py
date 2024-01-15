1c822de4d0929bc8bea2f4c321c7c325f565c719
dtimonin <dtimonin@astralinux.ru>
2024-01-15 18:14:02 +0300

    add conf space create

--- auditd/libs/libpublic.py
+++ auditd/libs/libpublic.py
@@ -54,14 +54,45 @@ class Public:
                  'storage':'SAS SSD 3.8Tb'}
         }
 
-    def preset_publish(self, c_pp, c_np):
+    def preset_publish(self, c_pp, c_np, release_pp=False, release_np=False):
 
         confluence_report = ReportToConfluence(username=self.username, password=None, token=self.token)
 
         #создать страницу confluence
-        confluence_report.create_confluence_page(self.c_space,
-                                                 c_pp,
-                                                 c_np)
+        def name_page(arg):
+            top_page = f'STRESS ⬝ {str(arg).split("_")[1][:3]}'
+            version_page = f'STRESS ⬝ {str(arg).split("_")[1]}'
+            return top_page, version_page
+
+        if release_pp and release_np:
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(release_np)[0],
+                                                     name_page(release_np)[1])
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(release_np)[1],
+                                                     release_pp)
+            confluence_report.create_confluence_page(self.c_space,
+                                                     release_pp,
+                                                     release_np)
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(release_np)[1],
+                                                     name_page(c_np)[1])
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(c_np)[1],
+                                                     c_pp)
+            confluence_report.create_confluence_page(self.c_space,
+                                                     c_pp,
+                                                     c_np)
+        else:
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(c_np)[0],
+                                                     name_page(c_np)[1])
+            confluence_report.create_confluence_page(self.c_space,
+                                                     name_page(c_np)[1],
+                                                     c_pp)
+            confluence_report.create_confluence_page(self.c_space,
+                                                     c_pp,
+                                                     c_np)
         
         #прикрепить файлы к странице confluence
         for file in os.listdir(REPORT_DIR):
@@ -118,7 +149,11 @@ class Public:
         html_page = '\n'.join([header_table, rating, tables, images])
 
         #выкладываем информацию на страницу
-        confluence_report.update_confluence_page(self.c_space, c_np, html_page)    
+        if release_pp and release_np:
+            confluence_report.update_confluence_page(self.c_space, c_np, html_page)
+            confluence_report.update_confluence_page(self.c_space, release_np, html_page)
+        else:
+            confluence_report.update_confluence_page(self.c_space, c_np, html_page)  
 
 
     def run_publish(self):
@@ -132,8 +167,7 @@ class Public:
             rare_cnp = self.c_np.split('_')
             cnp = '_'.join([release_version if release_version in rare_cnp[i] else rare_cnp[i] for i in range(len(rare_cnp))])
 
-            self.preset_publish(self.c_pp, self.c_np)
-            self.preset_publish(cpp, cnp)
+            self.preset_publish(self.c_pp, self.c_np, release_pp=cpp, release_np=cnp)
 
         elif len(check_len_version) == 6 and check_len_version[3] == 'UU':
             release_version = '.'.join(check_len_version[:5])
@@ -142,8 +176,7 @@ class Public:
             rare_cnp = self.c_np.split('_')
             cnp = '_'.join([release_version if release_version in rare_cnp[i] else rare_cnp[i] for i in range(len(rare_cnp))])
 
-            self.preset_publish(self.c_pp, self.c_np)
-            self.preset_publish(cpp, cnp)
+            self.preset_publish(self.c_pp, self.c_np, release_pp=cpp, release_np=cnp)
 
         else:
             self.preset_publish(self.c_pp, self.c_np)

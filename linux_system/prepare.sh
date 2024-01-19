4e5cf30d4773d61c959b9458850a09e239feaab8
dtimonin <dtimonin@astralinux.ru>
2024-01-19 18:57:22 +0300

    fix err

--- linux_system/prepare.sh
+++ linux_system/prepare.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y gcc make libpdp-dev
 # create venv in script_dir
-sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev python3-joblib
+sudo apt-get install -y python3-dev python3-venv python3-requests python3-pip libffi-dev
 sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib python3-lxml python3-bs4 python3-pexpect python3-prettytable
 #python3-pandas
 #python3-sklearn

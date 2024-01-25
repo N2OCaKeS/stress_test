657bf7f7c86edb28bb68ff888d776219c1656c6a
dtimonin <dtimonin@astralinux.ru>
2024-01-25 19:00:51 +0300

    add vagrant & upd

--- apache2/apache_bench.py
+++ apache2/apache_bench.py
@@ -1,10 +1,31 @@
 from libs.libapa import (command,
                          check_output_command)
+import os
+import numpy as np
 
-requests = 10000
-concurrency = 10
+
+#Количество запусков бенчмарка
+repetitions_counter = 30
+#Запросы
+requests = 20000
+#Потоки
+concurrency = 20
+#Лимит группы по количеству элементов, принимаемой к расчетам, в %
+valid_values_percent = 50
+#Лимит отклонения, в %
+percent_limit = 15
+
+
 ip = check_output_command("hostname -I | awk '{print $1}'"); print(ip)
 modules = ['proxy', 'proxy_http', 'proxy_balancer', 'lbmethod_byrequests', 'headers']
+
+html_page = """<!DOCTYPE html>
+<html>
+<body>
+Hello
+</body>
+</html>"""
+
 proxy_sett = r"""<VirtualHost *:80>
 ServerName loadbalancer
 RequestHeader set X-Remote-User expr=%{REMOTE_USER}
@@ -34,7 +55,10 @@ proxy_member_sett_2 = r"""<VirtualHost *:8097>
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>"""
 
-print(f'ab -n {requests} -c {concurrency} http://{ip}:80/ > result.txt')
+
+def start_benchmark(count):
+    print(f'ab -n {requests} -c {concurrency} http://{ip}:80/ >> result.txt')
+    [command(f'ab -n {requests} -c {concurrency} http://{ip}:80/ >> result.txt') for i in range(count)]
 
 command('sudo apt-get install apache2 -y')
 command('sudo sed -i -e \'s/# AstraMode on/AstraMode off/\' /etc/apache2/apache2.conf')
@@ -42,6 +66,9 @@ command('sudo sed -i -e \'s/AstraMode on/AstraMode off/\' /etc/apache2/apache2.c
 
 with open('/etc/apache2/sites-available/000-default.conf', 'w')  as site:
     site.write(proxy_sett)
+
+with open('/var/www/html/index.html', 'w') as index:
+    index.write(html_page)
 
 with open('/etc/apache2/ports.conf', 'a') as ports:
     ports.write('Listen 8096\nListen 8097\n')
@@ -58,57 +85,33 @@ command('sudo a2ensite 001-member-8096.conf')
 command('sudo a2ensite 002-member-8097.conf')
 command('sudo systemctl restart apache2')
 
-command(f'ab -n {requests} -c {concurrency} http://{ip}:80/ > result.txt')
+if os.path.isfile('result.txt'):
+    os.remove('result.txt')
+
+start_benchmark(repetitions_counter)
 
 with open('result.txt', 'r') as f:
-    for line in f:
-        if 'Requests per second:' in line:
-            value = line.split(':')[1].strip()
-            print(value.split(' ')[0])
+    result_file = f.readlines()
+values = [v.split(':')[1].strip().split(' ')[0] for v in result_file if 'Requests per second:' in v]; print(values)
+ab_values = np.array([int(float(x)) for x in values if x.replace('.', '', 1).isdigit()]); print(ab_values)
 
 
+def check_value(value, all_values, percent_limit):
+    diffs = np.abs((all_values - value) / value * 100)
+    return np.sum(diffs <= percent_limit) >= len(all_values) / 2
 
 
+valid_values = [value for value in ab_values if check_value(value, ab_values, percent_limit)]
+novalid_values = [value for value in ab_values if value not in valid_values]
+print('Используемые в расчетах значения:', valid_values)
+print('Отсеянные значения:', novalid_values)
 
+if len(valid_values) >= len(ab_values) * valid_values_percent / 100:
+    mean_cleaned = np.mean(valid_values)
+    print(f"Среднее значение без учета аномалий: {int(mean_cleaned)}")
+else:
+    print('Нет подходящих групп значений для расчета среднего')
 
-
-# ip = check_output_command("hostname -I | awk '{print $1}'")
-# modules = ['proxy', 'proxy_http', 'proxy_balancer', 'lbmethod_byrequests', 'headers']
-# proxy_sett = r"""<VirtualHost *:80>
-# ServerName loadbalancer
-# RequestHeader set X-Remote-User expr=%{REMOTE_USER}
-# ProxyPreserveHost On
-# ProxyRequests Off
-# ServerName localhost
-# ProxyPass / "balancer://mycluster/"
-# ProxyPassReverse / "balancer://mycluster/"
-# AstraMode off
-# ProxyReceiveBufferSize 2048
-# ProxyTimeout 1
-# <Proxy "balancer://mycluster/">
-#         BalancerMember "http://localhost:8096"
-# </Proxy>
-# </VirtualHost>"""
-
-# command('sudo apt-get install apache2 -y')
-
-# with open('/etc/apache2/sites-available/000-default.conf', 'w')  as site:
-#     site.write(proxy_sett)
-
-# [command(f'sudo a2enmod {module}') for module in modules]
-# command('sudo a2ensite 000-default.conf')
-# command('sudo systemctl restart apache2')
-
-# command(f'ab -n {requests} -c {concurrency} http://{ip}:80/ > result.txt')
-
-# with open('result.txt', 'r') as f:
-#     for line in f:
-#         if 'Requests per second:' in line:
-#             value = line.split(':')[1].strip()  
-#             print(value.split(' ')[0])
-#         else:
-#             value = 'Nan'
-#             print('Error: => Line "Requests per second" not found')
 
     
 

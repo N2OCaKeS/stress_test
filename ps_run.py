d70d7d4761dd910ad8e96815776fe30f98f3c938
dtimonin <dtimonin@astralinux.ru>
2024-01-30 14:53:04 +0300

    add parse perf output

--- ps_run.py
+++ ps_run.py
@@ -1,5 +1,7 @@
 from libs.libparsec import (check_output_command,
                             command)
+from os.path import isfile
+from re import findall
 
 
 #количество создаваемых потоков 
@@ -7,28 +9,39 @@ concurrency = 10
 #количество циклов для каждого потока
 counter = 1000
 
+perf_report_name = 'perf_report.txt'
+shared_object_name = 'kernel.kallsyms'
+flamegraph_name = 'result_flamegraph.svg'
 load_command = f'cd libs && sudo perf record -a -g -F 99 time ./load_test /tmp {concurrency} {counter}'
-load_rare_results = check_output_command(load_command)
+load_rare_results = check_output_command(load_command); print(load_rare_results)
 find_args = ['real', 'user', 'sys']
+found_functions = f"cat {perf_report_name} | grep {shared_object_name} | awk '{{print $6}}'"
 
+command(f'cd libs && sudo perf report > {perf_report_name}')
 command('cd libs && sudo perf script -i perf.data > out.perf')
 command('cd libs && sudo perl libstackcollapse-perf.pl out.perf > out.folded')
-command('sudo perl libs/libflamegraph.pl libs/out.folded > result_flamegraph.svg')
+command(f'sudo perl libs/libflamegraph.pl libs/out.folded > {flamegraph_name}')
 
-print(load_rare_results)
+# # # Found used function
+if isfile(f'libs/{perf_report_name}'):
+    used_functions = check_output_command(found_functions)
+print(f'used_functions {used_functions}')
+cleared_used_functions = [func for func in set(used_functions)]
+print(f'cleared_used_functions {cleared_used_functions}')
 
-#load_rare_results = """double free or corruption (out)
-#/bin/sh: строка 1: 17361 Аварийный останов         ./load_parsec /tmp 10 1000
-#real    0m11,150s
-#user    0m0,154s
-#sys     0m6,594s"""
+def used_cpu_time(func_name): 
+    func_command = f"cat {flamegraph_name} | grep {func_name} | awk '{{print $4}}'"
+    return check_output_command(func_command)
+
+used_cpu_dict = {
+    func:[findall(r'\d+\.\d+', x)[0] for x in used_cpu_time(func).split('\n')] for func in cleared_used_functions
+}
+print(f'used_cpu_dict {used_cpu_dict}')
 
 load_rare_results = [
     result.replace('\t', '-').split('-') for match in find_args for result in \
     load_rare_results.strip().split('\n') if result.startswith(match)
     ]
-
-#print(load_rare_results)
 
 load_rare_results = {i[0]:i[1] for i in load_rare_results}
 load_results = {

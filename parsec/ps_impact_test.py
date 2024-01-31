86ba2f0c2f0ca4cb48023c54358787f9fdee2511
dtimonin <dtimonin@astralinux.ru>
2024-01-31 13:47:31 +0300

    add func & mount dir

--- parsec/ps_impact_test.py
+++ parsec/ps_impact_test.py
@@ -1,6 +1,7 @@
 from libs.libparsec import (check_output_command,
                             command)
-from os.path import isfile
+from os.path import isfile, isdir
+from os import mkdir
 from re import findall
 
 
@@ -9,7 +10,7 @@ concurrency = 100
 #количество циклов для каждого потока
 counter = 1000
 
-load_dir = '/tmp'
+load_dir = '/tmp/x'
 perf_report_name = 'perf_report.txt'
 shared_object_name = 'kernel.kallsyms'
 flamegraph_name = 'result_flamegraph.svg'
@@ -22,7 +23,20 @@ load_rare_results = 'real\t0m0,000s\nuser\t0m0,000s\nsys\t0m0,000s'
 parsec_function = ['parsec_capable_ilev', 'parsec_inode_permission_a', 'i_pdpl_get',
 'parsec_secid', 'pdpl_put', 'pdpl_get', 'parsec_current_permission', 'parsec_caps_task_get',
 'caps_cpy', 'pdpml_permission', 'parsec_realpath_from_dentry', 'parsec_task_lbl_get',
-'parsec_log', 'parsec_inode_permission']
+'parsec_log', 'parsec_inode_permission', 'parsec_audit_rule_match', 'pdpl_permission',
+'psc_audit_check', 'parsec_confine_check', 'is_kernel_audit', 'i_audit_get', 'di_audit_init',
+'pdpml_conf_permission', 'di_pdpl_set_from_tsk', 'di_pdpl_set', 'parsec_file_open',
+'di_faud_from_xattr', 'pdpl_cmp', 'irelax_permission', 'di_pdpl_init', 'parsec_path_mknod',
+'psc_secid_to_lbl', 'inode_post_create', 'parsec_instantiate', 'parsec_i_alloc_security',
+'di_getxattr', 'file_audit_inherit_default_audit', 'parsec_i_free_security', 'parsec_inode_init_xattrs',
+'di_pdpl_from_xattr', 'parsec_path_hook.isra.0', 'pdpl_dup', 'pdpl_inherit_integrity', 'cell_check',
+'pdpml_put', 'do_i_init_security', 'aud_cpy', 'di_chlbl_permission', 'do_post_create',
+'parsec_current_getsecid_subj', 'pdpl_or_type', 'faud_dup', 'parsec_oracle_check_dentry',
+'inode_use_init_xattrs.isra.0', 'I_LBL_UPD', 'parsec_aud_task_get', 'pdpl_inherit_secrecy']
+
+if not isdir(load_dir):
+    mkdir(load_dir, 0o777)
+    command(f'sudo mount -t tmpfs -o size=100M test_dir {load_dir}')
 
 command(load_command)
 command(f'cd libs && sudo perf report > {perf_report_name}')
@@ -43,8 +57,8 @@ def found_used_functions(shared_object=False):
             used_functions = check_output_command(found_functions)
         #print(f'used_functions {used_functions}')
         cleared_used_functions = used_functions.split('\n')
-    else: cleared_used_functions = parsec_function
-    print(f'\033[93m\nUsed Functions:\n{parsec_function}\n\033[0m')
+    else: cleared_used_functions = set(parsec_function)
+    print(f'\033[93m\nUsed Functions:\n{set(parsec_function)}\n\033[0m')
     #print(f'cleared_used_functions {cleared_used_functions}')
     return cleared_used_functions
 

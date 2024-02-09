d99ab9a795e1bc93081432191cc3c24922115c55
dtimonin <dtimonin@astralinux.ru>
2024-02-09 15:03:05 +0300

    add 'freeipa authentication test':'FreeIPA_auth'

--- parsec/libs/zefir.py
+++ parsec/libs/zefir.py
@@ -437,7 +437,7 @@ class ZefirResultTable:
                             'postgresql benchmark parsec':'PSQL_parsec', 'postgresql benchmark vanilla':'PSQL_vanilla',
                             'tantor benchmark vanilla':'Tantor_vanilla', 'postgresql benchmark kernels':'PSQL_kernels',
                             'tantor benchmark kernels':'Tantor_kernels', 'linux_system_benchmark. UnixBench parsec':'UnixBench_parsec',
-                            'postgresql benchmark balance':'PSQL_balance'}
+                            'postgresql benchmark balance':'PSQL_balance', 'freeipa authentication test':'FreeIPA_auth'}
         for k, v in testname_columns.items():
             self.new_tab.rename(columns={k:v}, inplace=True)
         for name in self.new_tab.columns:

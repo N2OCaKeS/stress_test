ed3be2a417001e224864df2a1205f40e7012cc8e
dtimonin <dtimonin@astralinux.ru>
2024-02-16 16:06:22 +0300

    upd parsec func

--- parsec/ps_test.py
+++ parsec/ps_test.py
@@ -23,19 +23,8 @@ class ParsecImpactTest:
         self.find_args = ['real', 'user', 'sys']
         self.found_functions = f"cat libs/{self.perf_report_name} | grep {self.shared_object_name} | awk '{{print $6}}'"
         self.load_rare_results = 'real\t0m0,000s\nuser\t0m0,000s\nsys\t0m0,000s'
-        self.parsec_function = ['parsec_capable_ilev', 'parsec_inode_permission_a', 'i_pdpl_get',
-        'parsec_secid', 'pdpl_put', 'pdpl_get', 'parsec_current_permission', 'parsec_caps_task_get',
-        'caps_cpy', 'pdpml_permission', 'parsec_realpath_from_dentry', 'parsec_task_lbl_get',
-        'parsec_log', 'parsec_inode_permission', 'parsec_audit_rule_match', 'pdpl_permission',
-        'psc_audit_check', 'parsec_confine_check', 'is_kernel_audit', 'i_audit_get', 'di_audit_init',
-        'pdpml_conf_permission', 'di_pdpl_set_from_tsk', 'di_pdpl_set', 'parsec_file_open',
-        'di_faud_from_xattr', 'pdpl_cmp', 'irelax_permission', 'di_pdpl_init', 'parsec_path_mknod',
-        'psc_secid_to_lbl', 'inode_post_create', 'parsec_instantiate', 'parsec_i_alloc_security',
-        'di_getxattr', 'file_audit_inherit_default_audit', 'parsec_i_free_security', 'parsec_inode_init_xattrs',
-        'di_pdpl_from_xattr', 'parsec_path_hook.isra.0', 'pdpl_dup', 'pdpl_inherit_integrity', 'cell_check',
-        'pdpml_put', 'do_i_init_security', 'aud_cpy', 'di_chlbl_permission', 'do_post_create',
-        'parsec_current_getsecid_subj', 'pdpl_or_type', 'faud_dup', 'parsec_oracle_check_dentry',
-        'inode_use_init_xattrs.isra.0', 'I_LBL_UPD', 'parsec_aud_task_get', 'pdpl_inherit_secrecy']
+        self.parsec_function = check_output_command('nm /usr/lib/modules/`uname -r`/misc/parsec.ko | grep -i " t " | \
+                                                     awk \'{print $3}\'').split('\n')
 
     def parsec_impact_by_fs_load(self):
         if not isdir(self.load_dir):

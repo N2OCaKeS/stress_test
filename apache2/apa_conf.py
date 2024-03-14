b687ece65b5854a3cadea92009ad6780171bb6ee
dtimonin <dtimonin@astralinux.ru>
2024-03-14 18:05:24 +0300

    up req count

--- apache2/apa_conf.py
+++ apache2/apa_conf.py
@@ -9,7 +9,7 @@ VENV_PATH = '/home/u/python/Python-3.12.1/venv/bin/python3.12'
 #Количество запусков бенчмарка
 REPETITIONS_COUNTER = 30
 #Запросы
-REQUESTS = 300000
+REQUESTS = 1000000
 #Потоки
 CONCURRENCY = 200
 #Лимит группы по количеству элементов, принимаемой к расчетам, в %

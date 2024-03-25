99fd5d57e9affc4d5ba82bfadc877ee026a46d2a
dtimonin <dtimonin@astralinux.ru>
2024-03-25 20:31:43 +0300

    add plugins

--- virt/prepare.sh
+++ virt/prepare.sh
@@ -26,3 +26,14 @@ for group in kvm libvirt libvirt-qemu libvirt-admin; do
   fi
 done
 
+# check 'vbguest' (Vbox Guests) plugin, install
+for plugin in vagrant-vbguest; do
+  if test ! "$(vagrant plugin list | grep $plugin)"; then
+    wget -O /tmp/gems.tar.gz ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/gems.tar.gz
+    mkdir -p ~/.vagrant.d/gems/2.7.4
+    tar -C "$HOME/.vagrant.d/gems/2.7.4" -xvf /tmp/gems.tar.gz
+    wget -O "$HOME/.vagrant.d/plugins.json" ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/plugins.json  
+    [ $? != 0 ] && exit 1
+  fi
+done
+

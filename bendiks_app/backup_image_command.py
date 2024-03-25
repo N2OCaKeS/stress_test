71a77730bce9bb5f5dd8aa5ee2b570fc31647461
dtimonin <dtimonin@astralinux.ru>
2024-03-25 13:46:03 +0300

    bendiks_app - v2.13.4

--- bendiks_app/backup_image_command.py
+++ bendiks_app/backup_image_command.py
@@ -73,7 +73,9 @@ cz_comm = {
         '1.8.0.11':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore LowServer-180rc11 nvme0n1',
         '1.8.0.12':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
-                -l ru_RU.UTF-8 startdisk restore LowServer-180rc12 nvme0n1'
+                -l ru_RU.UTF-8 startdisk restore LowServer-180rc12 nvme0n1',
+        '1.8.0.13':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
+                -l ru_RU.UTF-8 startdisk restore LowServer-180rc13 nvme0n1'
         },
     'stand4':{
         '1.7.5':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
@@ -117,7 +119,9 @@ cz_comm = {
         '1.8.0.11':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
                 -l ru_RU.UTF-8 startdisk restore MiddleServer-180rc11 nvme0n1',
         '1.8.0.12':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
-                -l ru_RU.UTF-8 startdisk restore MiddleServer-180rc12 nvme0n1'
+                -l ru_RU.UTF-8 startdisk restore MiddleServer-180rc12 nvme0n1',
+        '1.8.0.13':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
+                -l ru_RU.UTF-8 startdisk restore MiddleServer-180rc13 nvme0n1'
         },
     'stand2':{
         '1.7.5':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h " 10.177.103.202" \

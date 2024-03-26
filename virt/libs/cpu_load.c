6da71eefb506e1701503d088e1be7432fae3c057
dtimonin <dtimonin@astralinux.ru>
2024-03-26 22:07:04 +0300

    add iostat cmd

--- virt/libs/cpu_load.c
+++ virt/libs/cpu_load.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
+#include <pthread.h>            // -lpthread
 #include <unistd.h>
 
 /* #define CPUID_EAX            0x80000002 */
@@ -59,7 +59,8 @@ void *steal_time_function(void *vargp)
     }
 
     while(1) {
-        fs = popen("top -b -n 1 | awk '/%Cpu/{print $10}'", "r");
+        // fs = popen("top -b -n 1 | awk '/%Cpu/{print $10}'", "r");
+        fs = popen("iostat -c | awk 'NR==4{print $5}'", "r");
 
         if (fgets(buff, 127, fs) != NULL) {
             fprintf(file, "Steal Time: %s", buff);

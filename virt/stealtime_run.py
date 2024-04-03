1949c013b363a42a5f83609d2ea80a6199186759
dtimonin <dtimonin@astralinux.ru>
2024-04-03 17:02:16 +0300

    upd objects

--- virt/stealtime_run.py
+++ virt/stealtime_run.py
@@ -1,22 +1,27 @@
 from virt_test import StealTime
 from virt_conf import LOW, HIGH, TESTDIR
 
+box = '1.8.0.14'
 
-test = StealTime(rc_vbox='1.8.0.14',
-                 vm_count=LOW,
-                 testdir=TESTDIR,
-                 load_type='low')
+low_load_test = StealTime(rc_vbox=box,
+                          vm_count=LOW,
+                          testdir=TESTDIR,
+                          load_type='low')
 
-test.prepare_and_start()
-test.vms_destroy()
-if test.results_processing() == LOW:
+high_load_test = StealTime(rc_vbox=box,
+                           vm_count=HIGH,
+                           testdir=TESTDIR,
+                           load_type='high')
+
+
+low_load_test.prepare_and_start()
+low_load_test.vms_destroy()
+if low_load_test.results_processing() == LOW:
     print('Low load test successfully done')
 
-test.vm_count = HIGH
-test.load_type = 'high'
-test.prepare_and_start()
-test.vms_destroy()
-if test.results_processing() == HIGH:
+high_load_test.prepare_and_start()
+high_load_test.vms_destroy()
+if high_load_test.results_processing() == HIGH:
     print('High load test successfully done')
 
 

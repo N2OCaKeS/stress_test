ad353bf38b6881f8edd46ac57b2b9d733b08dfa2
dtimonin <dtimonin@astralinux.ru>
2024-04-04 20:32:20 +0300

    add print

--- virt/virt_test.py
+++ virt/virt_test.py
@@ -86,7 +86,9 @@ class StealTime:
         cmd(f'vagrant mutate {box_name} libvirt --input-provider virtualbox --force-virtio')
 
         # create_vm
-        cmd(f'''UPDATE={box_name} BOX_URL={box_url} RC={self.rc_name} KERNEL={str(self.kernel)} 
+        print(f'''UPDATE={box_name} BOX_URL={box_url} RC={self.rc_name} KERNEL={self.kernel} 
+                  COUNT={self.vm_count} vagrant up --provider=libvirt''')
+        cmd(f'''UPDATE={box_name} BOX_URL={box_url} RC={self.rc_name} KERNEL={self.kernel} 
                 COUNT={self.vm_count} vagrant up --provider=libvirt''')
         
         self.vm_dates = {

3c5086adb3f37beb693c6bcbd108bbdbc1102be2
dtimonin <dtimonin@astralinux.ru>
2024-04-12 11:24:31 +0300

    add 1763 snap

--- bendiks_app/backup_image_command.py
+++ bendiks_app/backup_image_command.py
@@ -64,6 +64,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-4-qa-team-13-175UU1rc7 nvme0n1',
         '1.7.6.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore LowServer-176rc1 nvme0n1',
+        '1.7.6.3':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
+                -l ru_RU.UTF-8 startdisk restore LowServer-176rc3 nvme0n1',
         '1.8.0.14':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore LowServer-180rc14 nvme0n1'
         },
@@ -98,6 +100,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore stand-3-qa-team-13-175UU1rc7 nvme0n1',
         '1.7.6.1':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
                 -l ru_RU.UTF-8 startdisk restore MiddleServer-176rc1 nvme0n1',
+        '1.7.6.3':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
+                -l ru_RU.UTF-8 startdisk restore MiddleServer-176rc3 nvme0n1',
         '1.8.0.14':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.203" \
                 -l ru_RU.UTF-8 startdisk restore MiddleServer-180rc14 nvme0n1'
         },

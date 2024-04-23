420647b7ff63e93de275c41b8ac238616eed5a3d
dtimonin <dtimonin@astralinux.ru>
2024-04-23 17:58:29 +0300

    bendiks_app - v2.17.1

--- bendiks_app/backup_image_command.py
+++ bendiks_app/backup_image_command.py
@@ -66,6 +66,8 @@ cz_comm = {
                 -l ru_RU.UTF-8 startdisk restore LowServer-176rc1 nvme0n1',
         '1.7.6.3':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore LowServer-176rc3 nvme0n1',
+        '1.7.6.4':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
+                -l ru_RU.UTF-8 startdisk restore LowServer-176rc3-flavour nvme0n1',
         '1.8.0.14':'sudo drbl-ocs -g auto -e1 auto -e2 -r -x -j2 -k0 -sc0 -p reboot -h "10.177.103.204" \
                 -l ru_RU.UTF-8 startdisk restore LowServer-180rc14 nvme0n1'
         },

59ba7d9d12e453106da57c1dc34caf28208a7309
dtimonin <dtimonin@astralinux.ru>
2024-05-02 17:42:36 +0300

    add install exfatprogs

--- file_systems/prepare.sh
+++ file_systems/prepare.sh
@@ -2,9 +2,10 @@
 
 # create venv in script_dir
 sudo apt update
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 sudo apt-get install -y libffi-dev strace 
 sudo apt-get install -y python3-requests
+sudo apt-get install exfatprogs
 
 sudo mkdir /home/u/python
 cd /home/u/python

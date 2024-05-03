074cef2e0ce03823a9bf2eee63f3857a6161d5b4
dtimonin <dtimonin@astralinux.ru>
2024-05-03 16:15:11 +0300

    fix err

--- virt/libs/libtests.py
+++ virt/libs/libtests.py
@@ -61,7 +61,7 @@ class CreateVM:
             if true_key == False:
                 for i in dates['astra-version']['vagrant_box']:
                     if str(box).startswith('1.7'):
-                          if '1.7.5.o' in str(i):
+                          if '1.7.1.o' in str(i):
                             box_name = i['1.7.1.o'][0]
                             box_url = i['1.7.1.o'][1]
                     elif str(box).startswith('1.8'):

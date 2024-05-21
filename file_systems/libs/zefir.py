d0bc1629407764a833094bf7416b2b4c7e9281d2
dtimonin <dtimonin@astralinux.ru>
2024-05-21 20:10:55 +0300

    add T

--- file_systems/libs/zefir.py
+++ file_systems/libs/zefir.py
@@ -510,7 +510,8 @@ class ZefirResultTable:
         self.new_tab = self.new_tab.sort_values(by=['Режим', '№ стенда'], ascending=[True, True])
         self.new_tab = self.new_tab[[x for x in self.new_tab if x not in self.new_tab.columns[4:].sort_values()] 
                         + [x for x in self.new_tab.columns[4:].sort_values() if x in self.new_tab]]
-        self.new_tab.to_html('res.html', index=False)
+        self.new_tab = self.new_tab.T
+        self.new_tab.to_html('res.html', header=False)
 
         #Создаем новую html страницу
         html_string = '<p><h3 style="font-family: Century Gothic, sans-serif;"><b>{text}</b></h3></p>'

9d47f029c674f63be7203dcd871c794508f4322f
ivelikanov <ivelikanov@astralinux.ru>
2024-05-22 18:31:05 +0300

    fix prep

--- cluster_file_systems/prepare.sh
+++ cluster_file_systems/prepare.sh
@@ -21,7 +21,8 @@ for plugin in vagrant-vbguest; do
     wget -O /tmp/gems.tar.gz ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/gems.tar.gz
     mkdir -p /home/u/.vagrant.d/gems/2.7.4
     tar -C "/home/u/.vagrant.d/gems/2.7.4" -xvf /tmp/gems.tar.gz
-    wget -O "/home/u/.vagrant.d/plugins.json" ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/plugins.json  
+    wget -O "/home/u/.vagrant.d/plugins.json" ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/plugins.json
+    chown -R u:u /home/u/.vagrant.d
     [ $? != 0 ] && exit 1
   fi
 done

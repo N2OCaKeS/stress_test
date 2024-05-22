e99d7efbf3448abb37f91a1943f01a8f50212ec7
ivelikanov <ivelikanov@astralinux.ru>
2024-05-22 19:38:29 +0300

    fix prep

--- cluster_file_systems/prepare.sh
+++ cluster_file_systems/prepare.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+dpkg -s jq &> /dev/null || sudo apt-get install jq -y
+wget http://bendiks.devos.astralinux.ru/rest/api/get-repo-path -O releases.json
+sudo jq -r ".\"$2\"[]" releases.json > /etc/apt/sources.list
+cat << EOF | sudo tee /etc/apt/preferences.d/devel
+Package: *
+Pin: release l=devel
+Pin-Priority: 500
+
+Package: *
+Pin: release l=extended
+Pin-Priority: 500
+EOF
+sudo apt update
+
 #lvirt
 sudo apt-get install -y astra-kvm  # virt-manager libvirt-clients libvirt-daemon libvirt-dev libvirt0 -y
 sudo apt purge -y firewalld
@@ -44,7 +58,7 @@ sudo make altinstall
 python3.12 -m venv venv
 source venv/bin/activate
 
-cd /home/u/git/stress_test/cluster_file_systems
+cd /home/u/git/stress_test/$1
 python3.12 -m pip install --upgrade pip
 python3.12 -m pip install -r req.txt
 

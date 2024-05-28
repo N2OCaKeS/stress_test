33c1642b655f887baffa6b0b5cf64d335481a692
dtimonin <dtimonin@astralinux.ru>
2024-05-28 11:49:48 +0300

    fix err

--- bendiks_app/libs/libbend.py
+++ bendiks_app/libs/libbend.py
@@ -251,7 +251,7 @@ def info_collector(page, ajax=None):
                                 stand2_snap=cz_comm()['stand2'].keys(),
                                 stand3_snap=cz_comm()['stand3'].keys(),
                                 stand4_snap=cz_comm()['stand4'].keys(),
-                                stand4_snap=cz_comm()['stand5'].keys())
+                                stand5_snap=cz_comm()['stand5'].keys())
 
 
 

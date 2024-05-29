e30eb712177562dc1bd7328264446fce1e792f5b
dtimonin <dtimonin@astralinux.ru>
2024-05-29 14:07:44 +0300

    add install linux-tools

--- parsec/prepare.sh
+++ parsec/prepare.sh
@@ -23,6 +23,7 @@ sudo apt-get install -y python3-requests
 
 if test "$(grep -E '1.8.*' /etc/astra_version)"; then
     sudo apt-get install -y linux-tools-6.1*-generic
+    sudo apt-get install -y linux-tools-6.6*-generic
     sudo apt-get install -y linux-tools-common-6.*
 else
     sudo apt-get install -y linux-tools-5.10*-generic linux-tools-5.15*-generic linux-tools-common-5.15*

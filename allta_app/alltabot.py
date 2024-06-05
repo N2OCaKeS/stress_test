0216c095e2a2585321c72f7d36bcfa1bd5895977
dtimonin <dtimonin@astralinux.ru>
2024-06-05 17:59:38 +0300

    add outside url

--- allta_app/alltabot.py
+++ allta_app/alltabot.py
@@ -553,7 +553,7 @@ async def get_message(message: types.Message):
             await message.reply(help_text)
         else: 
             await bot.send_photo(chat_id=message.chat.id, 
-                                photo='http://allta.devos.astralinux.ru/rest/api/get-ping2-pic', 
+                                photo='https://thumbs.dreamstime.com/z/%D0%BF%D0%B8%D0%BD%D0%B3%D0%B2%D0%B8%D0%BD-%D0%B2-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%BC-%D1%88%D0%B0%D1%80%D1%84%D0%B5-%D0%BC%D0%B8%D0%BB%D1%8B%D0%B9-%D0%B8%D0%B7-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%B0-%D1%81-%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D0%BC%D0%B8-%D0%B3%D0%BB%D0%B0%D0%B7%D0%B0%D0%BC%D0%B8-231085136.jpg', 
                                 caption='Oops! Команда не идентифицирована.\nНапиши мне help, если нужна помощь', 
                                 reply_to_message_id=message.message_id)
     elif message.reply_to_message and message.reply_to_message.from_user:
@@ -564,7 +564,7 @@ async def get_message(message: types.Message):
                 await message.reply(help_text)
             else: 
                 await bot.send_photo(chat_id=message.chat.id, 
-                                    photo='http://allta.devos.astralinux.ru/rest/api/get-ping2-pic', 
+                                    photo='https://thumbs.dreamstime.com/z/%D0%BF%D0%B8%D0%BD%D0%B3%D0%B2%D0%B8%D0%BD-%D0%B2-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%BC-%D1%88%D0%B0%D1%80%D1%84%D0%B5-%D0%BC%D0%B8%D0%BB%D1%8B%D0%B9-%D0%B8%D0%B7-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%B0-%D1%81-%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D0%BC%D0%B8-%D0%B3%D0%BB%D0%B0%D0%B7%D0%B0%D0%BC%D0%B8-231085136.jpg', 
                                     caption='Oops! Команда не идентифицирована.\nНапиши мне help, если нужна помощь', 
                                     reply_to_message_id=message.message_id)
 

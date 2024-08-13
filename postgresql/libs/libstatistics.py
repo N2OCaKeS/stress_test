50a08e028c040332d53799eb46faa11f1d529356
dtimonin <dtimonin@astralinux.ru>
2024-08-13 16:35:09 +0300

    fix err

--- postgresql/libs/libstatistics.py
+++ postgresql/libs/libstatistics.py
@@ -9,7 +9,7 @@ class PSQLStatistics:
         
         self.username = username
         self.token = token
-        self.url = 'http://allta.devos.astralinux.ru:7777/base-statistcs'
+        self.url = 'http://allta.devos.astralinux.ru:7777/base-statistics'
 
     def update_statistics(self):
         data = {

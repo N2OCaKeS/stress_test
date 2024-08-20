2b179343a5bcae21ecfc869836e13c88286caf52
dtimonin <dtimonin@astralinux.ru>
2024-08-20 13:09:44 +0300

    add actual repo

--- postgresql/prepare.sh
+++ postgresql/prepare.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+18repo() {
+cat << EOF > /etc/apt/sources.list
+deb https://releases.devos.astralinux.ru/frozen/1.8/1.8.0/1.8.0.14/installation 1.8_x86-64 main contrib non-free
+deb https://releases.devos.astralinux.ru/frozen/1.8/1.8.0/1.8.0.14/extended-repository 1.8_x86-64 main contrib non-free
+deb https://releases.devos.astralinux.ru/frozen/1.8/1.8.0/1.8.0.14/devel-repository 1.8_x86-64 main contrib non-free
+EOF
+}
+
+17repo() {
+cat << EOF > /etc/apt/sources.list
+deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/1.7.1.8/installation/ 1.7_x86-64 main contrib non-free
+deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/1.7.1.8/base-repository/ 1.7_x86-64 main contrib non-free
+deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/1.7.1.8/update-repository/ 1.7_x86-64 main contrib non-free
+deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/EXT_latest/extended-repository/ 1.7_x86-64 main contrib non-free
+EOF
+}
+
+test "$(grep 1.7 /etc/astra_version)" && 17repo && sudo apt update
+test "$(grep 1.8 /etc/astra_version)" && 18repo && sudo apt update
+
 dpkg -s jq &> /dev/null || sudo apt-get install jq -y
 wget http://allta.devos.astralinux.ru/rest/api/get-repo-path -O releases.json
 sudo jq -r ".\"$2\"[]" releases.json > /etc/apt/sources.list

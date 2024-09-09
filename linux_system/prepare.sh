8b2271492496ba6fe8b8a871fb1a49e034b14917
dtimonin <dtimonin@astralinux.ru>
2024-09-09 19:13:28 +0300

    del multiselect

--- linux_system/prepare.sh
+++ linux_system/prepare.sh
@@ -11,7 +11,6 @@ EOF
 }
 
 17repo() {
-echo "grub-pc grub-pc/install_devices multiselect /dev/sda" | sudo debconf-set-selections
 cat << EOF > /etc/apt/sources.list
 deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/1.7.1.8/installation/ 1.7_x86-64 main contrib non-free
 deb https://releases.devos.astralinux.ru/frozen/1.7/1.7.1/1.7.1.8/base-repository/ 1.7_x86-64 main contrib non-free

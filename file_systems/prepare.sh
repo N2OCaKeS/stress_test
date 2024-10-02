2f77a61283694b03b361297143f371719202983b
dtimonin <dtimonin@astralinux.ru>
2024-10-02 12:44:20 +0300

    add pip hook

--- file_systems/prepare.sh
+++ file_systems/prepare.sh
@@ -36,6 +36,9 @@ source venv/bin/activate
 cd /home/u/git/stress_test/$1
 python3.12 -m pip install --upgrade pip
 python3.12 -m pip install -r req.txt
+if [[ $? != 0 ]]; then
+    python3.12 -m pip install -r req.txt
+fi
 
 sudo mkdir /home/u/modules
 sudo wget -P /home/u/modules ftp://10.177.103.10/modules/*

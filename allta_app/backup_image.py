ca433c8740b247936c3f27cadd8056fffb57cf92
dtimonin <dtimonin@astralinux.ru>
2024-10-03 15:36:36 +0300

    change clonezilla balance snapshot to 1.8.1.6

--- allta_app/backup_image.py
+++ allta_app/backup_image.py
@@ -210,8 +210,8 @@ port = 22
 ipmi = BootOrder(stand=args.STAND)
 clonezilla_command = cz_comm()[args.STAND][args.RELEASE]
 if args.PSQL_BALANCE:
-    #clonezilla_command_balance = cz_comm()['stand4']['1.8.0.14']
-    clonezilla_command_balance = cz_comm()['stand4'][args.RELEASE]
+    clonezilla_command_balance = cz_comm()['stand4']['1.8.1.6']
+    #clonezilla_command_balance = cz_comm()['stand4'][args.RELEASE]
 branch = args.BRANCH
 parent_page = args.PARP
 systems = ['debian10', 'debian10-5.15', 'altlinux-5.10']

8b4bc65f13702d762b661693f85b1438498e638f
ivelikanov <ivelikanov@astralinux.ru>
2024-10-14 12:15:51 +0300

    changed url statistics

--- postgresql/libs/libstatistics.py
+++ postgresql/libs/libstatistics.py
@@ -9,14 +9,14 @@ class PSQLStatistics:
         
         self.username = username
         self.token = token
-        self.url = 'http://allta.devos.astralinux.ru:7777/base-statistics'
+        self.url = 'http://allta.devos.astralinux.ru:7777/postgresql-statistics'
 
     def update_statistics(self):
         data = {
             'title_statistics':'PostgreSQL',
             'username':self.username,
             'token':self.token,
-            'set_of_test_types':['postgresql', 'postgresql-sm', 'postgresql-aud-off', 'psql_parsec', 'psql_vanilla', 'tantor_vanilla'],
+            'set_of_test_types':['postgresql', 'postgresql-sm', 'postgresql-aud-off', 'psql_parsec', 'psql_vanilla', 'tantor_vanilla', 'psql_balance'],
             'comparison_list':[
                 ['postgresql', 'postgresql-sm'], 
                 ['postgresql', 'postgresql-aud-off'], 

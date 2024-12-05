5f5a69d1a106092a3f5ae1fff5cc14ea8d35aac3
dtimonin <dtimonin@astralinux.ru>
2024-12-05 18:58:23 +0300

    fix some errs

--- freeipa/locust/locust_prepare.sh
+++ freeipa/locust/locust_prepare.sh
@@ -2,9 +2,31 @@
 
 
 sudo apt update
-sudo apt-get install -y libsasl2-dev libldap2-dev libkrb5-dev gcc python3-dev libsasl2-modules-gssapi-mit krb5-config krb5-user
+sudo apt-get install -y libsasl2-dev libldap2-dev libkrb5-dev gcc python3-dev libsasl2-modules-gssapi-mit krb5-config krb5-user python3-pip
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install -y libffi-dev strace
+sudo apt-get install -y python3-requests
 
 
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+sudo mkdir /home/u/python
+cd /home/u/python
+sudo wget -P /home/u/python ftp://10.177.103.10/python/*
+tar -xf Python-3.12.1.tar.xz
+cd Python-3.12.1
+./configure --enable-optimizations
+make -j 6
+sudo make altinstall
+
+python3.12 -m venv venv
+source venv/bin/activate
+
+cd /home/u/git/stress_test/freeipa
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install -r locust/requirements.txt
+if [[ $? != 0 ]]; then
+    python3.12 -m pip install -r locust/requirements.txt
+fi
+
+
+
 
--- freeipa/locust_run.py
+++ freeipa/locust_run.py
@@ -1,10 +1,11 @@
-from libs.libipa import (remote_put_file,
-                         remote_exec)
-
+from libs.libipa import remote_put_file
+from time import sleep
+from fabric import Connection
 
 
 AV = '1.7.6.UU.2.1'
 USER = 'u'
+PASSWORD = '1'
 HOSTS = {
     'server': {
                 'ip': '10.177.103.204'
@@ -19,6 +20,29 @@ HOSTS = {
 
 
 
+def host_is_available(node):
+    try:
+        with Connection(host=HOSTS[node]['ip'],
+                        user=USER,
+                        connect_kwargs={"password": PASSWORD}) as node_client:
+            if str(node_client.run('uptime')):
+                return True
+    except Exception:
+        return False
+
+def remote_exec(command, node="server"):
+    while host_is_available(node) == False:
+        sleep(1)
+        print("{host} временно не доступен...повторная попытка подключения...".format(host=HOSTS[node]['ip']))
+    try:
+        with Connection(host=HOSTS[node]['ip'],
+                            user=USER,
+                            connect_kwargs={"password": PASSWORD}) as node_client:
+            return str(node_client.run(command))
+    except Exception as err:
+        return "Что то пошло не так...{message}".format(message=err)
+
+
 remote_put_file(HOSTS['server']['ip'], f'/home/{USER}/ipa_conf.py', "ipa_conf.py")
 remote_put_file(HOSTS['server']['ip'], f'/home/{USER}/ipa_init_dc.py', "ipa_init_dc.py")
 remote_put_file(HOSTS['server']['ip'], f'/home/{USER}/prepare.sh', "prepare.sh")
@@ -26,3 +50,13 @@ remote_exec(f"sudo bash prepare.sh {AV}", 'server')
 remote_exec("sudo python3 ipa_init_dc.py", 'server')
 
 
+
+
+
+#locust -f ../locust/locust_file.py --use_bind --use_unbind --headless -u 10 --run-time 30s
+
+#for i in {1..25}; do nohup locust -f ../locust/locust_file.py --worker --master-host=localhost & done
+#locust -f ../locust/locust_file.py --master --headless -u 10 --run-time 30s
+
+
+#sudo locust -f ../locust/locust_file.py --process 10 --headless -u 20 --run-time 2m

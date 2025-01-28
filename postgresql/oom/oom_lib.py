5240c9bd851f5c0296e77e8d656362c333792087
dtimonin <dtimonin@astralinux.ru>
2025-01-28 10:19:25 +0300

    fix errs

--- postgresql/oom/oom_lib.py
+++ postgresql/oom/oom_lib.py
@@ -33,7 +33,7 @@ class VBox():
         return system.cmd_with_returncode("sudo bash balance/bl_prepare_vbox.sh")
 
     @classmethod
-    def build(cls, box_name: str, box_url: str, kernel: str, rc: str, vms: list) -> int:
+    def build(cls, box_name: str, box_url: str, kernel: str, rc: str) -> int:
         def _check_vm_list():
             return system.check_output_command('vboxmanage list vms')
                 
@@ -49,7 +49,7 @@ class VBox():
         
         if system.cmd_with_returncode(f'cd oom && vagrant box add {box_name} {box_url} --force') != 0:
             return 1
-        if system.cmd_with_returncode(f'cd oom && UPDATE={box_name} BOX_URL={box_url} vagrant up --provider=virtualbox') != 0:
+        if system.cmd_with_returncode(f'cd oom && UPDATE={box_name} BOX_URL={box_url} RC={rc} vagrant up --provider=virtualbox') != 0:
             return 1
     
         return 0
--- postgresql/oom_run.py
+++ postgresql/oom_run.py
@@ -33,5 +33,5 @@ with open('box-config.json', 'r') as r:
 
 box_name, box_url = bl.box_wrapper(args.SET_BOX, dates)
 
-provider.prepare
-provider.build
+provider.prepare()
+provider.build(box_name=box_name, box_url=box_url, kernel='kernel', rc=args.SET_BOX)

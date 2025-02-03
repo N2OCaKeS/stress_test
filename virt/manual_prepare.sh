d00695ce0df1f548c7820db23c370487daac0160
mfilippenko <mfilippenko@astralinux.ru>
2025-02-03 15:29:40 +0300

    refactor manual_prepare.sh for improved pip installation based on Astra build version

--- virt/manual_prepare.sh
+++ virt/manual_prepare.sh
@@ -7,8 +7,20 @@ if [ "$1" == "debian" ] || [ "$1" == "astra" ]; then
     wget -r -nH --cut-dirs=2 --no-parent ftp://qa111.devos.astralinux.ru/packages/vagrant
     sudo dpkg -i vagrant_2.2.19_x86_64.deb
     $pm install pip -y
-    python3 -m pip install --upgrade pip --break-system-packages
-    python3 -m pip install -r req.txt --break-system-packages
+    if [ "$1" == "astra"]; then
+        if [ grep 1.8 /etc/astra/build_version ]; then
+            python3 -m pip install --upgrade pip --break-system-packages
+            python3 -m pip install --upgrade setuptools wheel --break-system-packages
+            python3 -m pip install -r req.txt --break-system-packages
+        elif [ grep 1.7 /etc/astra/build_version ]; then
+            python3 -m pip install --upgrade pip
+            python3 -m pip install --upgrade setuptools wheel
+            python3 -m pip install -r req.txt
+        fi
+    fi
+    # python3 -m pip install --upgrade pip --break-system-packages
+    # python3 -m pip install --upgrade setuptools wheel
+    # python3 -m pip install -r req.txt --break-system-packages
     $pm install virt-manager libvirt-clients libvirt-daemon libvirt-dev libvirt0 -y
 elif [ "$1" == "alt" ]; then
     pm=apt-get
@@ -37,20 +49,20 @@ fi
 sudo adduser $USER libvirt
 
 for group in kvm libvirt libvirt-qemu libvirt-admin; do
-  if test ! "$(groups | grep ${group})"; then
-    sudo usermod -aG ${group} $USER 
-  fi
+    if test ! "$(groups | grep ${group})"; then
+        sudo usermod -aG ${group} $USER
+    fi
 done
 
 # check 'vbguest' (Vbox Guests) plugin, install
 for plugin in vagrant-vbguest; do
-  if test ! "$(vagrant plugin list | grep $plugin)"; then
-    wget -O /tmp/gems.tar.gz ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/gems.tar.gz
-    mkdir -p ~/.vagrant.d/gems/2.7.4
-    tar -C "$HOME/.vagrant.d/gems/2.7.4" -xvf /tmp/gems.tar.gz
-    wget -O "$HOME/.vagrant.d/plugins.json" ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/plugins.json  
-    [ $? != 0 ] && exit 1
-  fi
+    if test ! "$(vagrant plugin list | grep $plugin)"; then
+        wget -O /tmp/gems.tar.gz ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/gems.tar.gz
+        mkdir -p ~/.vagrant.d/gems/2.7.4
+        tar -C "$HOME/.vagrant.d/gems/2.7.4" -xvf /tmp/gems.tar.gz
+        wget -O "$HOME/.vagrant.d/plugins.json" ftp://qa111.devos.astralinux.ru/packages/vagrant-plugins/plugins.json
+        [ $? != 0 ] && exit 1
+    fi
 done
 
 rm Vagrantfile
@@ -58,6 +70,6 @@ mv manual_Vagrantfile Vagrantfile
 
 if [[ $(egrep -c '(vmx|svm)' /proc/cpuinfo) -gt 0 ]]; then
     echo "supports hardware virtualization is ok"
-else 
-    echo "system does not supports hardware virtualization" 
+else
+    echo "system does not supports hardware virtualization"
 fi

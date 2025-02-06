6045fa4295ad4bb1068a98e7d226ff83ae8c7c0e
dtimonin <dtimonin@astralinux.ru>
2025-02-06 14:01:02 +0300

    add wait

--- allta_infocollector/src/handler/import_dashboard.sh
+++ allta_infocollector/src/handler/import_dashboard.sh
@@ -38,7 +38,7 @@ j2=$(jq '.' ./allta_dashboard.json)
 echo "{\"dashboard\": ${j2},\"overwrite\":${GRAFANA_OVERWRITE},\"inputs\": [{\"name\":\"DS_PROMETHEUS\",\"type\":\"datasource\", \"pluginId\":\"prometheus\",\"value\":\"${DS_NAME}\"}],\"folderUid\": \"\"}" > payload2.json
 
 echo waiting...
-sleep 3
+sleep 15
 
 curl -v -k -u "$GRAFANA_CRED" \
   -H "Accept: application/json" \

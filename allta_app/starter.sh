884a66d6b673a682e481311bc4532d3c560e7a2a
dtimonin <dtimonin@astralinux.ru>
2025-02-26 12:53:55 +0300

    allta_app - v2.35.13

--- allta_app/starter.sh
+++ allta_app/starter.sh
@@ -63,7 +63,8 @@ git checkout $1
 #Настраиваем окружение и запускаем тест
 cd $1
 #sed -i '2i export DEBIAN_FRONTEND=noninteractive' prepare.sh
-#echo curl http://10.177.103.10:18181/rest/api/dashboard/$localhost/full >> prepare.sh
+echo curl http://10.177.103.10:18181/rest/api/dashboard/$localhost/full >> prepare.sh
+echo sed -i 's/.*cgroup_controllers.*/cgroup_controllers = [ "cpu", "devices", "memory", "blkio", "cpuacct" ]/g' /etc/libvirt/qemu.conf >> prepare.sh
 bash prepare.sh $1 $3 $5
 
 if [ "$4" == "kernel" ]; then

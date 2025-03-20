8adb361f993984103240c4a331968b402550ba3d
mfilippenko <mfilippenko@astralinux.ru>
2025-03-20 14:19:12 +0300

    add vm and prepare

--- postgresql/new_balance/main.py
+++ postgresql/new_balance/main.py
@@ -1,1 +1,17 @@
-import allta
+from allta import VBoxManager
+from .roles.role_settings import vms_groups, vms, vms_dates, domain
+
+from .task.apt import apt_install
+
+
+
+
+
+provision = './provision.sh'
+prepare = './prepare/prepare.sh'
+vagrant_path = './'
+
+prov = VBoxManager()
+# prov.prepare (prepare)
+# prov.build(vagrant_path, '1.8.0.s', '1.8.0', vms, vms_dates, provision) # TODO Переработать версию
+prov.apt.install(apt_install, vms_dates, vms_groups)
---
+++ postgresql/new_balance/task/apt.py
@@ -0,0 +1,11 @@
+apt_install = {
+    'g_domain_client': ['astra-freeipa-client'],
+    'g_database': [f'postgresql-15'],
+    'g_load_balaner': ['pgpool2'],
+    'domain': ['astra-freeipa-server']
+}
+
+# apt_remove = {
+
+
+# }

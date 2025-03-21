f1ddfccf84b6c4882f16b990d686f1bc83b6844f
mfilippenko <mfilippenko@astralinux.ru>
2025-03-21 15:19:59 +0300

    error

--- postgresql/new_balance/main.py
+++ postgresql/new_balance/main.py
@@ -1,17 +1,17 @@
 from allta import VBoxManager
-from .roles.role_settings import vms_groups, vms, vms_dates, domain
+from roles.role_settings import vms_groups, vms, vms_dates, domain
 
-from .task.apt import apt_install
-
-
+from task.apt import apt_install
 
 
 
-provision = './provision.sh'
+
+
+provision = './provision/provision.sh'
 prepare = './prepare/prepare.sh'
 vagrant_path = './'
 
 prov = VBoxManager()
 # prov.prepare (prepare)
-# prov.build(vagrant_path, '1.8.0.s', '1.8.0', vms, vms_dates, provision) # TODO Переработать версию
-prov.apt.install(apt_install, vms_dates, vms_groups)
+prov.build(vagrant_path, '1.8.0.s', '1.8.0', vms, vms_dates, provision) # TODO Понять почему после сборки ВМ не доступны через shh по ip bridge
+#prov.apt.install(apt_install, vms_dates, vms_groups)

853655700d4ed6944d2a21909e5bcf757794d711
ivelikanov <ivelikanov@astralinux.ru>
2025-04-02 13:01:41 +0300

    added getting repository by version

---
+++ testrun_changelog/utils.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+def fetch_repositry(version):
+    try:
+        res = requests.get("http://allta.devos.astralinux.ru/rest/api/get-repo-path")
+    except requests.exceptions.RequestException as e:
+            print(f"Ошибка при получении данных с http://allta.devos.astralinux.ru/rest/api/get-repo-path: {e}")
+    info_version_repo = json.loads(res.text)
+    print(info_version_repo[version])
+
+fetch_repositry("1.8.2.6")

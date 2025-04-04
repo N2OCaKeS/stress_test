95844e84e0dc7627dc6d194566d0df98ece30d0f
ivelikanov <ivelikanov@astralinux.ru>
2025-04-04 21:39:57 +0300

    added api, container prepared

--- testrun_changelog/package_groups.py
+++ testrun_changelog/package_groups.py
@@ -1,11 +1,11 @@
 GROUPS = {
-    'Parsec': ['drbd'],
-    'UnixBench': ['linux-5.15', 'linux-6.1', 'linux-6.12'],
-    'Docker/Podman/LXC': [],
+    'Parsec': ['parsec', 'parsec-sumic', 'libparsec-cap3', 'libparsec-mac-db-sssd3', 'parsec-su', 'pdp', 'libparsec-cap-db-sssd3', 'libparsec-mic3', 'libparsec-aud-db-ipa3', 'parsec-base', 'parsec-sumac', 'libparsec-aud-db-sssd3', 'libparsec-aud3', 'parsec-mac', 'parsec-sudo', 'libparsec-base3', 'libparsec-mic-db-sssd3', 'libparsec-mac3', 'parsec-tools', 'parsec-aud', 'parsec-cap', 'libparsec-db-sssd3', 'libpdac++', 'libpdp', 'libparsec-cap3-dev', 'libparsec-aud3-dev', 'libpdp-dev', 'libpdac++-dev', 'libparsec-mic3-dev', 'libparsec-base3-dev', 'pdp-dev', 'parsec-dev', 'libparsec-mac3-dev'],
+    'UnixBench': ['linux-5.10', 'linux-5.15', 'linux-6.1', 'linux-6.12', 'xfsprogs', 'exfat-utils', 'e2fsprogs', 'dosfstools', 'ntfs-3g', 'libc6'],
+    'Docker/Podman/LXC': ['docker.io', 'podman', 'containerd', 'runc'],
     'Apache': ["apache2"],
     'FreeIPA': ["astra-freeipa-server", "astra-freeipa-client", "freeipa-client", "freeipa-server"],
     'PostgreSQL': ["postgresql"],
-    'Qemu/KVM/Libvirt': ["astra-kvm"],
+    'Qemu/KVM/Libvirt': ["astra-kvm", 'astra-kvm-secure', 'qemu-kvm', 'qemu-utils', 'bridge-utils', 'libvirt-daemon', ' libvirt-daemon-system', ' virtiofsd'],
     'Системные службы': ['systemd', 'syslog-ng', 'auditd'],
     'Файловые системы': ['xfsprogs', 'exfat-utils', 'e2fsprogs', 'dosfstools', 'ntfs-3g']
 }

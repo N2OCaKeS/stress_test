64359c36d0f43f775ca1f47906cdfd8eff9ec579
amedvedev <amedvedev@astralinux.ru>
2025-04-21 17:41:39 +0300

    upd run.py

--- docker/run.py
+++ docker/run.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 import argparse
 from docker_conf import VENV_PATH
-
+from libs.zefir import UploaderZC
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--test",
@@ -10,6 +10,76 @@ parser.add_argument("-t", "--test",
                     choices=["web"],
                     help="Choose test name.",
                     dest="TEST")
+parser.add_argument('-u', '--username',
+                    action='store',
+                    required=True,
+                    help='confluence user',
+                    dest='USER')
+
+parser.add_argument('-t', '--token',
+                    action='store',
+                    required=False,
+                    default=None,
+                    help='confluence access token',
+                    dest='TOKEN')
+
+parser.add_argument('-cs', '--confluence-space',
+                    action='store',
+                    required=True,
+                    help='confluence space',
+                    dest='SPACE')
+
+parser.add_argument('-cpp', '--confluence-parent-page',
+                    action='store',
+                    required=True,
+                    help='confluence parent page',
+                    dest='PPAGE')
+
+parser.add_argument('-cnp', '--confluence-new-page',
+                    action='store',
+                    required=True,
+                    help='confluence new page',
+                    dest='NPAGE')
+
+parser.add_argument('-sn', '--stand-num',
+                    action='store',
+                    choices=['1',
+                             '2',
+                             '3',
+                             '4'],
+                    required=True,
+                    help='stand num',
+                    dest='STAND')
+
+parser.add_argument('-fti', '--folder-tree-id',
+                    action='store',
+                    required=True,
+                    help='folder-tree-id',
+                    dest='FTI')
+
+parser.add_argument('-tcyc', '--test-cycle-name',
+                    action='store',
+                    required=True,
+                    help='test-cycle-name',
+                    dest='TCYC')
+
+parser.add_argument('-tcas', '--test-case-name',
+                    action='store',
+                    required=True,
+                    help='test-case-name',
+                    dest='TCAS')
+
+parser.add_argument('-ba', '--basic-auth',
+                    action='store',
+                    required=True,
+                    help='basic-auth',
+                    dest='BA')
+
+parser.add_argument('-tcv', '--test-cycle-version',
+                    action='store',
+                    required=True,
+                    help='test-cycle-version',
+                    dest='TCV')
 args = parser.parse_args()
 
 
@@ -28,13 +98,33 @@ def run_command(command):
         print("Команда завершилась с ошибкой:")
         print(e)
         return False
+    
 
+if __name__ == "__main__":
 
-if args.TEST == "web":
-    run_command("cd ./site && bash start.sh final")
-    run_command(f"source {VENV_PATH}/activate && python3 report.py")
-    run_command(f"source {VENV_PATH}/activate && python3 publish.py")
-else: "Тест не найден"
+    uzs = UploaderZC(folder_tree_id=args.FTI,
+                 test_cycle_name=args.TCYC,
+                 test_case_name=args.TCAS,
+                 basic_auth=args.BA,
+                 test_cycle_version=args.TCV,
+                 token=args.TOKEN,
+                 username=args.USER,
+                 conf_space=args.SPACE,
+                 conf_parent_page=args.PPAGE,
+                 conf_new_page_name=args.NPAGE,
+                 grade_stand=args.STAND)
+
+    uzs.upload_test_cycle_status('progress')
+    
+    """
+        TODO Здесь запускаем тесты
+    """
+
+    if args.TEST == "web":
+        run_command("cd ./site && bash start.sh final")
+        run_command(f"source {VENV_PATH}/activate && python3 report.py")
+        run_command(f"source {VENV_PATH}/activate && python3 publish.py")
+    else: "Тест не найден"
 
 
 

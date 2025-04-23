8e8549343aaad263a3ea2c36ad66efaccdbaf8a2
ivelikanov <ivelikanov@astralinux.ru>
2025-04-23 19:17:05 +0300

    fixes bugs

--- drblsrv.py
+++ drblsrv.py
@@ -9,10 +9,10 @@ child.sendline('N')
 
 child.expect(r'Do you want to use the serial console output on the client computer.*\?.*\[y/N\]')
 child.sendline('N')
-child.expect(r'Do you want to upgrade the operating system\?.*\[y/N\]', timeout=300)
+child.expect(r'Do you want to upgrade the operating system\?.*\[y/N\]', timeout=100)
 child.sendline('N')
-child.expect(r'\[1\]:.*kernel', timeout=300)
-child.sendline('2')
+child.expect(r'\[1\]:.*kernel.*from this DRBL server.*', timeout=300)
+child.sendline('1')
 try:
     child.expect(pexpect.EOF, timeout=600)
 except pexpect.TIMEOUT:

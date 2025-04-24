b1f5cb6ac7a2e6851d8aa2d62be4b5bc6e6c6f6b
amedvedev <amedvedev@astralinux.ru>
2025-04-24 12:51:42 +0300

    upd venv

--- docker/run.py
+++ docker/run.py
@@ -15,4 +15,4 @@ args = parser.parse_args()
 with open(f'/home/u/{args.NAME}', 'r') as r:
     dates = r.read()
 
-subprocess.run(f'sudo {VENV_PATH} wa_run.py {dates}', shell=True)
+subprocess.run(f'sudo {VENV_PATH}/python3.12 wa_run.py {dates}', shell=True)

ca90957d043a1789307ad410c2b9cb2d43a803bc
mfilippenko <mfilippenko@astralinux.ru>
2025-05-26 16:05:49 +0300

    change vbox to libvirt

---
+++ allta_app/make/vm_prepare/libvirt_vm.py
@@ -0,0 +1,71 @@
+from allta import Libvirt, LibvirtManager, SystemCommands
+
+
+
+Libvirt.prepare()
+kernel = "5.10.190-1-generic"
+rc = "1.7.5"
+
+vms = {  # Полный список ВМ
+    'virtual-station1': {'host-port': '22',
+                  'ip': '10.0.0.11',
+                  'sshnum': '',
+                  'ip_bridge': '10.177.103.111',
+                  'cpu': '2',
+                  'ram': '4096'},
+    'virtual-station2': {'host-port': '22',
+                  'ip': '10.0.0.12',
+                  'sshnum': '1',
+                  'ip_bridge': '10.177.103.112',
+                  'cpu': '12',
+                  'ram': '256000'},
+    'virtual-station3': {'host-port': '22',
+                  'ip': '10.0.0.13',
+                  'sshnum': '2',
+                  'ip_bridge': '10.177.103.113',
+                  'cpu': '12',
+                  'ram': '256000'},
+    'virtual-station4': {'host-port': '22',
+              'ip': '10.0.0.41',
+              'sshnum': '3',
+              'ip_bridge': '10.177.103.141',
+              'cpu': '12',
+              'ram': '256000'}
+}
+
+vms_list = ['virtual-station1', 'virtual-station2', 'virtual-station3', 'virtual-station4']
+
+group = {'all': ['virtual-station1', 'virtual-station2', 'virtual-station3', 'virtual-station4',]}
+new_vms = Libvirt.build('1.7.5.o', '1.7.5', vms)
+
+scp_prepare = {
+    "g_all": [
+        {
+            'mode': 'push',
+            'path_host': './provision/env_provision.sh',
+            'path_vm': '/home/u/env_provision.sh'            
+        }
+    ]
+}
+
+Libvirt.scp(scp_settings=scp_prepare, vms_dates=new_vms, vms_groups=group, username='u', password='1')
+
+prepare = {}
+
+for vm_name in vms:
+    prepare[vm_name] = {
+        'set hostname': {
+            'command': f"sudo hostnamectl set-hostname {vm_name} && if grep -q '^127\\.0\\.1\\.1' /etc/hosts; then sudo sed -i 's/^127\\.0\\.1\\.1.*/127.0.1.1\\t{vm_name}/' /etc/hosts; else echo -e '127.0.1.1\\t{vm_name}' | sudo tee -a /etc/hosts; fi",
+            'signal set': 'hostname',
+            'signal get': ''
+        },
+        'prepare': {
+            'command': f"sudo chmod 777 /home/u/env_provision.sh && sudo su -c '/home/u/env_provision.sh {vm_name} {kernel} {rc}'",
+            'signal set': '',
+            'signal get': ['hostname']
+        }
+    }
+Libvirt.execute(commands=prepare, vms_dates=new_vms, vms_groups=group, username='u', password='1')
+
+LibvirtManager.create_snapshot(vms_list, "Snapshot_1_7_5")
+
---
+++ allta_app/make/vm_prepare/network.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Использование: ./recreate_bridge_with_mac.sh <bridge_name>
+# Пример: ./recreate_bridge_with_mac.sh br0
+
+declare -A vm_mac_map=(
+    ["virtual-station1"]="080027ABCD01"
+    ["virtual-station2"]="080027ABCD02"
+    ["virtual-station3"]="080027ABCD03"
+    ["virtual-station4"]="080027ABCD04"
+)
+
+BRIDGE="br0"
+
+if [[ -z "$BRIDGE" ]]; then
+    echo "Использование: $0 <bridge_name>"
+    exit 1
+fi
+
+for VM in "${!vm_mac_map[@]}"; do
+    MAC="${vm_mac_map[$VM]}"
+    echo "============================"
+    echo "[*] Работаем с ВМ: $VM (MAC: $MAC)"
+
+    TMP_XML="/tmp/${VM}_bridge.xml"
+
+    # Остановить ВМ если работает
+    STATE=$(virsh -c qemu:///system domstate "$VM" 2>/dev/null)
+    if [[ "$STATE" != "shut off" ]]; then
+        echo "  ...Останавливаем $VM"
+        virsh -c qemu:///system -c qemu:///system destroy "$VM"
+        while [[ $(virsh -c qemu:///system -c qemu:///system domstate "$VM" 2>/dev/null) != "shut off" ]]; do
+            echo "    ...Ожидание выключения $VM"
+            sleep 2
+        done
+    fi
+
+    # Экспортируем XML-конфиг
+    virsh -c qemu:///system dumpxml "$VM" > "$TMP_XML"
+
+    # Удаляем все interface секции и вставляем новый bridge-интерфейс с нужным MAC
+    awk -v mac="$MAC" -v bridge="$BRIDGE" '
+    BEGIN {ins=0}
+    /<devices>/ {
+        print
+        print "    <interface type=\"bridge\">"
+        print "      <mac address=\""mac"\"/>"
+        print "      <source bridge=\""bridge"\"/>"
+        print "      <model type=\"virtio\"/>"
+        print "    </interface>"
+        ins=1
+        next
+    }
+    /^ *<interface /, /^ *<\/interface>/ {next}
+    {print}
+    ' "$TMP_XML" > "${TMP_XML}.mod"
+
+    # Обновляем определение ВМ
+    virsh -c qemu:///system define "${TMP_XML}.mod"
+
+    # Запускаем обратно ВМ
+    virsh -c qemu:///system start "$VM"
+
+    echo "[+] $VM теперь использует только bridge $BRIDGE с MAC $MAC."
+done
+
+echo "============================"
+echo "[!] Массовое пересоздание сети завершено."
+
+
+

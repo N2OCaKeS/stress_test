3d640f1b95fdc8199ed6295ad7000c894bd32479
mfilippenko <mfilippenko@astralinux.ru>
2025-05-26 17:32:09 +0300

    update dependencies

--- allta_app/make/vm_prepare/vms_prepare.sh
+++ allta_app/make/vm_prepare/vms_prepare.sh
@@ -31,18 +31,36 @@ sudo ifup $BRIDGE
 echo "[+] Сеть перезапущена. Проверь IP: ip a show $BRIDGE"
 
 
+# create venv in script_dir
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt-get install -y libffi-dev strace
+sudo apt-get install -y python3-requests
+if test "$(grep -E '1.8.*' /etc/astra_version)"; then
+    sudo apt-get install -y linux-tools-6.1*-generic
+    sudo apt-get install -y linux-tools-6.6*-generic
+else
+    sudo apt-get install -y linux-tools-5.10*-generic linux-tools-5.15*-generic linux-tools-common-5.15*
+    sudo apt-get install -y linux-tools-5.15*-lowlatency
+    sudo apt-get install -y libssl1.1 psmisc
+fi
+
 sudo mkdir /home/u/python
 cd /home/u/python
 sudo wget -P /home/u/python ftp://10.177.103.10/python/*
 tar -xf Python-3.12.1.tar.xz
 cd Python-3.12.1
-
+./configure --enable-optimizations
+make -j 6
+sudo make altinstall
 
 python3.12 -m venv venv
 source venv/bin/activate
 pip install -i http://10.177.103.10:3141/root/release --trusted-host 10.177.103.10:3141 allta
 
+
 cd /home/u/git/stress_test/allta_app/make/vm_prepare
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt-get install astra-kvm wget tar -y
+sudo usermod -aG kvm,libvirt,libvirt-qemu $USER
 
 # СБОРКА ВМ
 python libvirt_vm.py

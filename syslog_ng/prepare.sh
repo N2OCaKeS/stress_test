9c8a0cf5e8b864912a4b796283ee655ad4dbf585
dtimonin <dtimonin@astralinux.ru>
2025-06-11 17:29:13 +0300

    upd prepare method on allta

--- syslog_ng/prepare.sh
+++ syslog_ng/prepare.sh
@@ -62,6 +62,7 @@ sudo make altinstall
 
 python3.12 -m venv venv
 source venv/bin/activate
+pip install -i http://10.177.103.10:3141/root/release --trusted-host 10.177.103.10:3141 allta
 
 cd /home/u/git/stress_test/$1
 python3.12 -m pip install --upgrade pip

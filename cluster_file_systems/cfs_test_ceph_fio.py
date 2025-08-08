2b583001d59fd45f4e45036da2d945ff87e515c1
ivelikanov <ivelikanov@astralinux.ru>
2025-08-08 19:04:32 +0300

    fio small fixes

--- cluster_file_systems/cfs_test_ceph_fio.py
+++ cluster_file_systems/cfs_test_ceph_fio.py
@@ -1,16 +1,21 @@
-import subprocess
+import argparse
 
 from libs.libcfs import check_output_command
 from cfs_conf import STORAGE_MOUNT_DIR
 
 class CephFIOTest:
 
-    def __init__(self):
+    def __init__(self, abv):
         self.size = "1G"
         self.directory = STORAGE_MOUNT_DIR
         self.runtime = 60 # в секундах
         self.blocksize = "4k"
         self.report_file = "/var/tmp/report_fio.txt"
+        self.abv = abv
+        if self.abv.startswith("1.8"):
+            check_output_command(command="dpkg -i /var/tmp/fio_3.33-3_amd64.deb")
+        else:
+            check_output_command(command="dpkg -i /var/tmp/fio_3.12-2_amd64.deb")
 
     def run_test(self):
         com_test = f"sudo fio --directory={self.directory} --direct=1 --rw=randrw --bs={self.blocksize} --ioengine=libaio --iodepth=256 --size={self.size} --runtime={self.runtime} --numjobs=3 --time_based --group_reporting --name=iops-qateam13-job --eta-newline=1 > {self.report_file}"
@@ -18,5 +23,14 @@ class CephFIOTest:
 
 
 if __name__ == "__main__":
-    fio_test = CephFIOTest()
+    DESCRIPTION = ""
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-abv', 
+                    action='store',
+                    required=True,
+                    help='astra build version',
+                    dest='ABV')
+    args = parser.parse_args()
+
+    fio_test = CephFIOTest(abv=args.ABV)
     fio_test.run_test()

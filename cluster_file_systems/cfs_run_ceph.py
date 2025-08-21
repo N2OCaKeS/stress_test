a53f1d4135be296bae8f007efe7a471164420bea
ivelikanov <ivelikanov@astralinux.ru>
2025-08-21 16:34:30 +0300

    small fixes

--- cluster_file_systems/cfs_run_ceph.py
+++ cluster_file_systems/cfs_run_ceph.py
@@ -70,6 +70,9 @@ class Ceph:
             check_output_command(self.storagecreate.format(size="25", number=ind))
             sleep(20)
             check_output_command(self.storageattach.format(node=node, storage_name=STORAGE_NAME))
+            sleep(10)
+            check_output_command(self.startvm.format(node=node))
+            sleep(10)
         
         create_remote_file(local_file_path="libs", 
                             remote_file_path="/var/tmp/libs", 

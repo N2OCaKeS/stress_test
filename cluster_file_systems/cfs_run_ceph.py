28bb791ec7eb78307a56d64118114051b94d8d9b
ivelikanov <ivelikanov@astralinux.ru>
2025-09-04 22:15:36 +0300

    small fixes and args

--- cluster_file_systems/cfs_run_ceph.py
+++ cluster_file_systems/cfs_run_ceph.py
@@ -1,10 +1,122 @@
+import argparse
 from time import sleep
 from libs.libcfs import check_output_command, send_remote_command, create_remote_file, get_remote_file
 
 from cfs_create_vms import VMS
 from cfs_storage_init_ceph import CephStorageCreate
-from cfs_conf import STORAGE_NAME, SCRIPT_DIR, REPORT_DIR, REPORT_FILENAME
+from cfs_conf import STORAGE_NAME, SCRIPT_DIR, REPORT_DIR_HOST, REPORT_PATH_HOST
 from libs.libtable import Report
+from libs.zefir import UploaderZC
+
+def parse_args():
+    
+    DESCRIPTION = ""
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-u', '--username',
+                        action='store',
+                        required=True,
+                        help='confluence user',
+                        dest='USER')
+
+    parser.add_argument('-t', '--token',
+                        action='store',
+                        required=False,
+                        default=None,
+                        help='confluence access token',
+                        dest='TOKEN')
+
+    parser.add_argument('-cs', '--confluence-space',
+                        action='store',
+                        required=True,
+                        help='confluence space',
+                        dest='SPACE')
+
+    parser.add_argument('-cpp', '--confluence-parent-page',
+                        action='store',
+                        required=True,
+                        help='confluence parent page',
+                        dest='PPAGE')
+
+    parser.add_argument('-cnp', '--confluence-new-page',
+                        action='store',
+                        required=True,
+                        help='confluence new page',
+                        dest='NPAGE')
+
+    parser.add_argument('-sn', '--stand-num',
+                        action='store',
+                        choices=['1',
+                                '2',
+                                '3',
+                                '4'],
+                        required=True,
+                        help='stand num',
+                        dest='STAND')
+
+    parser.add_argument('-fti', '--folder-tree-id',
+                        action='store',
+                        required=True,
+                        help='folder-tree-id',
+                        dest='FTI')
+
+    parser.add_argument('-tcyc', '--test-cycle-name',
+                        action='store',
+                        required=True,
+                        help='test-cycle-name',
+                        dest='TCYC')
+
+    parser.add_argument('-tcas', '--test-case-name',
+                        action='store',
+                        required=True,
+                        help='test-case-name',
+                        dest='TCAS')
+
+    parser.add_argument('-ba', '--basic-auth',
+                        action='store',
+                        required=True,
+                        help='basic-auth',
+                        dest='BA')
+
+    parser.add_argument('-tcv', '--test-cycle-version',
+                        action='store',
+                        required=True,
+                        help='test-cycle-version',
+                        dest='TCV')
+    
+    parser.add_argument('--disk-size',
+                        action='store',
+                        required=False,
+                        type=str,
+                        default='25',
+                        help='size of vdi disk',
+                        dest='DISK_SIZE')
+
+    parser.add_argument('--test-set',
+                        action='store',
+                        choices=['base_load',
+                                 'timeout',
+                                 'multithreaded',
+                                 'big_files',
+                                 'fs_mark_count',
+                                 'fs_mark_size'],
+                                 default='fs_mark_count',
+                                 required=False,
+                                 dest='TS')
+    
+    parser.add_argument('-vbox', 
+                        action='store',
+                        required=True,
+                        help='vbox name',
+                        dest='VBOX')
+
+    parser.add_argument('-kernel', 
+                        action='store',
+                        required=True,
+                        help='vbox name',
+                        dest='KERNEL')
+
+    return parser.parse_args()
+
 
 class Ceph:
 
@@ -15,11 +127,13 @@ class Ceph:
     controlvm_off = 'virsh --connect qemu:///system destroy {host}'
     run_test_cmd = 'sudo python3 {dir}/{file} --test-set {ts}'
 
-    def __init__(self, vbox, kernel, all_hosts, type_load_test):
+    def __init__(self, vbox, kernel, all_hosts, type_load_test, **kwargs):
         self.vbox = vbox
         self.kernel = kernel
         self.all_hosts = all_hosts
         self.type_load_test = type_load_test
+        for key, value in kwargs.items():
+            setattr(self, key, value)
         self.HOSTS = {
             'testvm1': {
                 'ip': "127.0.0.1",
@@ -53,6 +167,22 @@ class Ceph:
                 "port": 60005
             }
         }
+        # TODO 
+        uzs = UploaderZC(folder_tree_id=args.FTI,
+                         test_cycle_name=args.TCYC,
+                         test_case_name=args.TCAS,
+                         basic_auth=args.BA,
+                         test_cycle_version=args.TCV,
+                         token=args.TOKEN,
+                         username=args.USER,
+                         conf_space=args.SPACE,
+                         conf_parent_page=args.PPAGE,
+                         conf_new_page_name=args.NPAGE,
+                         grade_stand=args.STAND,
+                         file_system=args.FS,
+                         test_set=args.TS)
+    
+        uzs.upload_test_cycle_status('progress')
 
 
     def start(self):
@@ -143,7 +273,7 @@ class Ceph:
                                 user=self.HOSTS["testvm1"]['login'], 
                                 password=self.HOSTS["testvm1"]['password'])
             get_remote_file(remote_file_path="/var/tmp/report_fio.txt",
-                            local_file_path=f"{REPORT_FILENAME}",
+                            local_file_path=f"{REPORT_PATH_HOST}",
                             ip=self.HOSTS["testvm1"]['ip'], 
                             user=self.HOSTS["testvm1"]['login'], 
                             password=self.HOSTS["testvm1"]['password'])
@@ -157,18 +287,22 @@ class Ceph:
                                 user=self.HOSTS["testvm1"]['login'], 
                                 password=self.HOSTS["testvm1"]['password'])
             get_remote_file(remote_file_path="/var/tmp/report",
-                            local_file_path=f"{REPORT_DIR}",
+                            local_file_path=f"{REPORT_DIR_HOST}",
                             ip=self.HOSTS["testvm1"]['ip'], 
                             user=self.HOSTS["testvm1"]['login'], 
                             password=self.HOSTS["testvm1"]['password'])
-
+        
+        self.uzs.public = True
+        self.uzs.statistics = True
+        self.uzs.upload_test_cycle_status(zefir_status='pass')
 
 if __name__ == "__main__":
+    args = parse_args()
+    args_dict = vars(args)
+
     c = Ceph(vbox="1.8.3.7",
              kernel="6.1",
              all_hosts=["testvm1", "testvm2", "testvm3", "testvm4", "testvm5"],
-             type_load_test="fio")
+             type_load_test="fio",
+             **args_dict)
     c.start()
-
-
-

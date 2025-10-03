be8b0866a0f28eb390fb73fe1be3c1e681f9f843
ivelikanov <ivelikanov@astralinux.ru>
2025-10-03 20:38:15 +0300

    fixes

--- freeipa/ipa_create_users_test_thr.py
+++ freeipa/ipa_create_users_test_thr.py
@@ -5,15 +5,19 @@ import time
 import threading
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import queue
+import os
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 SERVER = "virtual-station1.stress-testing.local"
 USER_START = 1
-USER_END = 100
+USER_STEP = 10
+USER_MAX = 100
+MAX_WORKERS = os.cpu_count()
 
 client = ClientMeta(SERVER, verify_ssl=False)
-client.login_kerberos()
+# client.login_kerberos()
+client.login("admin", "12345678")
 
 results_queue = queue.Queue()
 
@@ -43,35 +47,54 @@ def create_user(i, client_inst):
         print(f"Ошибка создания {login}: {e}")
         results_queue.put((login, 0, False))
 
+def del_user(user_id):
+    try:
+        login = f"user{user_id}"
+        client.user_del(login)
+        print(f"Пользователь {login} успешно удален")
+        # return (username, True)
+    except FreeIPAError as e:
+        print(f"Ошибка удаления {login}: {e}")
+        # return (username, False)
+
 def main():
-    start_total = time.time()
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_user = {
-            executor.submit(create_user, i, client): i for i in range(USER_START, USER_END + 1)
-        }
+    for user_count in range(USER_START, USER_MAX + USER_STEP, USER_STEP):
+        start_total = time.time()
+        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            future_to_user = {
+                executor.submit(create_user, i, client): i for i in range(USER_START, user_count)
+            }
+            
+            for future in as_completed(future_to_user):
+                user_id = future_to_user[future]
+                try:
+                    future.result()
+                except Exception as exc:
+                    print(f'Пользователь {user_id} сгенерировал исключение: {exc}')
         
-        for future in as_completed(future_to_user):
-            user_id = future_to_user[future]
-            try:
-                future.result()
-            except Exception as exc:
-                print(f'Пользователь {user_id} сгенерировал исключение: {exc}')
-    
-    end_total = time.time()
-    total_time = end_total - start_total
-    
-    elapsed_times = []
-    successful_users = 0
-    
-    while not results_queue.empty():
-        login, elapsed, success = results_queue.get()
-        if success:
-            elapsed_times.append(elapsed)
-            successful_users += 1
-    
-    average_time_per_user = sum(elapsed_times) / successful_users if successful_users > 0 else 0
-    
+        end_total = time.time()
+        total_time = end_total - start_total
+        
+        elapsed_times = []
+        successful_users = 0
+        
+        while not results_queue.empty():
+            login, elapsed, success = results_queue.get()
+            if success:
+                elapsed_times.append(elapsed)
+                successful_users += 1
+        
+        average_time_per_user = sum(elapsed_times) / successful_users if successful_users > 0 else 0
+        print(successful_users)
+        print(total_time)
+        print(average_time_per_user)
+        with open("ipa_report.txt", 'a') as report_file:
+            report_file.write(f"{user_count} {successful_users} {total_time} {average_time_per_user}")
+        
+        print("Удаление пользователей...")
+        for user_id in range(USER_START, user_count):
+            del_user(user_id=user_id)
+       
 
 if __name__ == "__main__":
     main()
-    print("все пользователи созданы")

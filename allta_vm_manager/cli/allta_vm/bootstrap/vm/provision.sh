2f80bc9a7df0608e217c95a0ec7da344611a59d6
mfilippenko <mfilippenko@astralinux.ru>
2025-10-06 15:36:17 +0300

    fix dns_server

--- allta_vm_manager/cli/allta_vm/bootstrap/vm/provision.sh
+++ allta_vm_manager/cli/allta_vm/bootstrap/vm/provision.sh
@@ -10,7 +10,7 @@ sudo timedatectl set-ntp true
 iface=`ip a | grep '2: ' | awk '{print$2}' | tr -d ':' | head -n 1`
 
 ip=$1
-dns="10.177.180.248, 10.177.128.198"
+dns="10.177.180.246 10.177.128.198"
 gateway="10.177.103.254"
 
 cat << EOF > /etc/network/interfaces
@@ -37,62 +37,7 @@ echo "u  ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 ip a
 mkdir -p /home/u/git
 
-OUTFILE="/home/u/git/git_clone.py"
-touch "$OUTFILE"
-
-cat > "$OUTFILE" <<'EOF'
-#!/bin/python3
-import subprocess
-from os import getcwd
-from sys import exit
-from ftplib import FTP
-
-def cmd(command):
-    subprocess.run([command], shell=True, check=True)
-
-
-attention_line = '=' * 130
-conf_file = getcwd() + '/gitclone.conf'
-
-#Проверяем директорию запуска
-if getcwd() != '/home/u/git':
-    print('\n', '\033[1m\033[33mВнимание!!!\033[0m')
-    print(attention_line)
-    print(f'Текущая директория {getcwd()}')
-    print('Запустите скрипт из директории /home/u/git')
-    print(attention_line, '\n')
-    exit(1)
-
-#Удаляем старый гит
-try:
-    cmd('sudo rm -r /home/u/git/stress_test')
-except Exception as e:
-    print('\n', '\033[1m\033[33mВнимание!!!\033[0m')
-    print(attention_line)
-    print(e)
-    print(attention_line, '\n')
-
-#Скачиваем конфиг
-def download_conf():
-    ftp = FTP('10.177.5.111')
-    ftp.login()
-    ftp.cwd('stress_reports/stress_test_config')
-    with open(conf_file, 'wb') as wf:
-        ftp.retrbinary('RETR gitclone.conf', wf.write)
-    ftp.quit()
-    with open(conf_file, 'r') as r:
-        conf = r.read()
-    return conf
-
-#Клонируем гит
-cmd(download_conf())
-EOF
-
-chmod +x "$OUTFILE"
-echo "Файл $OUTFILE создан и сделан исполняемым."
-
-mkdir /home/u/git/stress_test
-cd /home/u/git
+wget ftp://10.177.103.10/utils
 
 
 # auto eth0

ae29569e3e2694baf2057f8f67ac63cc320d7f92
ivelikanov <ivelikanov@astralinux.ru>
2025-10-08 19:36:55 +0300

    test fixes

--- cluster_file_systems/cfs_test_ceph_fio.py
+++ cluster_file_systems/cfs_test_ceph_fio.py
@@ -6,9 +6,9 @@ from cfs_conf import STORAGE_MOUNT_DIR
 class CephFIOTest:
 
     def __init__(self, abv):
-        self.size = "1G"
+        self.size = "20G"
         self.directory = STORAGE_MOUNT_DIR
-        self.runtime = 60 # в секундах
+        self.runtime = 300 # в секундах
         self.blocksize = "4k"
         self.report_file = "/var/tmp/report/report_fio.txt"
         self.abv = abv
@@ -18,7 +18,7 @@ class CephFIOTest:
             check_output_command(command="dpkg -i /var/tmp/fio/fio_3.12-2_amd64.deb")
 
     def run_test(self):
-        com_test = f"sudo fio --directory={self.directory} --direct=1 --rw=randrw --bs={self.blocksize} --ioengine=libaio --iodepth=256 --size={self.size} --runtime={self.runtime} --numjobs=3 --time_based --group_reporting --name=iops-qateam13-job --eta-newline=1 > {self.report_file}"
+        com_test = f"sudo fio --directory={self.directory} --direct=1 --rw=randrw --bs={self.blocksize} --ioengine=libaio --iodepth=256 --size={self.size} --runtime={self.runtime} --numjobs=2 --time_based --group_reporting --name=iops-qateam13-job --eta-newline=1 > {self.report_file}"
         check_output_command(command=com_test)
 
 

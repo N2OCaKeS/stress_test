1083a9136dbfa4827e3c602a5b275a5dc3ff7df4
mfilippenko <mfilippenko@astralinux.ru>
2025-10-13 18:55:36 +0300

    add command generator and disk resize for build

--- libs/test.py
+++ libs/test.py
@@ -1,21 +1,27 @@
 from allta import Libvirt, LibvirtManager
 
-Libvirt.prepare()
+# Libvirt.prepare()
+    
+a = Command()
 
+command = ["sudo ls /", "sudo ls ~", "sudo ls ~"]
 
-vms_dates = {
-    "testvm1": {
-        "cpu": "4",
-        "ram": "4096",
-        "ip_bridge": "10.177.103.180",
-        "disk": "100g"
-    }
-}
-for vm in vms_dates:
-    a = vms_dates.get(vm, {}) or {}
-    print(a.get("disk"))
-vms=['testvm1']
+cm = a.generator(command=command, target="test", sync=True)
 
-old = Libvirt.build(box="debian12", rc="1.7.5", vms=vms, vms_dates=vms_dates)
+print(cm)
 
-LibvirtManager.Vm.bridge(vms_date=old, new_vms_date=vms_dates, username="u", password="1")
+# vms_dates = {
+#     "testvm1": {
+#         "cpu": "4",
+#         "ram": "4096",
+#         "ip_bridge": "10.177.103.180",
+#         "disk": "100"
+#     }      
+# }
+
+# vms=['testvm1']
+
+# Libvirt.build(box="debian12", rc="1.7.5", vms=vms, vms_dates=vms_dates)
+
+# LibvirtManager.Vm.bridge(vms_date=old, new_vms_date=vms_dates, username="u", password="1")
+

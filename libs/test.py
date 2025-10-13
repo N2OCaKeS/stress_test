10c6b507380b0b2f23b0ca4b6d4c6159b801e192
mfilippenko <mfilippenko@astralinux.ru>
2025-10-13 19:57:59 +0300

    upd docs

--- libs/test.py
+++ libs/test.py
@@ -1,27 +1,20 @@
 from allta import Libvirt, LibvirtManager
 
-# Libvirt.prepare()
-    
-a = Command()
+Libvirt.prepare()
 
-command = ["sudo ls /", "sudo ls ~", "sudo ls ~"]
 
-cm = a.generator(command=command, target="test", sync=True)
+vms_dates = {
+    "testvm1": {
+        "cpu": "4",
+        "ram": "4096",
+        "ip_bridge": "10.177.103.180",
+        "disk": "100"
+    }      
+}
 
-print(cm)
+vms=['testvm1']
 
-# vms_dates = {
-#     "testvm1": {
-#         "cpu": "4",
-#         "ram": "4096",
-#         "ip_bridge": "10.177.103.180",
-#         "disk": "100"
-#     }      
-# }
-
-# vms=['testvm1']
-
-# Libvirt.build(box="debian12", rc="1.7.5", vms=vms, vms_dates=vms_dates)
+Libvirt.build(box="1.8.1.s", rc="1.8.1.6", vms=vms, vms_dates=vms_dates, bridge=True)
 
 # LibvirtManager.Vm.bridge(vms_date=old, new_vms_date=vms_dates, username="u", password="1")
 

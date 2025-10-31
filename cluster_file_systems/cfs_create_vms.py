6a3ead0dd0df0e7474a6b0f261a16fb2750471ac
ivelikanov <ivelikanov@astralinux.ru>
2025-10-31 14:03:22 +0300

    small fixes

--- cluster_file_systems/cfs_create_vms.py
+++ cluster_file_systems/cfs_create_vms.py
@@ -119,7 +119,7 @@ class VMS:
                 } for vm in self.vms}
         print(f'VM dates is:\n{self.vm_dates}')
 
-        LibvirtManager.VM.stop(self.vm_dates.keys())
+        LibvirtManager.Vm.stop(self.vm_dates.keys())
 
 
 

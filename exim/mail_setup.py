a16f51c4e91c6c1732bd5a381cb2ce16966a19de
ivelikanov <ivelikanov@astralinux.ru>
2025-11-20 22:59:47 +0300

    set config exim4

--- exim/mail_setup.py
+++ exim/mail_setup.py
@@ -1,13 +1,43 @@
 import re
+import subprocess
 
 class Exim:
     def __init__(self):
         self.service_name = 'smtp'
         self.service_config_dir = f'/etc/exim4/'
         self.config_file = f"{self.service_config_dir}/exim4.conf"
+        self.ip_local = ""
+        self.update_config_file = f"{self.service_config_dir}/update-exim4.conf.conf"
+        
+    def get_default_config(self):
+        config = {
+            'dc_eximconfig_configtype': 'local',
+            'dc_other_hostnames': 'test.local',
+            'dc_local_interfaces': f'{self.ip_local}',
+            'dc_readhost': '',
+            'dc_relay_domains': 'test.local',
+            'dc_minimaldns': 'false',
+            'dc_relay_nets': '10.177.103.0/24',
+            'dc_smarthost': '',
+            'CFILEMODE': '644',
+            'dc_use_split_config': 'true',
+            'dc_hide_mailname': '',
+            'dc_mailname_in_oh': 'true',
+            'dc_localdelivery': 'maildir_home'
+        }
+        return config
+    
+    def write_config_file(self):
+        config_content = "\n".join([f"{key}='{value}'" for key, value in self.get_default_config().items()]) + "\n"
+        with open(self.update_config_file, 'w') as f:
+            f.write(config_content)
+    
+    def update_exim_config(self):
+        subprocess.run(['update-exim4.conf'], check=True)
 
-    def set_configuration(self):
-        pass
+    def restart_service(self):
+        subprocess.run(['systemctl', 'restart', 'exim4'], check=True)
+        
 
 
 class Dovecot:

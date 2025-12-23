7320e769385bd24b13de4fb66d8dd3d4f46eb120
mfilippenko <mfilippenko@astralinux.ru>
2025-12-23 13:43:34 +0300

    fix

--- astra_openvpn/ovpn/ovpn_vm.py
+++ astra_openvpn/ovpn/ovpn_vm.py
@@ -332,7 +332,7 @@ EOF'""",
                 f"--client_start {client_start} --client_count {client_count}'"
             )
 
-            start_client[host] = {"run_perf": {"command": cmd, "nowait": True, "nowait_timeout": {total_seconds}}}
+            start_client[host] = {"run_perf": {"command": cmd, "nowait": True, "nowait_timeout": total_seconds}}
 
         Libvirt.execute(
             commands=start_client,

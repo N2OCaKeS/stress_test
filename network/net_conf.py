06ef8f7a8c60bc86fba59f39eb779dbd0047e4b5
dtimonin <dtimonin@astralinux.ru>
2026-02-27 14:50:22 +0300

    add run.py

--- network/net_conf.py
+++ network/net_conf.py
@@ -8,6 +8,7 @@ response_jira_url = requests.get(jira_url_api)
 response_confluence_url = requests.get(confluence_url_api)
 JIRA_URL = response_jira_url.text
 CONFLUENCE_URL = response_confluence_url.text
+VENV_PATH = '/home/u/python/Python-3.12.1/venv/bin/python3.12'
 
 BASE_PATH = "/home/u/git/stress_test/network"
 
---
+++ network/run.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import subprocess
+import argparse
+from net_conf import REPORT_PATH, VENV_PATH
+from os import path, mkdir
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n',
+                    action='store',
+                    required=True,
+                    help='dates name',
+                    dest='NAME')
+
+args = parser.parse_args()
+
+if not path.isdir(REPORT_PATH):
+    mkdir(REPORT_PATH)
+
+with open(f'/home/u/{args.NAME}', 'r') as r:
+        dates = r.read()
+
+
+subprocess.run(f'sudo {VENV_PATH} test_run.py {dates}', shell=True)

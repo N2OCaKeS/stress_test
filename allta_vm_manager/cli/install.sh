60d49cb4a220d3dc2e1fee4e98c6e334ffc3b318
mfilippenko <mfilippenko@astralinux.ru>
2026-03-15 15:40:25 +0300

    fix apt

--- allta_vm_manager/cli/install.sh
+++ allta_vm_manager/cli/install.sh
@@ -135,7 +135,7 @@ upload() {
 
     echo "Обновляю пакет на ${username}@${UPLOAD_HOST}:/srv/ftp"
     ssh "${username}@${UPLOAD_HOST}" \
-        "sudo rm -f /srv/ftp/allta-vm*.deb && sudo mv \"\$HOME/${deb_name}\" /srv/ftp/"
+        "sudo rm -f /srv/ftp/boxes/allta-vm*.deb && sudo mv \"\$HOME/${deb_name}\" /srv/ftp/boxes/ && sudo chmod 640 /srv/ftp/boxes/allta-vm*.deb"
 }
 
 usage() {

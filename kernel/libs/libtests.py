0675c2b8bceb5c27671417fd94d5d45af7cb907e
ivelikanov <ivelikanov@astralinux.ru>
2026-04-15 18:36:53 +0300

    small fixes

--- kernel/libs/libtests.py
+++ kernel/libs/libtests.py
@@ -431,7 +431,7 @@ class XFSMemoryLeak(CreateVM):
                 {
                     "mode": "push",
                     "path_host": f'{BASE_PATH}/start_xfs_test.py', 
-                    "path_vm": '/home/u/get_info.py', 
+                    "path_vm": '/home/u/start_xfs_test.py', 
                 },
             ]
         }

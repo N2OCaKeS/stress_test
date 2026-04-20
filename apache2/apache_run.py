43c38238c5b80be644568a9d80b2d5a17bee0d1f
dtimonin <dtimonin@astralinux.ru>
2026-04-20 19:20:46 +0300

    add status pass

--- apache2/apache_run.py
+++ apache2/apache_run.py
@@ -112,8 +112,10 @@ if args.TESTNAME == 'apache_pam':
     abp.prepare_vms()
     abp.create_test_env()
     abp.start_test()
-
+    abp.vms_destroy()
 
 
     lead_time = get_duration((datetime.now() - time_start_script).total_seconds())
+
+    uzs.upload_test_cycle_status(zefir_status='pass')
 

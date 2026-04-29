fa5b92ec5329bdebdde73b584980cfa2eb47d578
dtimonin <dtimonin@astralinux.ru>
2026-04-29 15:45:59 +0300

    Add log

--- osbench/src/osbench.py
+++ osbench/src/osbench.py
@@ -1,10 +1,22 @@
 
 import sys
-from pathlib import Path
-sys.path.insert(0, str(Path(__file__).parent.parent))
 
+from config.conf import MAIN_DIR
+from src.osb_logger import log
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from unixbench import UnixBench
 
+
+
+log.setup(
+    name="OSBench",
+    log_file=f"{MAIN_DIR}/logs/osbench.log",
+    log_level="DEBUG",
+    console=True,
+    colored_console=True
+)
 
 
 

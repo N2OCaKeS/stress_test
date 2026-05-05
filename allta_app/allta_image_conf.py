76d2d8b2d2749d92214409ffb36ab203d61da9aa
ivelikanov <ivelikanov@astralinux.ru>
2026-05-05 22:39:22 +0300

    allta_app - v2.65.0

--- allta_app/allta_image_conf.py
+++ allta_app/allta_image_conf.py
@@ -111,7 +111,7 @@ tests_list = {'PostgreSQL':     ['postgresql', 'psql parsec', 'psql kernels', 'p
                                  'Dovecot-IMAP', 'Exim4-SMTP', 'SegFault', "XFS mem leak"],
             'UnixBench':        ['unix', 'unix parsec'],
             'FreeIPA':          ['FreeIPA auth', 'FreeIPA c-users', "FreeIPA plugin"],
-            'Parsec':           ['parsec impact-fs', 'parsec impact-fs aud-off', 'digsig-cdt'],
+            'Parsec':           ['parsec impact-fs', 'parsec impact-fs aud-off', 'digsig-cdt', 'raw-spin-lock'],
             'Apache':           ['apache-rp', 'apache-bp'],
             'Docker/Podman/LXC':['docker-wa'],
             'Qemu/KVM/Libvirt': ['steal time', 'steal time-sm', 'FIO', 'vUnixBench', 'vPingPong', 'FIO large'],
@@ -208,7 +208,8 @@ branches = {
     'Network benchmark. Init_on_free':'network',
     'segmentation_fault':'kernel',
     'postgresql benchmark olap':'postgresql',
-    'xfs memory leak':'kernel'
+    'xfs memory leak':'kernel',
+    'Raw spin lock benchmark':'parsec',
 }
 
 
@@ -279,7 +280,8 @@ tests = {
     'Network benchmark. Init_on_free':'InitOnFree',
     'segmentation_fault':'SegFault',
     'postgresql benchmark olap':'PSQL OLAP-hq',
-    'xfs memory leak': 'XFS mem leak'
+    'xfs memory leak': 'XFS mem leak',
+    'Raw spin lock benchmark': 'raw-spin-lock',
 }
 
 
@@ -312,7 +314,7 @@ main_tests = ['XFS', 'EXT4', 'NTFS', 'EXT4 parsec', 'postgresql', 'postgresql-sm
               'psql kernels', 'tantor kernels', 'unix parsec', 'psql balance', 'FreeIPA auth', 'parsec impact-fs', 'parsec impact-fs aud-off',
               'apache-rp', 'apache-bp', 'steal time', 'EXT2', 'EXT3', 'FAT', 'EXFAT', 'FIO', 'vUnixBench', 'vPingPong', 'OCFS2', 'steal time-sm', 'psql oom',
               'digsig-cdt', 'docker-wa', 'CEPH', 'CEPH fio', 'FreeIPA c-users', 'CEPH parsec', 'AOpenVPNcc', 'Dovecot-IMAP', 'Exim4-SMTP',
-              'FIO large', 'InitOnFree', 'SegFault', 'PSQL OLAP-hq', 'FreeIPA plugin', 'XFS mem leak']
+              'FIO large', 'InitOnFree', 'SegFault', 'PSQL OLAP-hq', 'FreeIPA plugin', 'XFS mem leak', 'raw-spin-lock']
 
 
 
@@ -400,7 +402,7 @@ def changelog_testcycle_handler(rc: str, final=False) -> tuple:
             'orel_stand4':      ['postgresql-aud-off', 'postgresql', 'psql balance', 'steal time', 'psql kernels'],
             'smolensk_stand4':  ['postgresql-sm', 'psql parsec', 'psql vanilla', 'steal time-sm'],
             'orel_stand10':     ['NTFS', 'OCFS2', 'CEPH', 'CEPH fio'],
-            'smolensk_stand10': ['CEPH parsec'],
+            'smolensk_stand10': ['CEPH parsec', 'raw-spin-lock'],
             'orel_stand11':     ['docker-wa', 'FIO', 'vUnixBench', 'vPingPong', 'AOpenVPNcc', 'Dovecot-IMAP', 'Exim4-SMTP', 'FIO large'],
             'smolensk_stand11': ['parsec impact-fs', 'parsec impact-fs aud-off', 'psql oom'],
             'orel_stand12':     ['syslog-ng', 'InitOnFree', 'SegFault', 'XFS mem leak'],
@@ -420,7 +422,7 @@ def changelog_testcycle_handler(rc: str, final=False) -> tuple:
 stands_groups = {
     'stand3_group': ['EXT2', 'EXT3', 'EXT4', 'FAT',  'EXFAT', 'XFS', 'EXT4 parsec', 'XFS parsec', 'FreeIPA auth', 'unix', 'unix parsec', 'FreeIPA c-users', 'FreeIPA plugin'],
     'stand4_group': ['postgresql-aud-off', 'postgresql', 'postgresql-sm', 'psql parsec', 'psql vanilla', 'psql balance', 'steal time', 'steal time-sm', 'psql kernels'],
-    'stand10_group':['NTFS', 'OCFS2', 'CEPH', 'CEPH fio', 'CEPH parsec'],
+    'stand10_group':['NTFS', 'OCFS2', 'CEPH', 'CEPH fio', 'CEPH parsec', 'raw-spin-lock'],
 
     'stand11_group':['parsec impact-fs', 'parsec impact-fs aud-off', 'docker-wa', 'FIO', 'vUnixBench', 'vPingPong', 'psql oom', 'AOpenVPNcc', 'Dovecot-IMAP', 'Exim4-SMTP', 'FIO large'],
     'stand12_group':['syslog-ng', 'auditd-f', 'auditd-p', 'auditd-u', 'digsig-cdt', 'apache-rp', 'apache-bp', 'InitOnFree', 'SegFault', 'PSQL OLAP-hq', 'XFS mem leak'],
@@ -485,6 +487,7 @@ tests_case_zefir_key = {
     'PSQL OLAP-hq':'BT-T19100',
     'FreeIPA plugin':'BT-T19492',
     'XFS mem leak':'BT-T19753',
+    'raw-spin-lock': 'BT-T19967'
 }
 
 
@@ -517,7 +520,8 @@ testname_columns = {
                     'Network benchmark. Init_on_free':'InitOnFree', 'segmentation_fault':'SegFault',
                     'postgresql benchmark olap':'PSQL_OLAP-hq',
                     'freeipa plugin test':'FreeIPA_plugin',
-                    'xfs memory leak':'XFS_mem_leak'
+                    'xfs memory leak':'XFS_mem_leak',
+                    'Raw spin lock benchmark': 'raw-spin-lock'
                     }
 
 
--- allta_app/backup_image.py
+++ allta_app/backup_image.py
@@ -375,6 +375,9 @@ elif args.PARSEC_IMPACT or args.PARSEC_IMPACT_AO:
 elif args.TEST == 'digsig-cdt':
     dates = f'{username} {token} {confluence_space} {confluence_parent_page} {confluence_new_page} \
               {sn} {fti} {tcyc} {tcas} {ba} {tcv} -ds'
+elif args.TEST == 'raw-spin-lock':
+    dates = f'{username} {token} {confluence_space} {confluence_parent_page} {confluence_new_page} \
+              {sn} {fti} {tcyc} {tcas} {ba} {tcv} -rsl'
 elif args.TEST == 'docker-wa':
     dates = f'{username} {token} {confluence_space} {confluence_parent_page} {confluence_new_page} \
               {sn} {fti} {tcyc} {tcas} {ba} {tcv} -wa'

a5e827104f8cca1f7f2fbfd9a4b25aea68f893c7
mfilippenko <mfilippenko@astralinux.ru>
2026-06-24 10:18:28 +0300

    limit dept task panel to dep_admin home (account_admin lacks server task access)

--- dbos_server_service/web_ui/src/pages/home/HomeAccountAdmin.tsx
+++ dbos_server_service/web_ui/src/pages/home/HomeAccountAdmin.tsx
@@ -23,7 +23,6 @@ import { listGroups } from "@/api/auth/groups";
 import { useMockMode, useQuery } from "@/api/auth/useQuery";
 import { RecentAuditEvents } from "./widgets/RecentAuditEvents";
 import { ServicesHealth } from "./widgets/ServicesHealth";
-import { AllTasksWidget } from "./widgets/AllTasksWidget";
 
 /**
  * Account-admin Home.
@@ -128,10 +127,6 @@ export function HomeAccountAdmin() {
         <section className="grid gap-4 md:grid-cols-2">
           <RecentAuditEvents />
           <ServicesHealth />
-        </section>
-
-        <section className="mt-4">
-          <AllTasksWidget />
         </section>
       </HomeShell>
     );

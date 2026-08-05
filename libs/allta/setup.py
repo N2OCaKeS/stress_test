4eecf1be25cd8ec6b462329da6780eef3509bfbd
mfilippenko <mfilippenko@astralinux.ru>
2026-08-05 14:23:42 +0300

    upd version

--- libs/allta/setup.py
+++ libs/allta/setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
   name='allta',
-  version='1.2.0',
+  version='1.2.2',
   author='team13',
   author_email='mfilippenko@astralinux.ru',
   description='Библиотеки для stress тестов',

39ae785eb5c757df8b98d4ffc936007519b66880
ivelikanov <ivelikanov@astralinux.ru>
2026-08-07 11:39:08 +0300

    added artifactory pypi

--- network/provision/provision.sh
+++ network/provision/provision.sh
@@ -18,5 +18,9 @@ wget ftp://10.177.103.10/allta*.deb
 sudo apt-get update
 sudo apt-get install -i allta*.deb
 
+sudo apt-get install python3-pip -y
+python3 -m pip config set global.extra-index-url https://artifactory.astralinux.ru/artifactory/api/pypi/gca-pypi-remote/simple
+sudo python3 -m pip config set global.extra-index-url https://artifactory.astralinux.ru/artifactory/api/pypi/gca-pypi-remote/simple
+
 cat /etc/astra/build_version > /home/u/av.txt
 uname -r > /home/u/kernel.txt

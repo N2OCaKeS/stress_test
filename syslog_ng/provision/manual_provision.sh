e2ffd3350460fe0255f219d6106c0fc5321d1050
mfilippenko <mfilippenko@astralinux.ru>
2026-08-31 15:07:28 +0300

    add global pip config before python

--- syslog_ng/provision/manual_provision.sh
+++ syslog_ng/provision/manual_provision.sh
@@ -2,6 +2,10 @@
 
 set -vx
 
+sudo install -d -m 0755 /etc
+printf '%s\n' '[global]' 'index-url = http://allta.devos.astralinux.ru:3141/root/release' 'trusted-host = allta.devos.astralinux.ru' | sudo tee /etc/pip.conf >/dev/null
+sudo chmod 0644 /etc/pip.conf
+
 if [ "$1" == "debian" ] || [ "$1" == "astra" ] || [ "$1" == "alt" ]; then
     pm=apt-get
 elif [ "$1" == "rhel" ] || [ "$1" == "redos" ]; then

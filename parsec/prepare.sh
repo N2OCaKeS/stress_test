90bb726da75a98c1a90a5aeb7fa7a9eb14acb1f9
mfilippenko <mfilippenko@astralinux.ru>
2026-08-31 15:07:10 +0300

    add global pip config before python

--- parsec/prepare.sh
+++ parsec/prepare.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo install -d -m 0755 /etc
+sudo tee /etc/pip.conf >/dev/null <<'EOF'
+[global]
+index-url = http://allta.devos.astralinux.ru:3141/root/release
+trusted-host = allta.devos.astralinux.ru
+EOF
+sudo chmod 0644 /etc/pip.conf
+
 dpkg -s jq &> /dev/null || sudo apt-get install jq -y
 wget http://allta.devos.astralinux.ru/rest/api/get-repo-path -O releases.json
 sudo jq -r ".\"$2\"[]" releases.json > /etc/apt/sources.list
@@ -35,9 +43,6 @@ sudo make altinstall
 
 python3.12 -m venv venv
 source venv/bin/activate
-
-sudo python3 -m pip config --global set global.index-url http://allta.devos.astralinux.ru:3141/root/release
-sudo python3 -m pip config --global set global.trusted-host allta.devos.astralinux.ru
 
 cd /home/u/git/stress_test/$1
 python3.12 -m pip install --upgrade pip

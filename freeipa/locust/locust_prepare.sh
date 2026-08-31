6d0b40f3a359f8f0395eb500c1e9200404aae655
mfilippenko <mfilippenko@astralinux.ru>
2026-08-31 12:31:26 +0300

    change pip repo to devpi

--- freeipa/locust/locust_prepare.sh
+++ freeipa/locust/locust_prepare.sh
@@ -21,6 +21,9 @@ python3.12 -m venv venv
 source venv/bin/activate
 
 cd /home/u/git/stress_test/freeipa
+# Allta devpi package index
+sudo python3 -m pip config --global set global.index-url http://allta.devos.astralinux.ru:3141/root/release
+sudo python3 -m pip config --global set global.trusted-host allta.devos.astralinux.ru
 python3.12 -m pip install --upgrade pip
 python3.12 -m pip install -r locust/requirements.txt
 if [[ $? != 0 ]]; then

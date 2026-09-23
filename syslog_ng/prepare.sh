4929718583f1ca9a0896f48c665b6fbbc1fb2825
mfilippenko <mfilippenko@astralinux.ru>
2026-09-23 11:50:07 +0300

    fix pip.conf

--- syslog_ng/prepare.sh
+++ syslog_ng/prepare.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo install -d -m 0755 /etc
-printf '%s\n' '[global]' 'index-url = http://allta.devos.astralinux.ru:3141/root/release' 'trusted-host = allta.devos.astralinux.ru' | sudo tee /etc/pip.conf >/dev/null
+sudo tee /etc/pip.conf >/dev/null <<'EOF'
+[global]
+index-url = http://allta.devos.astralinux.ru:3141/root/release
+trusted-host = allta.devos.astralinux.ru
+EOF
 sudo chmod 0644 /etc/pip.conf
 
 18repo() {
